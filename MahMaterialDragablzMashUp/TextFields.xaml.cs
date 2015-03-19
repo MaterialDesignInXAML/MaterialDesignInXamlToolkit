bc8d5716bc9c32d9f39fff8db3d819886be7f376
ButchersBoy <james.willock@mulhollandsoftware.com>
2015-03-19 20:57:22 +0000

    update demos

---
+++ MahMaterialDragablzMashUp/TextFields.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MahMaterialDragablzMashUp
+{
+	/// <summary>
+	/// Interaction logic for Fields.xaml
+	/// </summary>
+	public partial class TextFields : UserControl
+	{
+		public TextFields()
+		{
+			InitializeComponent();
+			DataContext = new TextFieldsViewModel();
+		}
+	}
+
+	public class TextFieldsViewModel : INotifyPropertyChanged
+	{
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set
+			{
+				_name = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
