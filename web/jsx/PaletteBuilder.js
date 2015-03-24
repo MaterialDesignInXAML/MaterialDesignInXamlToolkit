236235960074fc9ec39abbbd9f98b2d383d6523f
James Willock <james.willock@mulhollandsoftware.com>
2015-03-24 21:30:17 +0000

    basic working version palette builder [skip ci]

--- web/jsx/PaletteBuilder.js
+++ web/jsx/PaletteBuilder.js
@@ -1,27 +1,55 @@
+function colourItemMapper (colorItem) {
+			return (
+				<Color 
+					backgroundName={colorItem.backgroundName} 
+					backgroundColour={colorItem.backgroundColour} 
+					foregroundName={colorItem.foregroundName} 
+					foregroundColour={colorItem.foregroundColour} 
+					foregroundOpacity={colorItem.foregroundOpacity} />
+			);
+		}
+
 var Color = React.createClass({
 	render: function() {
 
-		var style = {
-			background:this.props.backgroundColour,
-			color:this.props.foregroundColour
+		var bgStyle = {
+			background:this.props.backgroundColour			
+		};
+		var fgStyle = {
+			color:this.props.foregroundColour,
+			opacity:this.props.foregroundOpacity
 		};
 
 		return (
-			<div style={style}>{this.props.backgroundName}</div>
+			<div className="swatchColor" style={bgStyle}>
+				<span style={fgStyle}>{this.props.backgroundName}</span>
+			</div>
+		);
+	}
+});
+
+var SwatchSelector = React.createClass({
+	handleUseClick: function(isAccent, e) {
+		e.preventDefault();		
+		this.props.onUseClick(isAccent, e);
+	},
+	render: function() {
+		return (
+			<div>Use <a href="#" onClick={this.handleUseClick.bind(this, false)}>Primary</a> <a href="#" onClick={this.handleUseClick.bind(this, true)}>Accent</a></div>
 		);
 	}
 });
 
 var Swatch = React.createClass({
+	handleUseClick: function(index, isAccent, e) {		
+		this.props.onUseClick(index, isAccent, e);		
+	},
 	render: function() {
-		var colorNodes = this.props.colors.map(function (colorItem) {
-			return (
-				<Color backgroundName={colorItem.backgroundName} backgroundColour={colorItem.backgroundColour} foregroundName={colorItem.foregroundName} foregroundColour={colorItem.foregroundColour} />
-			);
-		});
+		var colorNodes = this.props.colors.map(colourItemMapper);		
 		return (
 			<div className="swatch">
 				{this.props.name}
+				<SwatchSelector onUseClick={this.handleUseClick.bind(this, this.props.index)} />
 				{colorNodes}
 			</div>
 		);
@@ -29,12 +57,15 @@ var Swatch = React.createClass({
 });
 
 var SwatchesBox = React.createClass({
+	handleUseClick: function(index, isAccent, e) {
+		this.props.onUseClick(index, isAccent);		
+	},
 	render: function() {
-		var swatchNodes = this.props.swatches.map(function(swatch) {
+		var swatchNodes = this.props.swatches.map(function(swatch, index) {
 			return (
-				<Swatch name={swatch.name} colors={swatch.colors} />
+				<Swatch name={swatch.name} colors={swatch.colors} index={index} onUseClick={this.handleUseClick} />
 			);
-		});
+		}, this);
 
 		return (
 			<div>
@@ -44,7 +75,42 @@ var SwatchesBox = React.createClass({
 	}
 });
 
+var HuesBox = React.createClass({
+	getInitialState: function() {
+		return {
+			primarySwatch: swatches[0],
+			accentSwatch: swatches[5]
+		}
+	},	
+	render: function() {
+		var primaryColours = this.state.primarySwatch.colors.filter(function(colorItem) {
+			return colorItem.backgroundName.lastIndexOf("Primary") === 0;
+		});
+		var accentColours = this.state.accentSwatch.colors.filter(function(colorItem) {
+			return colorItem.backgroundName.lastIndexOf("Accent") === 0;
+		});
+		var primaryColourNodes = primaryColours.map(colourItemMapper);
+		var accentColourNodes = accentColours.map(colourItemMapper);
+		return (
+			<div>				
+				{primaryColourNodes}
+				{accentColourNodes}
+			</div>
+		);
+	}
+});
+
+var huesBox = React.render(
+	<HuesBox />,
+	document.getElementById('hues')
+	);
+
+function mergeSwatch(index, isAccent) {	
+	var newSwatch = (isAccent ? { accentSwatch: swatches[index]} : { primarySwatch: swatches[index]});
+	huesBox.setState(newSwatch);
+}
+
 React.render(
-	<SwatchesBox swatches={swatches} />,
+	<SwatchesBox swatches={swatches} onUseClick={mergeSwatch} />,
 	document.getElementById('swatches')
-	)
+	);
--- web/scripts/PaletteBuilder.js
+++ web/scripts/PaletteBuilder.js
@@ -1,27 +1,55 @@
+function colourItemMapper (colorItem) {
+			return (
+				React.createElement(Color, {
+					backgroundName: colorItem.backgroundName, 
+					backgroundColour: colorItem.backgroundColour, 
+					foregroundName: colorItem.foregroundName, 
+					foregroundColour: colorItem.foregroundColour, 
+					foregroundOpacity: colorItem.foregroundOpacity})
+			);
+		}
+
 var Color = React.createClass({displayName: "Color",
 	render: function() {
 
-		var style = {
-			background:this.props.backgroundColour,
-			color:this.props.foregroundColour
+		var bgStyle = {
+			background:this.props.backgroundColour			
+		};
+		var fgStyle = {
+			color:this.props.foregroundColour,
+			opacity:this.props.foregroundOpacity
 		};
 
 		return (
-			React.createElement("div", {style: style}, this.props.backgroundName)
+			React.createElement("div", {className: "swatchColor", style: bgStyle}, 
+				React.createElement("span", {style: fgStyle}, this.props.backgroundName)
+			)
+		);
+	}
+});
+
+var SwatchSelector = React.createClass({displayName: "SwatchSelector",
+	handleUseClick: function(isAccent, e) {
+		e.preventDefault();		
+		this.props.onUseClick(isAccent, e);
+	},
+	render: function() {
+		return (
+			React.createElement("div", null, "Use ", React.createElement("a", {href: "#", onClick: this.handleUseClick.bind(this, false)}, "Primary"), " ", React.createElement("a", {href: "#", onClick: this.handleUseClick.bind(this, true)}, "Accent"))
 		);
 	}
 });
 
 var Swatch = React.createClass({displayName: "Swatch",
+	handleUseClick: function(index, isAccent, e) {		
+		this.props.onUseClick(index, isAccent, e);		
+	},
 	render: function() {
-		var colorNodes = this.props.colors.map(function (colorItem) {
-			return (
-				React.createElement(Color, {backgroundName: colorItem.backgroundName, backgroundColour: colorItem.backgroundColour, foregroundName: colorItem.foregroundName, foregroundColour: colorItem.foregroundColour})
-			);
-		});
+		var colorNodes = this.props.colors.map(colourItemMapper);		
 		return (
 			React.createElement("div", {className: "swatch"}, 
 				this.props.name, 
+				React.createElement(SwatchSelector, {onUseClick: this.handleUseClick.bind(this, this.props.index)}), 
 				colorNodes
 			)
 		);
@@ -29,12 +57,15 @@ var Swatch = React.createClass({displayName: "Swatch",
 });
 
 var SwatchesBox = React.createClass({displayName: "SwatchesBox",
+	handleUseClick: function(index, isAccent, e) {
+		this.props.onUseClick(index, isAccent);		
+	},
 	render: function() {
-		var swatchNodes = this.props.swatches.map(function(swatch) {
+		var swatchNodes = this.props.swatches.map(function(swatch, index) {
 			return (
-				React.createElement(Swatch, {name: swatch.name, colors: swatch.colors})
+				React.createElement(Swatch, {name: swatch.name, colors: swatch.colors, index: index, onUseClick: this.handleUseClick})
 			);
-		});
+		}, this);
 
 		return (
 			React.createElement("div", null, 
@@ -44,7 +75,42 @@ var SwatchesBox = React.createClass({displayName: "SwatchesBox",
 	}
 });
 
+var HuesBox = React.createClass({displayName: "HuesBox",
+	getInitialState: function() {
+		return {
+			primarySwatch: swatches[0],
+			accentSwatch: swatches[5]
+		}
+	},	
+	render: function() {
+		var primaryColours = this.state.primarySwatch.colors.filter(function(colorItem) {
+			return colorItem.backgroundName.lastIndexOf("Primary") === 0;
+		});
+		var accentColours = this.state.accentSwatch.colors.filter(function(colorItem) {
+			return colorItem.backgroundName.lastIndexOf("Accent") === 0;
+		});
+		var primaryColourNodes = primaryColours.map(colourItemMapper);
+		var accentColourNodes = accentColours.map(colourItemMapper);
+		return (
+			React.createElement("div", null, 				
+				primaryColourNodes, 
+				accentColourNodes
+			)
+		);
+	}
+});
+
+var huesBox = React.render(
+	React.createElement(HuesBox, null),
+	document.getElementById('hues')
+	);
+
+function mergeSwatch(index, isAccent) {	
+	var newSwatch = (isAccent ? { accentSwatch: swatches[index]} : { primarySwatch: swatches[index]});
+	huesBox.setState(newSwatch);
+}
+
 React.render(
-	React.createElement(SwatchesBox, {swatches: swatches}),
+	React.createElement(SwatchesBox, {swatches: swatches, onUseClick: mergeSwatch}),
 	document.getElementById('swatches')
-	)
+	);
