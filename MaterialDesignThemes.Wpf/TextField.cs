f9211d43ca3abb331a5b78d0229298eeb3fd19d7
ButchersBoy <james.willock@mulhollandsoftware.com>
2015-06-23 14:09:03 +0100

    start password box style

--- MaterialDesignThemes.Wpf/TextField.cs
+++ MaterialDesignThemes.Wpf/TextField.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -66,4 +67,59 @@ namespace MaterialDesignThemes.Wpf
 			return (string) element.GetValue(HintProperty);
 		}
 	}
+
+    public static class PasswordField
+    {
+        public static readonly DependencyProperty ManagedProperty = DependencyProperty.RegisterAttached(
+            "Managed", typeof (PasswordBox), typeof (PasswordField), new PropertyMetadata(default(PasswordBox), ManagedPropertyChangedCallback));
+
+        private static void ManagedPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var passwordBox = dependencyPropertyChangedEventArgs.OldValue as PasswordBox;
+            if (passwordBox != null)
+            {
+                passwordBox.PasswordChanged -= PasswordBoxOnPasswordChanged;
+            }
+
+            passwordBox = dependencyPropertyChangedEventArgs.NewValue as PasswordBox;
+            if (passwordBox != null)
+            {
+                passwordBox.PasswordChanged += PasswordBoxOnPasswordChanged;
+                ConfigureHint(passwordBox);
+            }
+        }
+
+        private static void PasswordBoxOnPasswordChanged(object sender, RoutedEventArgs routedEventArgs)
+        {
+            ConfigureHint((PasswordBox)sender);
+        }
+
+        private static void ConfigureHint(PasswordBox passwordBox)
+        {
+            passwordBox.SetValue(HintVisibilityProperty, passwordBox.SecurePassword.Length == 0 ? Visibility.Visible : Visibility.Hidden);
+        }
+
+        public static readonly DependencyProperty HintVisibilityProperty = DependencyProperty.RegisterAttached(
+            "HintVisibility", typeof (Visibility), typeof (PasswordField), new PropertyMetadata(default(Visibility)));
+
+        public static void SetHintVisibility(DependencyObject element, Visibility value)
+        {
+            element.SetValue(HintVisibilityProperty, value);
+        }
+
+        public static Visibility GetHintVisibility(DependencyObject element)
+        {
+            return (Visibility) element.GetValue(HintVisibilityProperty);
+        }
+
+        public static void SetManaged(DependencyObject element, PasswordBox value)
+        {
+            element.SetValue(ManagedProperty, value);
+        }
+
+        public static PasswordBox GetManaged(DependencyObject element)
+        {
+            return (PasswordBox) element.GetValue(ManagedProperty);
+        }
+    }
 }
