ca991dfe7721b162ed8fc179060fedf2f56d20e8
James Willock <james@dragablz.net>
2015-08-28 22:29:58 +0100

    tooltips

---
+++ MaterialDesignThemes.Wpf/ToolTipAssist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public static class ToolTipAssist
+    {
+        public static CustomPopupPlacementCallback CustomPopupPlacementCallback => CustomPopupPlacementCallbackImpl;
+
+        public static CustomPopupPlacement[] CustomPopupPlacementCallbackImpl(Size popupSize, Size targetSize, Point offset)
+        {
+            return new []
+            {
+                new CustomPopupPlacement(new Point(targetSize.Width/2 - popupSize.Width/2, targetSize.Height + 14), PopupPrimaryAxis.Horizontal) 
+            };
+        }
+    }
+}
