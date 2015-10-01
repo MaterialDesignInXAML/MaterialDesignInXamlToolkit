b4f1a2b5268e4af5c37dbd6c3821f95e7c36f084
ButchersBoy <james.willock@mulhollandsoftware.com>
2015-10-01 15:11:06 +0100

    remove a bit of redundant code.  dont colour normal border on keyboard focus (let underline do it)

--- MaterialDesignThemes.Wpf/Underline.cs
+++ MaterialDesignThemes.Wpf/Underline.cs
@@ -16,16 +16,6 @@ namespace MaterialDesignThemes.Wpf
             DefaultStyleKeyProperty.OverrideMetadata(typeof(Underline), new FrameworkPropertyMetadata(typeof(Underline)));
         }
 
-        public static readonly DependencyProperty BindIsKeyboardFocusedProperty = DependencyProperty.Register(
-            "BindIsKeyboardFocused", typeof(bool), typeof(Underline),
-            new FrameworkPropertyMetadata(true, FrameworkPropertyMetadataOptions.AffectsRender, BindIsKeyboardFocusedPropertyChangedCallback));
-
-        public bool BindIsKeyboardFocused
-        {
-            get { return (bool)GetValue(BindIsKeyboardFocusedProperty); }
-            set { SetValue(BindIsKeyboardFocusedProperty, value); }
-        }
-
         public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register(
             "IsActive", typeof(bool), typeof(Underline),
             new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsRender));
@@ -34,41 +24,6 @@ namespace MaterialDesignThemes.Wpf
         {
             get { return (bool)GetValue(IsActiveProperty); }
             set { SetValue(IsActiveProperty, value); }
-        }
-
-        private void UpdateBindIsKeyboardFocused()
-        {
-            if (BindIsKeyboardFocused)
-            {
-                BindingOperations.ClearBinding(this, Underline.IsActiveProperty);
-
-                var uiElement = TemplatedParent as UIElement;
-                if (uiElement != null)
-                {
-                    Binding isKeyboardFocusedBinding = new Binding
-                    {
-                        Path = new PropertyPath("IsKeyboardFocused"),
-                        Source = uiElement
-                    };
-                    this.SetBinding(Underline.IsActiveProperty, isKeyboardFocusedBinding);
-                }
-            }
-            else
-            {
-                BindingOperations.ClearBinding(this, Underline.IsActiveProperty);
-            }
-        }
-
-        private static void BindIsKeyboardFocusedPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            (d as Underline)?.UpdateBindIsKeyboardFocused();
-        }
-
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-
-            UpdateBindIsKeyboardFocused();
-        }
+        }        
     }
 }
