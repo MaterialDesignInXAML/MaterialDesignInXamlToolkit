4c97f24f41cd31eeab00c69f45afef01ac651856
ButchersBoy <james.willock@mulhollandsoftware.com>
2015-10-01 09:26:47 +0100

    ickle code tidy [skip ci]

--- MaterialDesignThemes.Wpf/Ripple.cs
+++ MaterialDesignThemes.Wpf/Ripple.cs
@@ -1,21 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Documents.DocumentStructures;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MaterialDesignThemes.Wpf
 {
