d8f5610f09f0f016e0a73a8e55d3b2afdd77baed
Artur <sitdikov.arthur@gmail.com>
2015-10-07 01:20:41 +0300

    Transparent ListBoxItem style

---
+++ MaterialDesignThemes.Wpf/Converters/BrushRoundConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    public class BrushRoundConverter : IValueConverter
+    {
+        public Brush UpValue { get; set; } = Brushes.White;
+
+        public Brush LowValue { get; set; } = Brushes.Black;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var solidColorBrush = value as SolidColorBrush;
+            if (solidColorBrush == null) return Binding.DoNothing;
+
+            Color color = solidColorBrush.Color;
+
+            double brightness = 0.3 * color.R + 0.59 * color.G + 0.11 * color.B;
+
+            return brightness < 123 ? LowValue : UpValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
