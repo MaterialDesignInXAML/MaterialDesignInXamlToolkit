403d227cbbd583672d32ce28e479f920b599229e
ButchersBoy <james.willock@mulhollandsoftware.com>
2015-10-15 09:19:20 +0100

    nuspec stuff for MahApps

--- MaterialDesignThemes.MahApps/Properties/AssemblyInfo.cs
+++ MaterialDesignThemes.MahApps/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
