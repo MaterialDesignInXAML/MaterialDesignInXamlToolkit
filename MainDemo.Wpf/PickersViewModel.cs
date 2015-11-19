a0332887ed9d6f3f513e7585d5308ce02d939c16
James Willock <james@dragablz.net>
2015-11-19 21:10:00 +0000

    illutrate how to combine clock/calendar and dialog host

---
+++ MainDemo.Wpf/PickersViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialDesignColors.WpfExample
+{
+    public class PickersViewModel : INotifyPropertyChanged
+    {
+        private DateTime _date;
+        private DateTime _time;
+
+        public PickersViewModel()
+        {
+            Date = DateTime.Now;
+            Time = DateTime.Now;
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set
+            {
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime Time
+        {
+            get { return _time; }
+            set
+            {
+                _time = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
