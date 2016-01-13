104c129c0230155eeb4d3dd79ad6be059da027db
xaviergonz <xaviergonz@gmail.com>
2016-01-13 17:22:42 +0100

    Custom ink where possible
    Using new RippleAssist.Feedback attached prop

--- MaterialDesignThemes.Wpf/RippleAssist.cs
+++ MaterialDesignThemes.Wpf/RippleAssist.cs
@@ -1,9 +1,25 @@
 using System.Windows;
+using System.Windows.Media;
 
 namespace MaterialDesignThemes.Wpf
 {
     public static class RippleAssist
     {
+        #region Feedback
+
+        public static readonly DependencyProperty FeedbackProperty = DependencyProperty.RegisterAttached(
+            "Feedback", typeof(Brush), typeof(RippleAssist), new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static void SetFeedback(DependencyObject element, Brush value) {
+            element.SetValue(FeedbackProperty, value);
+        }
+
+        public static Brush GetFeedback(DependencyObject element) {
+            return (Brush)element.GetValue(FeedbackProperty);
+        }
+
+        #endregion
+
         #region ClipToBound
 
         public static readonly DependencyProperty ClipToBoundsProperty = DependencyProperty.RegisterAttached(
