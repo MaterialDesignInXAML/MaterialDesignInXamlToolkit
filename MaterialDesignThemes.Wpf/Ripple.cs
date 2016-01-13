2995eede1abf20e05d4bd713b0f11484dd1d574e
xaviergonz <xaviergonz@gmail.com>
2016-01-13 12:53:24 +0100

    Removed unnecessary using

--- MaterialDesignThemes.Wpf/Ripple.cs
+++ MaterialDesignThemes.Wpf/Ripple.cs
@@ -6,7 +6,6 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
-using MaterialDesignThemes.Wpf.Converters.CircularProgressBar;
 
 namespace MaterialDesignThemes.Wpf
 {
