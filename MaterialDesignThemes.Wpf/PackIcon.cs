384326051499614550b1538fc00fbc4cb47ddc32
ButchersBoy <james.willock@mulhollandsoftware.com>
2016-01-14 15:27:28 +0000

    get the icon base from paket/ControlzEx, migrate demo stuff to PackIcon

--- MaterialDesignThemes.Wpf/PackIcon.cs
+++ MaterialDesignThemes.Wpf/PackIcon.cs
@@ -5,66 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using ControlzEx;
 
 namespace MaterialDesignThemes.Wpf
 {
     /// <summary>
     /// Icon from the Material Design Icons project, <see cref="https://materialdesignicons.com/"/>.
     /// </summary>
-    public class PackIcon : Control
-    {
-        private static readonly Lazy<IReadOnlyDictionary<PackIconKind, string>> DataIndex = 
-            new Lazy<IReadOnlyDictionary<PackIconKind, string>>(PackIconDataFactory.Create);
-
+    public class PackIcon : PackIconBase<PackIconKind>
+    {        
         static PackIcon()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(PackIcon), new FrameworkPropertyMetadata(typeof(PackIcon)));
-        }
+        }     
 
-        public static readonly DependencyProperty KindProperty = DependencyProperty.Register(
-            "Kind", typeof (PackIconKind), typeof (PackIcon), new PropertyMetadata(default(PackIconKind)));
-
-        /// <summary>
-        /// Gets or sets the icon to display.
-        /// </summary>
-        public PackIconKind Kind
-        {
-            get { return (PackIconKind) GetValue(KindProperty); }
-            set { SetValue(KindProperty, value); }
-        }
-
-        private static readonly DependencyPropertyKey DataPropertyKey =
-            DependencyProperty.RegisterReadOnly(
-                "Data", typeof (string), typeof (PackIcon),
-                new PropertyMetadata(default(string), PropertyChangedCallback));
-
-        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
-        {
-            ((PackIcon)dependencyObject).UpdateData();
-        }
-
-        public static readonly DependencyProperty DataProperty =
-            DataPropertyKey.DependencyProperty;
-
-        /// <summary>
-        /// Gets the path data for the icon.
-        /// </summary>
-        public string Data
-        {
-            get { return (string) GetValue(DataProperty); }
-            private set { SetValue(DataPropertyKey, value); }
-        }
-
-        public override void OnApplyTemplate()
-        {
-            UpdateData();
-
-            base.OnApplyTemplate();
-        }
-
-        private void UpdateData()
-        {
-            Data = DataIndex.Value[Kind];
-        }        
+        public PackIcon() : base(PackIconDataFactory.Create)
+        { }    
     }
 }
