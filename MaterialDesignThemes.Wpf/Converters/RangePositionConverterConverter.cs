0f0f90d710fb8deaf91d7d3499a323f319f00e76
ButchersBoy <james.willock@mulhollandsoftware.com>
2016-08-25 15:54:32 +0100

    give the converter a more meaningful name

---
+++ MaterialDesignThemes.Wpf/Converters/RangePositionConverterConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    public class RangePositionConverterConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values == null || values.Length != 3 || values[0] == null || values[1] == null || values[2] == null)
+                return Binding.DoNothing;
+
+            double positionAsScaleFactor, lower, upper;
+            if (!double.TryParse(values[0].ToString(), out positionAsScaleFactor)
+                || !double.TryParse(values[1].ToString(), out lower)
+                || !double.TryParse(values[2].ToString(), out upper))
+
+                return Binding.DoNothing;
+
+            var result = upper + (lower - upper)*positionAsScaleFactor;
+
+            System.Diagnostics.Debug.WriteLine($"percantage={positionAsScaleFactor}, from={upper}, to={lower}, result={result}");
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
