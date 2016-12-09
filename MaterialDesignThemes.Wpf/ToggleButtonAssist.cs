a8888e4af6117df14403a201e22b0a082c89cabd
ButchersBoy <james.willock@mulhollandsoftware.com>
2016-12-09 12:20:41 +0000

    complete switch toggle on content support

--- MaterialDesignThemes.Wpf/ToggleButtonAssist.cs
+++ MaterialDesignThemes.Wpf/ToggleButtonAssist.cs
@@ -1,15 +1,43 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls.Primitives;
 
 namespace MaterialDesignThemes.Wpf
 {
     public static class ToggleButtonAssist
     {
+        private static readonly DependencyPropertyKey HasOnContentPropertyKey =
+            DependencyProperty.RegisterAttachedReadOnly(
+                "HasOnContent", typeof(bool), typeof(ToggleButtonAssist),
+                new PropertyMetadata(false));
+
+        public static readonly DependencyProperty HasOnContentProperty = HasOnContentPropertyKey.DependencyProperty;
+
+        private static void SetHasOnContent(DependencyObject element, object value)
+        {
+            element.SetValue(HasOnContentPropertyKey, value);
+        }
+
+        /// <summary>
+        /// Framework use only.
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public static bool GetHasOnContent(DependencyObject element)
+        {
+            return (bool)element.GetValue(HasOnContentProperty);
+        }
+
         /// <summary>
         /// Allows on (IsChecked) content to be provided on supporting <see cref="ToggleButton"/> styles.
         /// </summary>
         public static readonly DependencyProperty OnContentProperty = DependencyProperty.RegisterAttached(
-            "OnContent", typeof (object), typeof (ToggleButtonAssist), new PropertyMetadata(default(object)));
+            "OnContent", typeof (object), typeof (ToggleButtonAssist), new PropertyMetadata(default(object), OnContentPropertyChangedCallback));
+
+        private static void OnContentPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            SetHasOnContent(dependencyObject, dependencyPropertyChangedEventArgs.NewValue != null);
+        }
 
         /// <summary>
         /// Allows on (IsChecked) content to be provided on supporting <see cref="ToggleButton"/> styles.
