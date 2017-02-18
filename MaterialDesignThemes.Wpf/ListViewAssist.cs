c518e190b2f732d7fc681b1d5f2db408167fe7da
Insire <insirethomson@gmail.com>
2017-02-18 13:26:23 +0100

    added Style for ListView and ListViewItem

---
+++ MaterialDesignThemes.Wpf/ListViewAssist.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public static class ListViewAssist
+    {
+        public static readonly DependencyProperty ListViewItemPaddingProperty = DependencyProperty.RegisterAttached(
+            "ListViewItemPadding",
+            typeof(Thickness),
+            typeof(ListViewAssist),
+            new FrameworkPropertyMetadata(new Thickness(13, 8, 8, 8), FrameworkPropertyMetadataOptions.Inherits));
+
+        public static void SetListViewItemPadding(DependencyObject element, Thickness value)
+        {
+            element.SetValue(ListViewItemPaddingProperty, value);
+        }
+
+        public static Thickness GetListViewItemPadding(DependencyObject element)
+        {
+            return (Thickness)element.GetValue(ListViewItemPaddingProperty);
+        }
+    }
+}
