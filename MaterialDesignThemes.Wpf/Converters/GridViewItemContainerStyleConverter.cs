ef9c19d99b79bb5158560a749e540511d6150509
James Willock <james@dragablz.net>
2017-02-28 20:41:52 +0000

    trying to figure out a neat way to manage standard and GridView ListViews

---
+++ MaterialDesignThemes.Wpf/Converters/GridViewItemContainerStyleConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    public class GridViewItemContainerStyleConverter : IValueConverter
+    {
+    }
+}
--- MaterialDesignThemes.Wpf/ListViewAssist.cs
+++ MaterialDesignThemes.Wpf/ListViewAssist.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace MaterialDesignThemes.Wpf
 {
