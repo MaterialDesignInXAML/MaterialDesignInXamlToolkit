0f1c9b7c5b1dcd78d653bbc88e151ac1ae876830
Kevin Bost <kitokeboo@gmail.com>
2017-04-01 23:36:07 -0700

    Added the ability to control which edges the shadows are rendered on. Typically this is not an issue as you can simly lay another control over the top, but there are cases where it would be far simpler to simply have greater control over the shadow.

---
+++ MaterialDesignThemes.Wpf/Converters/ShadowInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media.Effects;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    internal static class ShadowInfo
+    {
+        private static readonly IDictionary<ShadowDepth, DropShadowEffect> ShadowsDictionary;
+
+        static ShadowInfo()
+        {
+            var resourceDictionary = new ResourceDictionary { Source = new Uri("pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Shadows.xaml", UriKind.Absolute) };
+
+            ShadowsDictionary = new Dictionary<ShadowDepth, DropShadowEffect>
+            {
+                { ShadowDepth.Depth0, null },
+                { ShadowDepth.Depth1, (DropShadowEffect)resourceDictionary["MaterialDesignShadowDepth1"] },
+                { ShadowDepth.Depth2, (DropShadowEffect)resourceDictionary["MaterialDesignShadowDepth2"] },
+                { ShadowDepth.Depth3, (DropShadowEffect)resourceDictionary["MaterialDesignShadowDepth3"] },
+                { ShadowDepth.Depth4, (DropShadowEffect)resourceDictionary["MaterialDesignShadowDepth4"] },
+                { ShadowDepth.Depth5, (DropShadowEffect)resourceDictionary["MaterialDesignShadowDepth5"] },
+            };
+        }
+
+        public static DropShadowEffect GetDropShadow(ShadowDepth depth)
+        {
+            return ShadowsDictionary[depth];
+        }
+    }
+}
