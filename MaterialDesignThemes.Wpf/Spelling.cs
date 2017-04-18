7f7826a3379b5b71a4d9da3112fa7794effe4851
Kevin Bost <kitokeboo@gmail.com>
2017-04-18 09:54:21 -0700

    Replaced derived MenuItem classes with keyed styles.

---
+++ MaterialDesignThemes.Wpf/Spelling.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public static class Spelling
+    {
+        public static ResourceKey SpellingSuggestionMenuItemStyleKey { get; } = new ComponentResourceKey(typeof(Spelling), ResourceKeyId.SpellingSuggestionMenuItemStyle);
+        public static ResourceKey SpellingIgnoreAllMenuItemStyleKey { get; } = new ComponentResourceKey(typeof(Spelling), ResourceKeyId.SpellingIgnoreAllMenuItemStyle);
+        public static ResourceKey SpellingNoSuggestionsMenuItemStyleKey { get; } = new ComponentResourceKey(typeof(Spelling), ResourceKeyId.SpellingNoSuggestionsMenuItemStyle);
+        public static ResourceKey SpellingSeparatorStyleKey { get; } = new ComponentResourceKey(typeof(Spelling), ResourceKeyId.SpellingSeparatorStyle);
+    }
+
+    internal enum ResourceKeyId
+    {
+        SpellingSuggestionMenuItemStyle,
+        SpellingIgnoreAllMenuItemStyle,
+        SpellingNoSuggestionsMenuItemStyle,
+        SpellingSeparatorStyle,
+    }
+}
--- MaterialDesignThemes.Wpf/SpellingNoSuggestionsMenuItem.cs
+++
@@ -1,7 +0,0 @@
-using System.Windows.Controls;
-
-namespace MaterialDesignThemes.Wpf
-{
-    public class SpellingNoSuggestionsMenuItem : MenuItem
-    { }
-}
