455a3199b2d127def2918653c0f921363000a62b
WJH <hou32hou@gmail.com>
2017-07-23 18:02:49 +0800

    Added guideline source for each type of control - so that user can easily refer to the original guidline posted by Google

--- MainDemo.Wpf/Domain/MainWindowViewModel.cs
+++ MainDemo.Wpf/Domain/MainWindowViewModel.cs
@@ -17,7 +17,8 @@ namespace MaterialDesignColors.WpfExample.Domain
                     new []
                     {
                         new DocumentationLink(DocumentationLinkType.Wiki, $"{ConfigurationManager.AppSettings["GitHub"]}/wiki", "WIKI"),
-                        DocumentationLink.DemoPageLink<Home>()
+                        DocumentationLink.DemoPageLink<Home>(),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/#"), 
                     }
                 ),
                 new DemoItem("Palette", new PaletteSelector { DataContext = new PaletteSelectorViewModel() },
@@ -28,9 +29,10 @@ namespace MaterialDesignColors.WpfExample.Domain
                         DocumentationLink.WikiLink("Swatches-and-Recommended-Colors", "Swatches"),
                         DocumentationLink.DemoPageLink<PaletteSelector>("Demo View"),
                         DocumentationLink.DemoPageLink<PaletteSelectorViewModel>("Demo View Model"),
-                        DocumentationLink.ApiLink<PaletteHelper>()
+                        DocumentationLink.ApiLink<PaletteHelper>(),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/style/color.html#color-color-palette"), 
                     }),
-                                new DemoItem("Buttons & Toggles", new Buttons(),
+                new DemoItem("Buttons & Toggles", new Buttons(),
                     new []
                     {
                         DocumentationLink.WikiLink("Button-Styles", "Buttons"),
@@ -39,7 +41,8 @@ namespace MaterialDesignColors.WpfExample.Domain
                         DocumentationLink.StyleLink("CheckBox"),
                         DocumentationLink.StyleLink("PopupBox"),
                         DocumentationLink.StyleLink("ToggleButton"),
-                        DocumentationLink.ApiLink<PopupBox>()
+                        DocumentationLink.ApiLink<PopupBox>(),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/components/buttons.html#"), 
                     })
                     {
                         VerticalScrollBarVisibilityRequirement = ScrollBarVisibility.Auto
@@ -50,6 +53,7 @@ namespace MaterialDesignColors.WpfExample.Domain
                         DocumentationLink.DemoPageLink<TextFields>(),
                         DocumentationLink.StyleLink("TextBox"),
                         DocumentationLink.StyleLink("ComboBox"),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/components/text-fields.html#"), 
                     })
                     {
                         VerticalScrollBarVisibilityRequirement = ScrollBarVisibility.Auto
@@ -60,23 +64,27 @@ namespace MaterialDesignColors.WpfExample.Domain
                         DocumentationLink.DemoPageLink<TextFields>(),
                         DocumentationLink.StyleLink("Clock"),
                         DocumentationLink.StyleLink("DatePicker"),
-                        DocumentationLink.ApiLink<TimePicker>()
+                        DocumentationLink.ApiLink<TimePicker>(),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/components/pickers.html"), 
                     }),
                 new DemoItem("Sliders", new Sliders(), new []
                     {
                         DocumentationLink.DemoPageLink<Sliders>(),
-                        DocumentationLink.StyleLink("Sliders")
+                        DocumentationLink.StyleLink("Sliders"),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/components/sliders.html#"), 
                     }),
                 new DemoItem("Chips", new Chips(), new []
                     {
                         DocumentationLink.DemoPageLink<Chips>(),
                         DocumentationLink.StyleLink("Chip"),
-                        DocumentationLink.ApiLink<Chip>()
+                        DocumentationLink.ApiLink<Chip>(),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/components/chips.html#chips-usage"), 
                     }),
                 new DemoItem("Typography", new Typography(), new []
                     {
                         DocumentationLink.DemoPageLink<Typography>(),
-                        DocumentationLink.StyleLink("TextBlock")
+                        DocumentationLink.StyleLink("TextBlock"),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/style/typography.html#typography-styles")
                     })
                     {
                         VerticalScrollBarVisibilityRequirement = ScrollBarVisibility.Auto,
@@ -86,7 +94,8 @@ namespace MaterialDesignColors.WpfExample.Domain
                     {
                         DocumentationLink.DemoPageLink<Cards>(),
                         DocumentationLink.StyleLink("Card"),
-                        DocumentationLink.ApiLink<Card>()
+                        DocumentationLink.ApiLink<Card>(),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/components/cards.html"), 
                     })
                 {
                     VerticalScrollBarVisibilityRequirement = ScrollBarVisibility.Auto
@@ -133,7 +142,8 @@ namespace MaterialDesignColors.WpfExample.Domain
                     new []
                     {
                         DocumentationLink.DemoPageLink<Expander>(),
-                        DocumentationLink.StyleLink("Expander")
+                        DocumentationLink.StyleLink("Expander"),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/components/expansion-panels.html#"), 
                     }),
                 new DemoItem("Group Boxes", new GroupBoxes(),
                     new []
@@ -146,13 +156,15 @@ namespace MaterialDesignColors.WpfExample.Domain
                     {
                         DocumentationLink.DemoPageLink<MenusAndToolBars>(),
                         DocumentationLink.StyleLink("Menu"),
-                        DocumentationLink.StyleLink("ToolBar")
+                        DocumentationLink.StyleLink("ToolBar"),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/components/menus.html#menus-usage"), 
                     }),
                 new DemoItem("Progress Indicators", new Progress(),
                     new []
                     {
                         DocumentationLink.DemoPageLink<Progress>(),
-                        DocumentationLink.StyleLink("ProgressBar")
+                        DocumentationLink.StyleLink("ProgressBar"),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/components/progress-activity.html#progress-activity-types-of-indicators"), 
                     }),
                 new DemoItem("Dialogs", new Dialogs { DataContext = new DialogsViewModel()},
                     new []
@@ -160,13 +172,15 @@ namespace MaterialDesignColors.WpfExample.Domain
                         DocumentationLink.WikiLink("Dialogs", "Dialogs"),
                         DocumentationLink.DemoPageLink<Dialogs>("Demo View"),
                         DocumentationLink.DemoPageLink<DialogsViewModel>("Demo View Model"),
-                        DocumentationLink.ApiLink<DialogHost>()
+                        DocumentationLink.ApiLink<DialogHost>(),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/components/dialogs.html#"), 
                     }),
                 new DemoItem("Drawer", new Drawers(),
                     new []
                     {
                         DocumentationLink.DemoPageLink<Drawers>(),
-                        DocumentationLink.ApiLink<DrawerHost>()
+                        DocumentationLink.ApiLink<DrawerHost>(),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/patterns/navigation-drawer.html#"), 
                     }),
                 new DemoItem("Snackbar", new Snackbars(),
                     new []
@@ -175,7 +189,8 @@ namespace MaterialDesignColors.WpfExample.Domain
                         DocumentationLink.DemoPageLink<Snackbars>(),
                         DocumentationLink.StyleLink("Snackbar"),
                         DocumentationLink.ApiLink<Snackbar>(),
-                        DocumentationLink.ApiLink<ISnackbarMessageQueue>()
+                        DocumentationLink.ApiLink<ISnackbarMessageQueue>(),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/components/snackbars-toasts.html#"), 
                     }),
                 new DemoItem("Transitions", new Transitions(),
                     new []
@@ -185,11 +200,13 @@ namespace MaterialDesignColors.WpfExample.Domain
                         DocumentationLink.ApiLink<Transitioner>("Transitions"),
                         DocumentationLink.ApiLink<TransitionerSlide>("Transitions"),
                         DocumentationLink.ApiLink<TransitioningContent>("Transitions"),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/motion/choreography.html#choreography-radial-reaction"), 
                     }),
                 new DemoItem("Shadows", new Shadows(),
                     new []
                     {
                         DocumentationLink.DemoPageLink<Shadows>(),
+                        DocumentationLink.GuidelineLink("https://material.io/guidelines/material-design/elevation-shadows.html#"), 
                     }),
             };
         }
