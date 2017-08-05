3af623d4fcfae295e300b4466d3cd57ac2123b97
WJH <hou32hou@gmail.com>
2017-08-05 22:35:39 +0800

    Successfully injected XamlDisplayerPanel into Dialogs.xaml

--- MainDemo.Wpf/Dialogs.xaml.cs
+++ MainDemo.Wpf/Dialogs.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MaterialDesignDemo.Helper;
 using MaterialDesignThemes.Wpf;
 
 namespace MaterialDesignColors.WpfExample
@@ -24,6 +25,7 @@ namespace MaterialDesignColors.WpfExample
         public Dialogs()
         {
             InitializeComponent();
+			XamlDisplayerPanel.Initialize(new SourceRouter(this.GetType().Name).GetSource());
         }
 
         private void Sample1_DialogHost_OnDialogClosing(object sender, DialogClosingEventArgs eventArgs)
