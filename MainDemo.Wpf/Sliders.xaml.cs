3291c19d669bbf53788a3aab339d2d4593918a26
WJH <hou32hou@gmail.com>
2017-08-13 22:35:16 +0800

    Removed all x:Name attributes from all XamlDisplayerPanel - because they are not needed anymore

--- MainDemo.Wpf/Sliders.xaml.cs
+++ MainDemo.Wpf/Sliders.xaml.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -14,7 +12,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
-using CodeDisplayer;
 
 namespace MaterialDesignColors.WpfExample
 {
@@ -26,16 +23,6 @@ namespace MaterialDesignColors.WpfExample
         public Sliders()
         {
             InitializeComponent();          
-        }
-
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
-            var b = sender as ToggleButton;
-            if (b.IsChecked.Value) {
-                XamlDisplayerPanel.DisplayMode = XamlDisplayer.DisplayModeEnum.LeftRight;
-            }
-            else {
-                XamlDisplayerPanel.DisplayMode = XamlDisplayer.DisplayModeEnum.TopBottom;
-            }
         }
     }
 }
