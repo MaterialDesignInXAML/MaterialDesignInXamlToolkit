2c35efc799a04426998dd9c7c076f26bc7b01643
WJH <hou32hou@gmail.com>
2017-09-04 15:56:40 +0800

    Completing the Demo for DrawerHost with new OpenMode

--- MainDemo.Wpf/Drawers.xaml.cs
+++ MainDemo.Wpf/Drawers.xaml.cs
@@ -1,4 +1,19 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
 
 namespace MaterialDesignDemo
 {
@@ -10,6 +25,21 @@ namespace MaterialDesignDemo
         public Drawers()
         {
             InitializeComponent();
+        }
+
+        private void StandardRadioButton_OnChecked(object sender, RoutedEventArgs e) {
+            DrawerHost.OpenMode = DrawerHost.DrawerHostOpenMode.Standard;
+            OpenAllDrawerButton.IsEnabled = true;
+        }
+
+        private void StaysOpenRadioButton_OnChecked(object sender, RoutedEventArgs e) {
+            DrawerHost.OpenMode = DrawerHost.DrawerHostOpenMode.StaysOpen;
+            OpenAllDrawerButton.IsEnabled = true;
+        }
+
+        private void PinnedRadioButton_OnChecked(object sender , RoutedEventArgs e) {
+            DrawerHost.OpenMode = DrawerHost.DrawerHostOpenMode.Pinned;
+            OpenAllDrawerButton.IsEnabled = false;
         }
     }
 }
