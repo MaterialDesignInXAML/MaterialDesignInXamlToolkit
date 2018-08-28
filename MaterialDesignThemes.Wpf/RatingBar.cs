c8ea8f61f31ea56ca093d7342adecffee36a2e1a
Mathivanan Palanisamy <15025473+dhilmathy@users.noreply.github.com>
2018-08-28 12:23:46 +0100

    Removed un-used using statements

--- MaterialDesignThemes.Wpf/RatingBar.cs
+++ MaterialDesignThemes.Wpf/RatingBar.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Input;
 
 namespace MaterialDesignThemes.Wpf
