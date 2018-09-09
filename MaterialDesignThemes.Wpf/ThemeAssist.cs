569ff4255a70e85eebd1740471b5281b6eec521e
Kevin B <Keboo@users.noreply.github.com>
2018-09-09 00:25:54 -0700

    Created a new ThemeAssist class that allows for easy overriding of the color themes. (#1036)
    Added a new property to the DialogHost for setting the theme color of the dialog. Defaulted the normal dialog host to light to maintain backward compatibility.

---
+++ MaterialDesignThemes.Wpf/ThemeAssist.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public static class ThemeAssist
+    {
+        public static BaseTheme GetTheme(DependencyObject obj)
+        {
+            return (BaseTheme)obj.GetValue(ThemeProperty);
+        }
+
+        public static void SetTheme(DependencyObject obj, BaseTheme value)
+        {
+            obj.SetValue(ThemeProperty, value);
+        }
+
+        public static readonly DependencyProperty ThemeProperty =
+            DependencyProperty.RegisterAttached("Theme", typeof(BaseTheme), typeof(ThemeAssist), new PropertyMetadata(default(BaseTheme), OnThemeChanged));
+
+        private static void OnThemeChanged(DependencyObject @do, DependencyPropertyChangedEventArgs e)
+        {
+            if (@do is FrameworkElement element)
+            {
+                if (e.OldValue is BaseTheme oldTheme && 
+                    GetResourceDictionarySource(oldTheme) is string oldSource)
+                {
+                    foreach(ResourceDictionary resourceDictionary in element.Resources.MergedDictionaries)
+                    {
+                        if (string.Equals(resourceDictionary.Source.ToString(), oldSource, StringComparison.Ordinal))
+                        {
+                            element.Resources.MergedDictionaries.Remove(resourceDictionary);
+                            break;
+                        }
+                    }
+                }
+
+                if (e.NewValue is BaseTheme newTheme &&
+                    GetResourceDictionarySource(newTheme) is string newThemeSource)
+                {
+                    element.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri(newThemeSource) });
+                }
+            }
+        }
+
+        private static string GetResourceDictionarySource(BaseTheme theme)
+        {
+            switch (theme)
+            {
+                case BaseTheme.Light:
+                    return "pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Light.xaml";
+                case BaseTheme.Dark:
+                    return "pack://application:,,,/MaterialDesignThemes.Wpf;component/Themes/MaterialDesignTheme.Dark.xaml";
+            }
+            return null;
+        }
+    }
+}
