2fcbeed067afc5c34f37319d69a1874250ebecda
Robert Snipe <bobblezor@gmail.com>
2018-10-07 12:46:03 +0100

    WIP color tool impl

---
+++ MaterialDesignThemes.Wpf/Converters/BooleanToZIndexConverter.cs
@@ -0,0 +1,7 @@
+﻿namespace MaterialDesignThemes.Wpf.Converters
+{
+    public sealed class BooleanToZIndexConverter : BooleanConverter<int>
+    {
+        public BooleanToZIndexConverter() : base(2, 0) { }
+    }
+}
