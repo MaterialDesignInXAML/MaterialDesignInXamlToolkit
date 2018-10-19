cdb1245f7d9c20cb19981475ed2a0417c9d10a8e
Kevin B <Keboo@users.noreply.github.com>
2018-10-19 11:35:01 -0700

    Updating icons. (#1076)
    Adding support for icon aliases
    Updated resource generator to handle different versions of TLS
    Updated demo app to only show distinct enum values.

---
+++ mdresgen/Icon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace mdresgen
+{
+    internal class Icon
+    {
+        public Icon(string name, string data, IList<string> aliases)
+        {
+            Name = name;
+            Data = data;
+            Aliases = aliases ?? new List<string>();
+        }
+
+        public string Name { get; }
+        public string Data { get; }
+        public IList<string> Aliases { get; }
+    }
+}
