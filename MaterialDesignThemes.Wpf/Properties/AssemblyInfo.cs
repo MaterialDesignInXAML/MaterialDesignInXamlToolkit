e91a2675762e31187938e435895485014dbb3cf9
Kevin Bost <kevin@intellitect.com>
2019-01-20 00:09:30 -0800

    Better versioning of the individual packages so the build only needs to run once.

--- MaterialDesignThemes.Wpf/Properties/AssemblyInfo.cs
+++ MaterialDesignThemes.Wpf/Properties/AssemblyInfo.cs
@@ -53,4 +53,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.*")]
+
+//These will be replaced with the MDIX_CORE_VERSION property 
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.*")]
