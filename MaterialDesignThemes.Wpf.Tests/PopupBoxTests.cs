331650b1421abe09d7b66e8649bd49803d656fbd
Kevin Bost <kitokeboo@gmail.com>
2019-03-17 12:56:14 -0700

    Fixes #1091 Toggle button inside of the popup box template should inherit the PopupBox's TabIndex

---
+++ MaterialDesignThemes.Wpf.Tests/PopupBoxTests.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using Xunit;
+
+namespace MaterialDesignThemes.Wpf.Tests
+{
+    public class PopupBoxTests
+    {
+        [StaFact]
+        [Description("Issue 1091")]
+        public void ToggleButtonInheritsTabIndex()
+        {
+            var popupBox = new PopupBox { TabIndex = 3 };
+            popupBox.ApplyDefaultStyle();
+
+            ToggleButton togglePart = popupBox.FindVisualChild<ToggleButton>(PopupBox.TogglePartName);
+            
+            Assert.Equal(3, togglePart.TabIndex);
+        }
+    }
+}
