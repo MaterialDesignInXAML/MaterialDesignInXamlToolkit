0fac92913247980e3465497e355fda7501bb5970
Albert Alonso <38870744+AlbertAlonso@users.noreply.github.com>
2019-06-05 17:59:23 +0200

    Added New Scrollbar Style

---
+++ MaterialDesignThemes.Wpf/ScrollbarAssist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public static class ScrollBarAssist
+    {
+
+        public static readonly DependencyProperty ArrowsVisibilityProperty =
+            DependencyProperty.RegisterAttached("ArrowsVisibility", typeof(Visibility), typeof(ScrollBarAssist), new PropertyMetadata(Visibility.Visible));
+
+        public static void SetArrowsVisibility(DependencyObject element, Visibility value)
+        {
+            element.SetValue(ArrowsVisibilityProperty, value);
+        }
+
+        public static Visibility GetArrowsVisibility(DependencyObject element)
+        {
+            return (Visibility)element.GetValue(ArrowsVisibilityProperty);
+        }
+    }
+}
