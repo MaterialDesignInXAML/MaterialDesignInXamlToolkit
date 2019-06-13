845632191557e80f7067d334605e0e9b173959b3
alexander <koutinho@yandex.ru>
2019-06-13 22:15:40 +0300

    ComboBoxItemToNormalVisualStateTransitionDuration added
    ComboBoxItemToNormalVisualStateTransitionDuration dependency property has been added to the ComboBoxAssist

--- MaterialDesignThemes.Wpf/ComboBoxAssist.cs
+++ MaterialDesignThemes.Wpf/ComboBoxAssist.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace MaterialDesignThemes.Wpf
 {
@@ -44,6 +45,42 @@ namespace MaterialDesignThemes.Wpf
         public static void SetShowSelectedItem(DependencyObject element, object value)
         {
             element.SetValue(ShowSelectedItemProperty, value);
+        }
+
+        /// <summary>
+        /// The transition duration of a ComboBoxItem from the selected/mouse over state to the normal state
+        /// </summary>
+        public static readonly DependencyProperty ComboBoxItemToNormalVisualStateTransitionDurationProperty = DependencyProperty.RegisterAttached(
+            "ComboBoxItemToNormalVisualStateTransitionDuration",
+            typeof(Duration),
+            typeof(ComboBoxAssist),
+            new FrameworkPropertyMetadata(new Duration(TimeSpan.FromMilliseconds(0)),
+                FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.Inherits, VisualTransitionDurationChanged));
+
+        public static Duration GetComboBoxItemToNormalVisualStateTransitionDuration(DependencyObject element)
+        {
+            return (Duration)element.GetValue(ComboBoxItemToNormalVisualStateTransitionDurationProperty);
+        }
+
+        public static void SetComboBoxItemToNormalVisualStateTransitionDuration(DependencyObject element, object value)
+        {
+            // Set the value of the GeneratedDuration property if a target element is VisualTransition
+            VisualTransition visualTransition = element as VisualTransition;
+            if (visualTransition != null)
+            {
+                visualTransition.GeneratedDuration = (Duration)value;
+            }
+
+            element.SetValue(ComboBoxItemToNormalVisualStateTransitionDurationProperty, value);
+        }
+
+        private static void VisualTransitionDurationChanged(DependencyObject element, DependencyPropertyChangedEventArgs e)
+        {
+            // Update the value of the GeneratedDuration property with new value if a target element is VisualTransition
+            if (element is VisualTransition)
+            {
+                (element as VisualTransition).GeneratedDuration = (Duration)e.NewValue;
+            }
         }
     }
 }
