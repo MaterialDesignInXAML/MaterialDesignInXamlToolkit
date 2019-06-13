23bd3f8b4be64b786035fb2601d61b53ed577bd4
alexander <koutinho@yandex.ru>
2019-06-13 21:57:46 +0300

    Binding proxy class has been added

---
+++ MaterialDesignThemes.Wpf/BindingProxy .cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MaterialDesignThemes.Wpf
+{
+    /// <summary>
+    /// The proxy class is used to hand the DataContext of an upper
+    /// element down to the elements in or outside of the visual tree.
+    /// Please refer to the url for more info: https://stackoverflow.com/questions/42487517/bind-to-controls-property-inside-visualstatemanager
+    /// </summary>
+    public class BindingProxy : Freezable
+    {
+        #region Overrides of Freezable
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        #endregion
+
+        public object Data
+        {
+            get { return (object)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(object),
+                                         typeof(BindingProxy));
+    }
+}
