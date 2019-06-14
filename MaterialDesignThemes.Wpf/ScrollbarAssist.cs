69fcda13692aaf4e4d40ca4d5bcfd7de035f49c1
Kevin Bost <kitokeboo@gmail.com>
2019-06-13 22:47:37 -0700

    Cleaned up scroll bar style
    Moved back to only having a single style and toggling values with attached properties.
    Added attached properties for thumb corner radius, height, and width.
    Renamed minimal style to match other styles in the library.

--- MaterialDesignThemes.Wpf/ScrollbarAssist.cs
+++ MaterialDesignThemes.Wpf/ScrollbarAssist.cs
@@ -1,26 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace MaterialDesignThemes.Wpf
 {
     public static class ScrollBarAssist
     {
+        public static readonly DependencyProperty ButtonsVisibilityProperty =
+            DependencyProperty.RegisterAttached("ButtonsVisibility", typeof(Visibility), typeof(ScrollBarAssist), new PropertyMetadata(Visibility.Visible));
 
-        public static readonly DependencyProperty ArrowsVisibilityProperty =
-            DependencyProperty.RegisterAttached("ArrowsVisibility", typeof(Visibility), typeof(ScrollBarAssist), new PropertyMetadata(Visibility.Visible));
-
-        public static void SetArrowsVisibility(DependencyObject element, Visibility value)
+        public static void SetButtonsVisibility(DependencyObject element, Visibility value)
         {
-            element.SetValue(ArrowsVisibilityProperty, value);
+            element.SetValue(ButtonsVisibilityProperty, value);
         }
 
-        public static Visibility GetArrowsVisibility(DependencyObject element)
+        public static Visibility GetButtonsVisibility(DependencyObject element)
         {
-            return (Visibility)element.GetValue(ArrowsVisibilityProperty);
+            return (Visibility)element.GetValue(ButtonsVisibilityProperty);
+        }
+
+        public static readonly DependencyProperty ThumbCornerRadiusProperty = DependencyProperty.RegisterAttached(
+            "ThumbCornerRadius", typeof(CornerRadius), typeof(ScrollBarAssist), new PropertyMetadata(default(CornerRadius)));
+
+        public static void SetThumbCornerRadius(DependencyObject element, CornerRadius value)
+        {
+            element.SetValue(ThumbCornerRadiusProperty, value);
+        }
+
+        public static CornerRadius GetThumbCornerRadius(DependencyObject element)
+        {
+            return (CornerRadius) element.GetValue(ThumbCornerRadiusProperty);
+        }
+
+        public static readonly DependencyProperty ThumbWidthProperty = DependencyProperty.RegisterAttached(
+            "ThumbWidth", typeof(double), typeof(ScrollBarAssist), new PropertyMetadata(double.NaN));
+
+        public static void SetThumbWidth(DependencyObject element, double value)
+        {
+            element.SetValue(ThumbWidthProperty, value);
+        }
+
+        public static double GetThumbWidth(DependencyObject element)
+        {
+            return (double) element.GetValue(ThumbWidthProperty);
+        }
+
+        public static readonly DependencyProperty ThumbHeightProperty = DependencyProperty.RegisterAttached(
+            "ThumbHeight", typeof(double), typeof(ScrollBarAssist), new PropertyMetadata(double.NaN));
+
+        public static void SetThumbHeight(DependencyObject element, double value)
+        {
+            element.SetValue(ThumbHeightProperty, value);
+        }
+
+        public static double GetThumbHeight(DependencyObject element)
+        {
+            return (double) element.GetValue(ThumbHeightProperty);
         }
     }
 }
