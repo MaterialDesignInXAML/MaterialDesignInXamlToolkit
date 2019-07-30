b0466887b339b8c6e9672d33edca666d7b7649fa
Mathivanan Palanisamy <mathy@outlook.com>
2019-07-30 09:54:53 +0530

    Null Reference bug fix. Fixes #1332

--- MaterialDesignThemes.Wpf/PaletteHelper.cs
+++ MaterialDesignThemes.Wpf/PaletteHelper.cs
@@ -165,11 +165,18 @@ namespace MaterialDesignThemes.Wpf
             Application.Current.Resources.SetSolidColorBrush(name, color);
         }
 
-        public virtual ITheme GetTheme() => Application.Current.Resources.GetTheme();
+        public virtual ITheme GetTheme()
+        {
+            if (Application.Current == null)
+                throw new InvalidOperationException("Cannot get theme outside of a WPF application. Use ResourceDictionaryExtensions.GetTheme on the appropriate resource dictionary instead.");
+            return Application.Current.Resources.GetTheme();
+        }
 
         public virtual void SetTheme(ITheme theme)
         {
             if (theme == null) throw new ArgumentNullException(nameof(theme));
+            if (Application.Current == null)
+                throw new InvalidOperationException("Cannot set theme outside of a WPF application. Use ResourceDictionaryExtensions.SetTheme on the appropriate resource dictionary instead.");
             Application.Current.Resources.SetTheme(theme);
         }
     }
