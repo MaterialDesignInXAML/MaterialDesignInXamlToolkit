d8a424184ec8550344f50f154869035db88de612
Andrew Smolak <awsmolak@gmail.com>
2019-08-08 10:40:00 -0600

    fixes https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit/issues/1352
    Fixed BundledTheme and CustomColorTheme to work with XAML designer. Changed them to derive from ResourceDictionary instead of MarkupExtension. It does not appear that MarkupExtensions providing ResourceDictionaries can be merged during design time.

--- MaterialDesignThemes.Wpf/ThemeMarkupExtension.cs
+++ MaterialDesignThemes.Wpf/ThemeMarkupExtension.cs
@@ -25,7 +25,7 @@ namespace MaterialDesignThemes.Wpf
     }
 
     //TODO: Naming of this?
-    public class BundledTheme : ThemeMarkupExtension
+    public class BundledThemeMarkupExtension : ThemeMarkupExtension
     {
         public BaseTheme BaseTheme { get; set; }
         public PrimaryColor PrimaryColor { get; set; }
@@ -39,7 +39,57 @@ namespace MaterialDesignThemes.Wpf
         }
     }
 
-    public class CustomColorTheme : ThemeMarkupExtension
+    //Implementation of BundledTheme that works during design time
+    //Markup extensions to provide ResourceDictionary cause XAML designer exception
+    public class BundledTheme : ResourceDictionary
+    {
+        private BaseTheme _baseTheme;
+        public BaseTheme BaseTheme
+        {
+            get { return _baseTheme; }
+            set
+            {
+                _baseTheme = value;
+                SetTheme();
+            }
+        }
+
+        private PrimaryColor _primaryColor;
+
+        public PrimaryColor PrimaryColor 
+        {
+            get { return _primaryColor; }
+            set
+            {
+                _primaryColor = value;
+                SetTheme();
+            }
+        }
+
+        private SecondaryColor _secondaryColor;
+        public SecondaryColor SecondaryColor
+        {
+            get { return _secondaryColor; }
+            set
+            {
+                _secondaryColor = value;
+                SetTheme();
+            }
+        }
+
+        private void SetTheme()
+        {
+            var theme = Theme.Create(BaseTheme.GetBaseTheme(),
+            SwatchHelper.Lookup[(MaterialDesignColor)PrimaryColor],
+            SwatchHelper.Lookup[(MaterialDesignColor)SecondaryColor]);
+
+            this.SetTheme(theme);
+        }
+
+        public BundledTheme() { }
+    }
+
+    public class CustomColorThemeMarkupExtension : ThemeMarkupExtension
     {
         public BaseTheme BaseTheme { get; set; }
         public Color Primary { get; set; }
@@ -48,6 +98,57 @@ namespace MaterialDesignThemes.Wpf
         protected override ITheme GetTheme()
         {
             return Theme.Create(BaseTheme.GetBaseTheme(), Primary, Secondary);
+        }
+    }
+
+    //Implementation of CustomColorTheme that works during design time
+    //Markup extensions to provide ResourceDictionary cause XAML designer exception
+    public class CustomColorTheme : ResourceDictionary
+    {
+        private BaseTheme _baseTheme;
+        public BaseTheme BaseTheme
+        {
+            get { return _baseTheme; }
+            set
+            {
+                _baseTheme = value;
+                TrySetTheme();
+            }
+        }
+
+        private Color _primary;
+        public Color Primary
+        {
+            get { return _primary; }
+            set
+            {
+                _primary = value;
+                TrySetTheme();
+            }
+        }
+
+        private Color _secondary;
+        public Color Secondary
+        {
+            get { return _secondary; }
+            set
+            {
+                _secondary = value;
+                TrySetTheme();
+            }
+        }
+
+        private void TrySetTheme()
+        {
+            if(Primary != null && Secondary != null)
+            {
+                var theme = Theme.Create(BaseTheme.GetBaseTheme(), Primary, Secondary);
+                this.SetTheme(theme);
+            }
+        }
+
+        public CustomColorTheme()
+        {
         }
     }
 }
