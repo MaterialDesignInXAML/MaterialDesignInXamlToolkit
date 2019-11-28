9d6dc34bc0e414557c0ed408b2c89afa3b5692f0
Kevin Bost <kitokeboo@gmail.com>
2019-11-28 00:50:15 -0800

    Fixes #746
    Allowing the option for projects to opt-into having the font files copied locally rather than used as an embedded resource.
    No longer setting font for DailogHost
    No longer setting font for Toolbar.

---
+++ MaterialDesignThemes.Wpf/MaterialDesignFonts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public static class MaterialDesignFonts
+    {
+        private static readonly Lazy<FontFamily> _roboto 
+            = new Lazy<FontFamily>(LoadRobotoFontFamily);
+
+        private static FontFamily LoadRobotoFontFamily()
+        {
+            string fontDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Roboto");
+            return new FontFamily(new Uri($"file:///{fontDirectory}"), "./#Roboto");
+        }
+
+        public static FontFamily Roboto => _roboto.Value;
+    }
+}
