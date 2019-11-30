d8c9b7e6948a1a5a46b729136edf78137eaa14f3
Kevin Bost <kitokeboo@gmail.com>
2019-11-30 15:38:29 -0800

    Script and nuspec updates to handle the icons

--- BuildNugets.ps1
+++ BuildNugets.ps1
@@ -2,36 +2,31 @@ $version = "1.2.3"
 $copywrite = "Copyright 2019 James Willock/Mulholland Software Ltd"
 $configuration = "Release"
 
-$iconUrl = "http://materialdesigninxaml.net/images/MD4XAML32.png"
-$nugetIconFile = [System.IO.Path]::GetFullPath("NugetIcon.png")
-
-
 function Update-Icon {
   param (
     [string]$Path
   )
-  
+  $Path = Resolve-Path $Path
   [xml] $xml = Get-Content $Path
   [string] $iconUrl = $xml.package.metadata.iconUrl;
   if (![string]::IsNullOrWhiteSpace($iconUrl) -and [string]::IsNullOrWhiteSpace($xml.package.metadata.icon)) {
     $nugetIconFile = "$($xml.package.metadata.id).Icon.png";
     Invoke-WebRequest $iconUrl -OutFile "$nugetIconFile"
-    [xml] $files = $xml.package.files;
-    $iconFile = $files.CreateElement("file");
-    $iconFile.target = "images\";
-    $iconFile.src = "$nugetIconFile"
-    $xml.package.metadata.icon = "images\icon.png"
+    $files = $xml.SelectSingleNode("/package/files")
+    $iconFile = $xml.CreateElement("file")
+    $iconFile.SetAttribute("src", "$nugetIconFile")
+    $iconFile.SetAttribute("target", "images\")
+    $files.AppendChild($iconFile) | Out-Null
+
+    $iconElement = $xml.CreateElement("icon")
+    $iconElement.InnerText = "images\$nugetIconFile"
+    $xml.package.metadata.AppendChild($iconElement) | Out-Null
   }
   $xml.Save($Path)
 }
 
 
-#Push-Location ".."
-
 Update-Icon .\MaterialDesignColors.nuspec 
 
-#nuget pack .\MaterialDesignColors.nuspec -version "$version" -Properties "Configuration=$configuration;Copywrite=$copywrite"
+nuget pack .\MaterialDesignColors.nuspec -version "$version" -Properties "Configuration=$configuration;Copywrite=$copywrite"
 
-
-
-#Pop-Location
