c1c328b2290142ad46c2dc5d43039f3805a0baec
Jesus Bamford <Jesus805@users.noreply.github.com>
2019-12-04 21:38:41 -0800

    Custom Thumb Control and ControlTemplate to fix issue #1531 (#1542)
    * Created Custom Thumb to override the cursor when dragging. Created ControlTemplate for Thumb that sets cursor to SizeWE
    
    * Implemented code review suggested changes
    
    GridViewColumnThumb is now internal.
    Created a new style explicitly for the GridView Column.
    GridViewColumnHeader now uses this new style.

---
+++ MaterialDesignThemes.Wpf/GridViewColumnThumb.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace MaterialDesignThemes.Wpf
+{
+    internal class GridViewColumnThumb : Thumb
+    {
+        public GridViewColumnThumb() : base() { }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.SizeWE;
+            base.OnMouseLeftButtonDown(e);
+        }
+
+        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonUp(e);
+            Mouse.OverrideCursor = null;
+        }
+    }
+}
