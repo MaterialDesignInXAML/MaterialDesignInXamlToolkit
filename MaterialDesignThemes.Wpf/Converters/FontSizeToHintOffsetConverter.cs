0ca05ec0c00fa81aaefd12bd792ae2e43b5e57f4
AmRo <VirtualEmailBox045@Gmail.Com>
2019-12-27 12:22:44 +0330

    Create FontSizeToHintOffset converter

---
+++ MaterialDesignThemes.Wpf/Converters/FontSizeToHintOffsetConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    public class FontSizeToHintOffsetConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fontSize = System.Convert.ToDouble(value);
+            var hintOffset = fontSize / 2 + 12;
+            return new Point(0, -hintOffset);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
