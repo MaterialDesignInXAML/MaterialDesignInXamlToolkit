a39f19e204ace638e8091559e0624a7592a5122a
Ahmed Mohamed <ahmed_master23@hotmail.com>
2020-01-31 07:18:08 +0200

    Add Custom mode to ColorZone (#1611)
    * Add Custom to ColorZone Modes
    
    * Add Custom to ColorZone Modes
    
    * Move the Custom Background and Foreground from the ColorZone to the ColorZone assist class and remvoe Custom from their names
    
    Add Custom GroupBox to the demo app and edit the Custom ColorZone Demo to reflect the new changes
    
    * Move the Custom Background and Foreground from the ColorZone to the ColorZone assist class
    
    * Adjusting to avoid needing the properties to inherit
    
    Co-authored-by: Kevin B <Keboo@users.noreply.github.com>

--- MaterialDesignThemes.Wpf/ColorZone.cs
+++ MaterialDesignThemes.Wpf/ColorZone.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace MaterialDesignThemes.Wpf
 {
@@ -12,11 +13,12 @@ namespace MaterialDesignThemes.Wpf
         PrimaryDark,
         Accent,
         Light,
-        Dark
+        Dark,
+        Custom
     }
 
     /// <summary>
-    /// User a colour zone to easily switch the background and foreground colours, whilst still remaining within the selected Material Design palette.
+    /// User a colour zone to easily switch the background and foreground colours, from selected Material Design palette or custom ones.
     /// </summary>
     public class ColorZone : ContentControl
     {
@@ -26,20 +28,20 @@ namespace MaterialDesignThemes.Wpf
         }
 
         public static readonly DependencyProperty ModeProperty = DependencyProperty.Register(
-            nameof(Mode), typeof (ColorZoneMode), typeof (ColorZone), new PropertyMetadata(default(ColorZoneMode)));
+            nameof(Mode), typeof(ColorZoneMode), typeof(ColorZone), new PropertyMetadata(default(ColorZoneMode)));
 
         public ColorZoneMode Mode
         {
-            get { return (ColorZoneMode) GetValue(ModeProperty); }
+            get { return (ColorZoneMode)GetValue(ModeProperty); }
             set { SetValue(ModeProperty, value); }
         }
 
         public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
-            nameof(CornerRadius), typeof (CornerRadius), typeof (ColorZone), new PropertyMetadata(default(CornerRadius)));
+            nameof(CornerRadius), typeof(CornerRadius), typeof(ColorZone), new PropertyMetadata(default(CornerRadius)));
 
         public CornerRadius CornerRadius
         {
-            get { return (CornerRadius) GetValue(CornerRadiusProperty); }
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
             set { SetValue(CornerRadiusProperty, value); }
         }
     }
