91d280fb51fa633bd62e7d5cbf5c74351409fecd
Kevin B <Keboo@users.noreply.github.com>
2020-01-30 21:17:09 -0800

    Proposed fix for 1550 (#1576)
    Uses theme MaterialDesignPaper brush or the combo box's background when set.

---
+++ MaterialDesignThemes.Wpf/Converters/FallbackBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    internal class FallbackBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values?.OfType<SolidColorBrush>()
+                .FirstOrDefault(x => x.Color != default && x.Color != Colors.Transparent);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
