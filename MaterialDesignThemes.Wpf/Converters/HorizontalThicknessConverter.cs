6d3b105b6c4547b864f4b7332b9d76589c931243
Kevin B <Keboo@users.noreply.github.com>
2020-01-30 22:37:51 -0800

    Fixes #1623 (#1629)
    Limiting the height of the rectangle to 0 to avoid keeping headers visible.

---
+++ MaterialDesignThemes.Wpf/Converters/HorizontalThicknessConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    internal class HorizontalThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Thickness thickness)
+            {
+                return new Thickness(thickness.Left, 0, thickness.Right, 0);
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
