e35532404b9ef1cbde575518b0bc5d81654ba2b0
Ole Wiedemann <o.wiedemann@w-syss.de>
2020-02-22 12:50:23 +0100

    [Snackbar 1/3] Improve duplicate handling
    Renamed IgnoreDuplicate to ShowAlways.
    Added MessageExpired method.
    Added appropriate Equals method.
    Added autogenerated GetHashCode method.
    
    ShowAlways more closely represented what that property actually does. IgnoreDuplicate is not very telling in the context of a SnachkBarMessageQueueItem.
    MessageExpired serves as a helper to determine if a message already exceeded it's duration.
    Equals and HashCode simplify comparisons of SnackBarMessageQueueItems, especially useful in the determining if a message is a duplicate.

--- MaterialDesignThemes.Wpf/SnackbarMessageQueueItem.cs
+++ MaterialDesignThemes.Wpf/SnackbarMessageQueueItem.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MaterialDesignThemes.Wpf
 {
     internal class SnackbarMessageQueueItem
     {
         public SnackbarMessageQueueItem(object content, TimeSpan duration, object actionContent = null, Action<object> actionHandler = null, object actionArgument = null, 
-            bool isPromoted = false, bool ignoreDuplicate = false)
+                                        bool isPromoted = false, bool showAlways = false)
         {
             Content = content;
             Duration = duration;
@@ -13,7 +14,7 @@ namespace MaterialDesignThemes.Wpf
             ActionHandler = actionHandler;
             ActionArgument = actionArgument;
             IsPromoted = isPromoted;
-            IgnoreDuplicate = ignoreDuplicate;
+            ShowAlways = showAlways;
         }
 
         /// <summary>
@@ -47,8 +48,37 @@ namespace MaterialDesignThemes.Wpf
         public bool IsPromoted { get; }
 
         /// <summary>
-        /// Still display this message even if it is a duplicate.
+        /// Always show this message, even if it's a duplicate
         /// </summary>
-        public bool IgnoreDuplicate { get; }
+        public bool ShowAlways { get; }
+
+        /// <summary>
+        /// Last time this message was shown
+        /// </summary>
+        public DateTime LastShownAt { get; set; }
+
+        public bool MessageExpired()
+        {
+            if (LastShownAt == null)
+                return true;
+            else
+                return LastShownAt <= DateTime.Now.Subtract(Duration);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is SnackbarMessageQueueItem message))
+                return false;
+
+            return Content.Equals(message.Content) && ActionContent.Equals(message.ActionContent);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -724337062;
+            hashCode = hashCode * -1521134295 + EqualityComparer<object>.Default.GetHashCode(Content);
+            hashCode = hashCode * -1521134295 + EqualityComparer<object>.Default.GetHashCode(ActionContent);
+            return hashCode;
+        }
     }
 }
