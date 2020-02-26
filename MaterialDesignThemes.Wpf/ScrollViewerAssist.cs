5a68066b4c38b142bef0b539e35c87f41c0485cb
AmRo <VirtualEmailBox045@Gmail.Com>
2020-02-26 12:31:32 +0330

    Add 'IsAutoHideEnabled' and 'CornerRectangleVisibility' properties to ScrollViewerAssist

--- MaterialDesignThemes.Wpf/ScrollViewerAssist.cs
+++ MaterialDesignThemes.Wpf/ScrollViewerAssist.cs
@@ -3,7 +3,7 @@ using System.Windows.Controls;
 
 namespace MaterialDesignThemes.Wpf
 {
-    internal static class ScrollViewerAssist
+    public static class ScrollViewerAssist
     {
         public static readonly DependencyProperty HorizontalOffsetProperty = DependencyProperty.RegisterAttached(
             "SyncHorizontalOffset", typeof(double), typeof(ScrollViewerAssist), new PropertyMetadata(default(double), OnSyncHorizontalOffsetChanged));
@@ -21,7 +21,33 @@ namespace MaterialDesignThemes.Wpf
 
         public static double GetSyncHorizontalOffset(DependencyObject element)
         {
-            return (double) element.GetValue(HorizontalOffsetProperty);
+            return (double)element.GetValue(HorizontalOffsetProperty);
+        }
+
+        public static readonly DependencyProperty IsAutoHideEnabledProperty = DependencyProperty.RegisterAttached(
+            "IsAutoHideEnabled", typeof(bool), typeof(ScrollViewerAssist), new PropertyMetadata(default(bool)));
+
+        public static void SetIsAutoHideEnabled(DependencyObject element, bool value)
+        {
+            element.SetValue(IsAutoHideEnabledProperty, value);
+        }
+
+        public static bool GetIsAutoHideEnabled(DependencyObject element)
+        {
+            return (bool)element.GetValue(IsAutoHideEnabledProperty);
+        }
+
+        public static readonly DependencyProperty CornerRectangleVisibilityProperty = DependencyProperty.RegisterAttached(
+            "CornerRectangleVisibility", typeof(Visibility), typeof(ScrollViewerAssist), new PropertyMetadata(default(Visibility)));
+
+        public static void SetCornerRectangleVisibility(DependencyObject element, Visibility value)
+        {
+            element.SetValue(CornerRectangleVisibilityProperty, value);
+        }
+
+        public static Visibility GetCornerRectangleVisibility(DependencyObject element)
+        {
+            return (Visibility)element.GetValue(CornerRectangleVisibilityProperty);
         }
     }
 }
