046ff0534952b5c1b17812922ec7c35e68fb2a37
Kevin Bost <kitokeboo@gmail.com>
2020-03-03 20:08:59 -0800

    Switching to a new converter to avoid memory leaks
    Added unit tests for the converter.
    Enabled InternalsVisibleTo to support testing internal converters.

---
+++ MaterialDesignThemes.Wpf/Converters/FloatingHintTransformConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    internal class FloatingHintTransformConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values?.Length == 2 &&
+                values[0] is double scale &&
+                values[1] is Point floatingOffset)
+            {
+                return new TranslateTransform 
+                {
+                    X = scale * floatingOffset.X,
+                    Y = scale * floatingOffset.Y
+                };
+            }
+            return Transform.Identity;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
