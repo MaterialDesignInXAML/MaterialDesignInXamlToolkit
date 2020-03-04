778b3d8fdf243db37dd53887893d4754c2d70104
Kevin B <Keboo@users.noreply.github.com>
2020-03-03 21:46:29 -0700

    Switching to a new converter to avoid memory leaks (#1683)
    Added unit tests for the converter.
    Enabled InternalsVisibleTo to support testing internal converters.

---
+++ MaterialDesignThemes.Wpf.Tests/Converters/FloatingHintTransformConverterTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+using MaterialDesignThemes.Wpf.Converters;
+using Xunit;
+
+namespace MaterialDesignThemes.Wpf.Tests.Converters
+{
+    public class FloatingHintTransformConverterTests
+    {
+        public static IEnumerable<object[]> InvalidParameters =>
+            new[] 
+            {
+                new object[] {null, null},
+                new object[] {1.0, null},
+                new object[] {null, new Point()},
+                new object[] {DependencyProperty.UnsetValue, new Point()},
+                new object[] {1.0, DependencyProperty.UnsetValue},
+            };
+
+        [StaTheory]
+        [MemberData(nameof(InvalidParameters))]
+        public void WhenParametersAreNotSetItReturnsIdentity(object scale, object offset)
+        {
+            var converter = new FloatingHintTransformConverter();
+
+            var result = converter.Convert(new[] { scale, offset }, typeof(Transform), null, CultureInfo.CurrentUICulture);
+
+            Assert.Equal(Transform.Identity, result);
+        }
+
+        [StaTheory]
+        [InlineData(2.0, 3.0, 4.0)]
+        [InlineData(1.5, 2.0, 3.0)]
+        public void WhenParametersAreSpecifiedItReturnsScaleTransform(double scale, double x, double y)
+        {
+            var converter = new FloatingHintTransformConverter();
+
+            var result = (TranslateTransform)converter.Convert(new object[] { scale, new Point(x, y) }, typeof(Transform), null, CultureInfo.CurrentUICulture);
+
+            Assert.Equal(scale * x, result.X);
+            Assert.Equal(scale * y, result.Y);
+        }
+    }
+
+    
+}
--- MaterialDesignThemes.Wpf/Properties/AssemblyInfo.cs
+++ MaterialDesignThemes.Wpf/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
@@ -22,11 +22,13 @@ using System.Windows.Markup;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
-[assembly:ThemeInfo(
+[assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                             //(used if a resource is not found in the page, 
-                             // or application resource dictionaries)
+                                     //(used if a resource is not found in the page, 
+                                     // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                      //(used if a resource is not found in the page, 
-                                      // app, or any theme specific resource dictionaries)
+                                              //(used if a resource is not found in the page, 
+                                              // app, or any theme specific resource dictionaries)
 )]
+
+[assembly: InternalsVisibleTo("MaterialDesignThemes.Wpf.Tests")]
