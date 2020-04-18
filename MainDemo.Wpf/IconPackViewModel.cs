7df5dd9c545f77c714da267477592d257ded161e
Oliver Ehrenmüller <oliver.ehrenmueller@gmail.com>
2020-04-18 06:16:55 +0200

    Support search for aliases of pack icons

--- MainDemo.Wpf/IconPackViewModel.cs
+++ MainDemo.Wpf/IconPackViewModel.cs
@@ -1,20 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Windows;
-using System.Windows.Input;
-using MaterialDesignColors.WpfExample.Domain;
+﻿using MaterialDesignColors.WpfExample.Domain;
 using MaterialDesignDemo.Domain;
 using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace MaterialDesignDemo
 {
+    public class PackIconKindGroup
+    {
+        public PackIconKindGroup(PackIconKind kind, string[] all)
+        {
+            Kind = kind;
+            Aliases = all;
+            PrimaryName = all[0];
+            ToolTip = string.Join(Environment.NewLine, all);
+        }
+
+        public PackIconKind Kind { get; }
+        public string[] Aliases { get; }
+        public string PrimaryName { get; }
+        public string ToolTip { get; }
+    }
+
     public class IconPackViewModel : INotifyPropertyChanged
     {
-        private readonly Lazy<IEnumerable<PackIconKind>> _packIconKinds;
+        private readonly Lazy<IEnumerable<PackIconKindGroup>> _packIconKinds;
         private readonly ISnackbarMessageQueue _snackbarMessageQueue;
 
         public IconPackViewModel(ISnackbarMessageQueue snackbarMessageQueue)
@@ -24,25 +40,50 @@ namespace MaterialDesignDemo
             OpenDotComCommand = new AnotherCommandImplementation(OpenDotCom);
             SearchCommand = new AnotherCommandImplementation(Search);
             CopyToClipboardCommand = new AnotherCommandImplementation(CopyToClipboard);
-            _packIconKinds = new Lazy<IEnumerable<PackIconKind>>(() =>
-                Enum.GetValues(typeof(PackIconKind))
-                    .OfType<PackIconKind>()
-                    .Distinct()
-                    .OrderBy(k => k.ToString(), StringComparer.InvariantCultureIgnoreCase).ToList()
-                );
+
+            _packIconKinds = new Lazy<IEnumerable<PackIconKindGroup>>(() =>
+                Enum.GetNames(typeof(PackIconKind))
+                    .GroupBy(k => (PackIconKind) Enum.Parse(typeof(PackIconKind), k))
+                    .Select(g => new PackIconKindGroup(g.Key, g.OrderBy(i => i.ToString(), StringComparer.InvariantCultureIgnoreCase).ToArray()))
+                    .OrderBy(i => i.Aliases[0], StringComparer.InvariantCultureIgnoreCase)
+                    .ToList());
         }
 
         public ICommand OpenDotComCommand { get; }
         public ICommand SearchCommand { get; }
         public ICommand CopyToClipboardCommand { get; }
 
-        private IEnumerable<PackIconKind> _kinds;
-        public IEnumerable<PackIconKind> Kinds
+        private IEnumerable<PackIconKindGroup> _kinds;
+        private PackIconKindGroup _group;
+        private PackIconKind? _kind;
+
+        public IEnumerable<PackIconKindGroup> Kinds
         {
-            get { return _kinds ?? (_kinds = _packIconKinds.Value); }
+            get => _kinds ??= _packIconKinds.Value;
             set
             {
                 _kinds = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public PackIconKindGroup Group
+        {
+            get => _group;
+            set
+            {
+                _group = value;
+                Kind = value?.Kind;
+                OnPropertyChanged();
+            }
+        }
+
+        public PackIconKind? Kind
+        {
+            get => _kind;
+            set
+            {
+                _kind = value;
                 OnPropertyChanged();
             }
         }
@@ -52,28 +93,27 @@ namespace MaterialDesignDemo
             Link.OpenInBrowser("https://materialdesignicons.com/");
         }
 
-        private void Search(object obj)
+        private async void Search(object obj)
         {
             var text = obj as string;
             if (string.IsNullOrWhiteSpace(text))
                 Kinds = _packIconKinds.Value;
             else
-                Kinds =
-                    _packIconKinds.Value.Where(
-                        x => x.ToString().IndexOf(text, StringComparison.CurrentCultureIgnoreCase) >= 0);
+                Kinds = await Task.Run(() => _packIconKinds.Value
+                    .Where(x => x.ToolTip.ToString().IndexOf(text, StringComparison.CurrentCultureIgnoreCase) >= 0)
+                    .ToList());
         }
 
         private void CopyToClipboard(object obj)
         {
-            var kind = (PackIconKind?)obj;
-            string toBeCopied = $"<materialDesign:PackIcon Kind=\"{kind}\" />";
+            var toBeCopied = $"<materialDesign:PackIcon Kind=\"{obj}\" />";
             Clipboard.SetDataObject(toBeCopied);
             _snackbarMessageQueue.Enqueue(toBeCopied + " copied to clipboard");
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
