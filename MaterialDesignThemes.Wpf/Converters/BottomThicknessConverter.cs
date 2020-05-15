5e0f2ba5f5caa75552a5f67d0c5de0a7484a7ea0
Oliver Ehrenmüller <oliver.ehrenmueller@gmail.com>
2020-05-15 14:40:27 +0200

    Added new BottomThicknessConverter with returning a Thickness where all properties but Bottom is set to zero

---
+++ MaterialDesignThemes.Wpf/Converters/BottomThicknessConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    internal class BottomThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Thickness thickness)
+            {
+                return new Thickness(0, 0, 0, thickness.Bottom);
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
