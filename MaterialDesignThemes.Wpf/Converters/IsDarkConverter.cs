c5c3f57eee820855463a1144f29caa81b4cd329b
Kevin Bost <kitokeboo@gmail.com>
2020-05-14 22:56:37 -0700

    Flipping foreground and background when theme is dark background

---
+++ MaterialDesignThemes.Wpf/Converters/IsDarkConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using MaterialDesignColors.ColorManipulation;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    internal class IsDarkConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value switch
+            {
+                SolidColorBrush brush => brush.Color,
+                Color c => c,
+                _ => (Color?)null
+            } is Color color
+                ? color.ContrastingForegroundColor() == Colors.White
+                : Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
