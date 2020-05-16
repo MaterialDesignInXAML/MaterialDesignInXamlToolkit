22f5fa8be0c248d19ab7b3f061cb0d80eb6c8e6e
Oliver Ehrenmüller <oliver.ehrenmueller@gmail.com>
2020-05-16 12:15:09 +0200

    Redesigned BottomThicknessConverter to return a Thickness restricted to Bottom (all others to zero)

--- MaterialDesignThemes.Wpf/Converters/BottomThicknessConverter.cs
+++ MaterialDesignThemes.Wpf/Converters/BottomThicknessConverter.cs
@@ -5,18 +5,12 @@ using System.Windows.Data;
 
 namespace MaterialDesignThemes.Wpf.Converters
 {
-    public class BottomThicknessConverter : IValueConverter
+    internal class BottomThicknessConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (!(value is Thickness thickness))
-                return Binding.DoNothing;
-
-            if (parameter == null)
-                return thickness.Bottom;
-
-            return thickness.Bottom * System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
-        }
+            => value is Thickness thickness
+                ? new Thickness(0, 0, 0, thickness.Bottom)
+                : Binding.DoNothing;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => throw new NotImplementedException();
