037e01213c8c89b93f0848c560fde0958f1c2660
Kevin B <Keboo@users.noreply.github.com>
2020-05-19 23:05:20 -0700

    Feature/material design choice chip list box (#1847)
    * Added MaterialDesignFilterChipOutlineListBox
    
    * Added MaterialDesignChoiceChip(Outline)RadioButton
    
    Added MaterialDesignChoiceChipAccent(Outline)RadioButton
    
    * Added MaterialDesignChoiceChip(Outline)ListBox
    
    Added MaterialDesignChoiceChip(AccentOutline)ListBox
    
    * Fixed FilterChipOutline Selected style to also have an outline
    
    Added colorred versions of FilterChips
    
    * Created color versions of ChoiceChip styles
    
    * Flipping foreground and background when theme is dark background
    
    * Applied dark theme fix to ChoiceChip RadioButtons
    
    Co-authored-by: Joris Voermans <Joris@clevr.net>

--- MaterialDesignThemes.Wpf/ThemeAssist.cs
+++ MaterialDesignThemes.Wpf/ThemeAssist.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Linq;
 using System.Windows;
+using System.Windows.Media;
 
 namespace MaterialDesignThemes.Wpf
 {
     public static class ThemeAssist
     {
+        internal static Brush GetTriggerColor(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(TriggerBrushProperty);
+        }
+
+        internal static void SetTriggerBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(TriggerBrushProperty, value);
+        }
+
+        internal static readonly DependencyProperty TriggerBrushProperty =
+            DependencyProperty.RegisterAttached("TriggerBrush", typeof(Brush), typeof(ThemeAssist), new PropertyMetadata(null));
+
         public static BaseTheme GetTheme(DependencyObject obj)
         {
             return (BaseTheme)obj.GetValue(ThemeProperty);
