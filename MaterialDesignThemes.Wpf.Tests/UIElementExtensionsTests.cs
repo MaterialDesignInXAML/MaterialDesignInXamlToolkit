0d8aa2a0e495a82b346e07b2f30331fb468949d3
Ole Wiedemann <61351036+w-syss@users.noreply.github.com>
2020-05-21 20:52:00 +0200

    [AdornerAssist] Refactoring & Tests (#1827)
    * [AdornerAssist] Refactoring & Tests
    
    Refactored AdornerAssist to improve code quality and increase robustness.
    
    - Added braces to single line statements
    - Throws exceptions on invalid operations
    - Added tests
    - Renamed functions to more closely portray what a function does
    - Fixed comments
    
    * Renamed AdornerAssist to UIElementExtensions.
    
    * Renamed AddAdornerOfType to AddAdorner.
    
    * Renamed RemoveAdornersOfType to RemoveAdorners.

---
+++ MaterialDesignThemes.Wpf.Tests/UIElementExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using Xunit;
+
+namespace MaterialDesignThemes.Wpf.Tests
+{
+    public class UIElementExtensionsTests : IClassFixture<UIElementExtensionsTests.ArdornerFixture>
+    {
+        private readonly ArdornerFixture testFixture;
+
+        public UIElementExtensionsTests(ArdornerFixture fixture)
+        {
+            testFixture = fixture;
+        }
+
+        [Fact]
+        public void AddAdornerNullArgumentThrowsException()
+        { 
+            Assert.Throws<ArgumentNullException>(
+                () => testFixture.testElement.AddAdorner<BottomDashedLineAdorner>(null));
+        }
+
+        [Fact]
+        public void AddAdornerToElementWithoutAdornerLayerThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(
+                () => testFixture.testElement.AddAdorner(new BottomDashedLineAdorner(testFixture.testElement)));
+        }
+
+        #region Class : ArdornerFixture
+        public class ArdornerFixture : IDisposable
+        {
+            public UIElement testElement;
+            public ArdornerFixture()
+            {
+                testElement = new UIElement();
+            }
+
+            public void Dispose()
+            {
+                // No cleanup required
+            }
+        }
+        #endregion
+    }
+}
