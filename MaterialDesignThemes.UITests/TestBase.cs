6fd11e4e70333940f2b450ac90e4bec8e0c54a83
Kevin Bost <kitokeboo@gmail.com>
2020-05-26 22:31:49 -0700

    Working base test

--- MaterialDesignThemes.UITests/TestBase.cs
+++ MaterialDesignThemes.UITests/TestBase.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Management;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
@@ -20,58 +21,29 @@ namespace MaterialDesignThemes.UITests
 
         private bool AppHasLaunched { get; set; }
 
-        public void Click(AppiumWebElement element)
-        {
-            WaitForElement(() => new Actions(Driver).MoveToElement(element).Click().Perform());
-        }
-
-        public void SendKeys(AppiumWebElement element, string input)
-        {
-            WaitForElement(() => new Actions(Driver).SendKeys(element, input).Perform());
-        }
-
-        public void DragAndDrop(AppiumWebElement element, int startOffsetX, int startOffsetY, int endOffsetX, int endOffsetY)
-        {
-            var crop = new Actions(Driver);
-            crop.MoveToElement(element, startOffsetX, startOffsetY)
-                .ClickAndHold()
-                .MoveByOffset(endOffsetX, endOffsetY)
-                .Release()
-                .Perform();
-        }
-
-        public void WaitForElement(Action action)
-        {
-            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
-            wait.IgnoreExceptionTypes(typeof(WebDriverException));
-            wait.Until(a =>
-            {
-                action();
-                return true;
-            });
-        }
-
-        protected bool WaitForElement(Func<bool> func, int timeoutSeconds = 20)
-        {
-            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timeoutSeconds));
-            wait.IgnoreExceptionTypes(typeof(WebDriverException));
-            try
-            {
-                return wait.Until(a =>
-                {
-                    return func();
-                });
-            }
-            catch (WebDriverException)
-            {
-                return false;
-            }
-        }
-
         protected async Task LaunchApplication()
         {
             if (AppHasLaunched) return;
 
+            IntPtr mainWindowHandle = await StartApp();
+
+            StartWinAppDriver();
+
+            var appOptions = new AppiumOptions();
+            appOptions.AddAdditionalCapability("deviceName", "WindowsPC");
+            appOptions.AddAdditionalCapability("appTopLevelWindow", mainWindowHandle.ToInt32().ToString("x"));
+
+            Driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appOptions);
+
+            WindowsElement? element = Driver.FindElementByName("Material Design In XAML Toolkit");
+
+            Assert.NotNull(element);
+
+            AppHasLaunched = true;
+        }
+
+        private static async Task<IntPtr> StartApp()
+        {
             string workingDirectory = Path.GetFullPath(@"..\..\..\..\MainDemo.Wpf\bin\Debug\netcoreapp3.1\");
             var processInfo = new ProcessStartInfo
             {
@@ -93,28 +65,26 @@ namespace MaterialDesignThemes.UITests
                 await Task.Delay(TimeSpan.FromSeconds(2));
             }
 
+            return appProcess.MainWindowHandle;
+        }
+
+        private static void StartWinAppDriver()
+        {
             Process[] winappProcess = Process.GetProcessesByName("WinAppDriver");
             if (winappProcess.Length == 0)
             {
-                var path = @"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe";
-                if (!File.Exists(path))
+                //Search default install location on all drives
+                foreach(var drive in Environment.GetLogicalDrives())
                 {
-                    throw new FileNotFoundException("WinAppDriver was not in default directory");
+                    var path = @$"{drive}Program Files (x86)\Windows Application Driver\WinAppDriver.exe";
+                    if (File.Exists(path))
+                    {
+                        Process.Start(path);
+                        return;
+                    }
                 }
-                Process.Start(path);
+                throw new InvalidOperationException("WinAppDriver was not found.");
             }
-
-            var appOptions = new AppiumOptions();
-            appOptions.AddAdditionalCapability("deviceName", "WindowsPC");
-            appOptions.AddAdditionalCapability("appTopLevelWindow", appProcess.MainWindowHandle.ToInt32().ToString("x"));
-
-            Driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appOptions);
-
-            WindowsElement? element = Driver.FindElementByName("Material Design In XAML Toolkit");
-
-            Assert.NotNull(element);
-
-            AppHasLaunched = true;
         }
     }
 }
--- MaterialDesignThemes.UITests/UnitTest1.cs
+++ MaterialDesignThemes.UITests/UnitTest1.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
-using OpenQA.Selenium.Interactions;
 using Xunit;
 
 namespace MaterialDesignThemes.UITests
@@ -17,24 +15,18 @@ namespace MaterialDesignThemes.UITests
         {
             await LaunchApplication();
 
-            WindowsElement? hamburgerToggleButton = Driver.FindElementByName("HamburgerToggleButton");
-            Assert.NotNull(hamburgerToggleButton);
-            Click(hamburgerToggleButton);
-
-            await Task.Delay(TimeSpan.FromSeconds(1));
-
             WindowsElement? listBox = Driver.FindElementByName("DemoPagesListBox");
             Assert.NotNull(listBox);
 
             IList<AppiumWebElement>? listItems = listBox.FindElements(By.XPath("//ListItem"));
-            listItems = listItems.Skip(listItems.Count - 3).ToList();
 
             foreach (AppiumWebElement? listItem in listItems)
             {
-                Click(listItem);
+                var hamburgerToggleButton = Driver.FindElementByName("HamburgerToggleButton");
+                hamburgerToggleButton.Click();
                 await Task.Delay(TimeSpan.FromSeconds(1));
-                hamburgerToggleButton = Driver.FindElementByName("HamburgerToggleButton");
-                Click(hamburgerToggleButton);
+
+                listItem.Click();
                 await Task.Delay(TimeSpan.FromSeconds(1));
             }
         }
