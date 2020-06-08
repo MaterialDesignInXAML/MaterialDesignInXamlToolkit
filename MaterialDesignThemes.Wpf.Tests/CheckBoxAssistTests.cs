eb05f3cabeb8adee10e0b2e7650ed4eb9680913f
Ole Wiedemann <o.wiedemann@w-syss.de>
2020-06-08 22:08:12 +0200

    [CheckBoxAssist] Refactoring & Tests
    - Streamlined attached properties
    - Extracted default values in const fields
    - Added basic tests for attached properties
    - Narrowed type for attached properties

---
+++ MaterialDesignThemes.Wpf.Tests/CheckBoxAssistTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using Xunit;
+
+namespace MaterialDesignThemes.Wpf.Tests
+{
+    public class CheckBoxAssistTests
+    {
+        private readonly CheckBox _testElement;
+
+        public CheckBoxAssistTests()
+        {
+            _testElement = new CheckBox();
+        }
+
+        [StaFact]
+        public void TestCheckBoxSizeProperty()
+        {
+            // Assert defaults
+            Assert.Equal("CheckBoxSize", CheckBoxAssist.CheckBoxSizeProperty.Name);
+            Assert.Equal(18.0, CheckBoxAssist.GetCheckBoxSize(_testElement));
+
+            // Assert setting works
+            CheckBoxAssist.SetCheckBoxSize(_testElement, 27.1);
+            Assert.Equal(27.1, CheckBoxAssist.GetCheckBoxSize(_testElement));
+        }
+    }
+}
