516a55a2c8c0c921cfd577d8b914fe99c4c35f3e
Ole Wiedemann <o.wiedemann@w-syss.de>
2020-06-08 22:33:55 +0200

    [Chip] Refactoring
    - Streamlined DependencyProperties
    - Removed comments which added no new information
    - Added braces for if-conditionals
    - Formatting

--- MaterialDesignThemes.Wpf/Chip.cs
+++ MaterialDesignThemes.Wpf/Chip.cs
@@ -10,112 +10,126 @@ namespace MaterialDesignThemes.Wpf
     [TemplatePart(Name = DeleteButtonPartName, Type = typeof(Button))]
     public class Chip : ButtonBase
     {
-        private ButtonBase _deleteButton;
-
         public const string DeleteButtonPartName = "PART_DeleteButton";
+
+        private ButtonBase _deleteButton;
 
         static Chip()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(Chip), new FrameworkPropertyMetadata(typeof(Chip)));
         }
 
-        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
-            "Icon", typeof(object), typeof(Chip), new PropertyMetadata(default(object)));
+        #region DependencyProperty : IconProperty
+		public object Icon
+		{
+			get => GetValue(IconProperty);
+			set => SetValue(IconProperty, value);
+		}
+		public static readonly DependencyProperty IconProperty
+			= DependencyProperty.Register(nameof(Icon), typeof(object), typeof(Chip), new PropertyMetadata(default(object)));
+		#endregion
+		
+        #region DependencyProperty : IconBackgroundProperty
+		public Brush IconBackground
+		{
+			get => (Brush)GetValue(IconBackgroundProperty);
+			set => SetValue(IconBackgroundProperty, value);
+		}
+		public static readonly DependencyProperty IconBackgroundProperty
+			= DependencyProperty.Register(nameof(IconBackground), typeof(Brush), typeof(Chip), new PropertyMetadata(default(Brush)));
+		#endregion
 
-        public object Icon
-        {
-            get { return (object)GetValue(IconProperty); }
-            set { SetValue(IconProperty, value); }
-        }
+        #region DependencyProperty : IconForegroundProperty
+		public Brush IconForeground
+		{
+			get => (Brush)GetValue(IconForegroundProperty);
+			set => SetValue(IconForegroundProperty, value);
+		}
+		public static readonly DependencyProperty IconForegroundProperty
+			= DependencyProperty.Register(nameof(IconForeground), typeof(Brush), typeof(Chip), new PropertyMetadata(default(Brush)));
+        #endregion
 
-        public static readonly DependencyProperty IconBackgroundProperty = DependencyProperty.Register(
-            "IconBackground", typeof(Brush), typeof(Chip), new PropertyMetadata(default(Brush)));
-
-        public Brush IconBackground
-        {
-            get { return (Brush)GetValue(IconBackgroundProperty); }
-            set { SetValue(IconBackgroundProperty, value); }
-        }
-
-        public static readonly DependencyProperty IconForegroundProperty = DependencyProperty.Register(
-            "IconForeground", typeof(Brush), typeof(Chip), new PropertyMetadata(default(Brush)));
-
-        public Brush IconForeground
-        {
-            get { return (Brush)GetValue(IconForegroundProperty); }
-            set { SetValue(IconForegroundProperty, value); }
-        }
-
-        public static readonly DependencyProperty IsDeletableProperty = DependencyProperty.Register(
-            "IsDeletable", typeof(bool), typeof(Chip), new PropertyMetadata(default(bool)));
-
+        #region DependencyProperty : IsDeletableProperty
         /// <summary>
         /// Indicates if the delete button should be visible.
         /// </summary>
         public bool IsDeletable
-        {
-            get { return (bool)GetValue(IsDeletableProperty); }
-            set { SetValue(IsDeletableProperty, value); }
-        }
+		{
+			get => (bool)GetValue(IsDeletableProperty);
+			set => SetValue(IsDeletableProperty, value);
+		}
+		public static readonly DependencyProperty IsDeletableProperty
+			= DependencyProperty.Register(nameof(IsDeletable), typeof(bool), typeof(Chip), new PropertyMetadata(default(bool)));
+		#endregion
+		
+        #region DependencyProperty : DeleteCommandProperty
+		public ICommand DeleteCommand
+		{
+			get => (ICommand)GetValue(DeleteCommandProperty);
+			set => SetValue(DeleteCommandProperty, value);
+		}
+		public static readonly DependencyProperty DeleteCommandProperty
+			= DependencyProperty.Register(nameof(DeleteCommand), typeof(ICommand), typeof(Chip), new PropertyMetadata(default(ICommand)));
+		#endregion
 
-        public static readonly DependencyProperty DeleteCommandProperty = DependencyProperty.Register(
-            "DeleteCommand", typeof(ICommand), typeof(Chip), new PropertyMetadata(default(ICommand)));
+        #region DependencyProperty : DeleteCommandParameterProperty
+		public object DeleteCommandParameter
+		{
+			get => GetValue(DeleteCommandParameterProperty);
+			set => SetValue(DeleteCommandParameterProperty, value);
+		}
+		public static readonly DependencyProperty DeleteCommandParameterProperty
+			= DependencyProperty.Register(nameof(DeleteCommandParameter), typeof(object), typeof(Chip), new PropertyMetadata(default(object)));
+		#endregion
+		
+        #region DependencyProperty : DeleteToolTipProperty
+		public object DeleteToolTip
+		{
+			get => GetValue(DeleteToolTipProperty);
+			set => SetValue(DeleteToolTipProperty, value);
+		}
+		public static readonly DependencyProperty DeleteToolTipProperty
+			= DependencyProperty.Register(nameof(DeleteToolTip), typeof(object), typeof(Chip), new PropertyMetadata(default(object)));
+        #endregion
 
-        public ICommand DeleteCommand
-        {
-            get { return (ICommand)GetValue(DeleteCommandProperty); }
-            set { SetValue(DeleteCommandProperty, value); }
-        }
-
-        public static readonly DependencyProperty DeleteCommandParameterProperty = DependencyProperty.Register(
-            "DeleteCommandParameter", typeof(object), typeof(Chip), new PropertyMetadata(default(object)));
-
-        public object DeleteCommandParameter
-        {
-            get { return (object)GetValue(DeleteCommandParameterProperty); }
-            set { SetValue(DeleteCommandParameterProperty, value); }
-        }
-
-        public static readonly DependencyProperty DeleteToolTipProperty = DependencyProperty.Register(
-            "DeleteToolTip", typeof(object), typeof(Chip), new PropertyMetadata(default(object)));
-
-        public object DeleteToolTip
-        {
-            get { return (object)GetValue(DeleteToolTipProperty); }
-            set { SetValue(DeleteToolTipProperty, value); }
-        }
-
-        /// <summary>
-        /// Event correspond to delete button left mouse button click 
-        /// </summary>
-        public static readonly RoutedEvent DeleteClickEvent = EventManager.RegisterRoutedEvent("DeleteClick", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(Chip));
-
-        /// <summary>
-        /// Add / Remove DeleteClickEvent handler 
-        /// </summary>
+        #region Event : DeleteClickEvent
         [Category("Behavior")]
-        public event RoutedEventHandler DeleteClick { add { AddHandler(DeleteClickEvent, value); } remove { RemoveHandler(DeleteClickEvent, value); } }
+        
+        public event RoutedEventHandler DeleteClick
+        {
+            add => AddHandler(DeleteClickEvent, value);
+            remove => RemoveHandler(DeleteClickEvent, value);
+        }
+        
+        public static readonly RoutedEvent DeleteClickEvent
+            = EventManager.RegisterRoutedEvent(nameof(DeleteClick), RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(Chip));
+        #endregion
 
         public override void OnApplyTemplate()
         {
             if (_deleteButton != null)
+            {
                 _deleteButton.Click -= DeleteButtonOnClick;
-
+            }
+            
             _deleteButton = GetTemplateChild(DeleteButtonPartName) as ButtonBase;
+            
             if (_deleteButton != null)
+            {
                 _deleteButton.Click += DeleteButtonOnClick;
-
+            }
+            
             base.OnApplyTemplate();
         }
 
         protected virtual void OnDeleteClick()
         {
-            var newEvent = new RoutedEventArgs(DeleteClickEvent, this);
-            RaiseEvent(newEvent);
+            RaiseEvent(new RoutedEventArgs(DeleteClickEvent, this));
 
-            var command = DeleteCommand;
-            if (command != null && command.CanExecute(DeleteCommandParameter))
-                command.Execute(DeleteCommandParameter);
+            if (DeleteCommand?.CanExecute(DeleteCommandParameter) ?? false)
+            {
+                DeleteCommand.Execute(DeleteCommandParameter);
+            }
         }
 
         private void DeleteButtonOnClick(object sender, RoutedEventArgs routedEventArgs)
