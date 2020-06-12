749712bc019dd3563abf95a193aebbb9033d0b74
Ole Wiedemann <61351036+w-syss@users.noreply.github.com>
2020-06-12 05:48:58 +0200

    [CheckBoxAssist] Refactoring & Tests (#1906)
    - Streamlined attached properties
    - Extracted default values in const fields
    - Added basic tests for attached properties
    - Narrowed type for attached properties

--- MaterialDesignThemes.Wpf/CheckBoxAssist.cs
+++ MaterialDesignThemes.Wpf/CheckBoxAssist.cs
@@ -1,13 +1,18 @@
 using System.Windows;
+using System.Windows.Controls;
 
 namespace MaterialDesignThemes.Wpf
 {
     public class CheckBoxAssist
     {
-        public static readonly DependencyProperty CheckBoxSizeProperty = DependencyProperty.RegisterAttached(
-            "CheckBoxSize", typeof(double), typeof(CheckBoxAssist), new PropertyMetadata(18d));
+        private const double DefaultCheckBoxSize = 18.0;
 
-        public static void SetCheckBoxSize(DependencyObject element, double value) => element.SetValue(CheckBoxSizeProperty, value);
-        public static double GetCheckBoxSize(DependencyObject element) => (double) element.GetValue(CheckBoxSizeProperty);
+        #region AttachedProperty : CheckBoxSizeProperty
+        public static readonly DependencyProperty CheckBoxSizeProperty
+            = DependencyProperty.RegisterAttached("CheckBoxSize", typeof(double), typeof(CheckBoxAssist), new PropertyMetadata(DefaultCheckBoxSize));
+
+        public static double GetCheckBoxSize(CheckBox element) => (double)element.GetValue(CheckBoxSizeProperty);
+        public static void SetCheckBoxSize(CheckBox element, double checkBoxSize) => element.SetValue(CheckBoxSizeProperty, checkBoxSize);
+        #endregion
     }
 }
