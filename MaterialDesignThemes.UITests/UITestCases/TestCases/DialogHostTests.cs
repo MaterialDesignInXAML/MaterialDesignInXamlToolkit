9736c014d7b9f549ca607090a1d98ead33714af1
Kevin Bost <kitokeboo@gmail.com>
2020-06-14 00:58:37 -0700

    Adding screenshot.

--- MaterialDesignThemes.UITests/UITestCases/TestCases/DialogHostTests.cs
+++ MaterialDesignThemes.UITests/UITestCases/TestCases/DialogHostTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using Xunit;
@@ -15,35 +16,45 @@ namespace MaterialDesignThemes.UITests.UITestCases.TestCases
         [Fact]
         public void WhenDialogHostIsShown_OverlayPreventsClicking()
         {
-            var mainWindow = new MainWindow(Driver);
-            AppiumWebElement testCase = mainWindow.GetTestCase("DialogHost/ShowsDialog");
-            testCase.Click();
-            mainWindow.ExecuteButton.Click();
-
-            WindowsElement resultTextBlock = Driver.FindElementByAccessibilityId("ResultTextBlock", TimeSpan.FromMilliseconds(200));
-            Driver.WaitFor(() => resultTextBlock.Text == "Clicks: 0");
-
-            WindowsElement testOverlayButton = Driver.FindElementByAccessibilityId("TestOverlayButton", TimeSpan.FromSeconds(0.5));
-            testOverlayButton.Click();
-
-            Driver.WaitFor(() => resultTextBlock.Text == "Clicks: 1");
-
-            WindowsElement showDialogButton = Driver.FindElementByAccessibilityId("ShowDialogButton", TimeSpan.FromSeconds(0.5));
-            showDialogButton.Click();
-            //The presence of the close button indicates the dialog is shown
-            WindowsElement closeDialogButton = Driver.FindElementByAccessibilityId("CloseDialogButton", TimeSpan.FromSeconds(0.5));
-            testOverlayButton.Click();
-            Driver.WaitFor(() => resultTextBlock.Text == "Clicks: 1");
-
-            closeDialogButton.Click();
-
-            Driver.WaitUntil(() =>
+            try
             {
-                Output.WriteLine("Input");
+                var mainWindow = new MainWindow(Driver);
+                AppiumWebElement testCase = mainWindow.GetTestCase("DialogHost/ShowsDialog");
+                testCase.Click();
+                mainWindow.ExecuteButton.Click();
+
+                WindowsElement resultTextBlock = Driver.FindElementByAccessibilityId("ResultTextBlock", TimeSpan.FromMilliseconds(200));
+                Driver.WaitFor(() => resultTextBlock.Text == "Clicks: 0");
+
+                WindowsElement testOverlayButton = Driver.FindElementByAccessibilityId("TestOverlayButton", TimeSpan.FromSeconds(0.5));
                 testOverlayButton.Click();
-                Assert.Equal("Clicks: 2", resultTextBlock.Text);
-                Output.WriteLine("Output");
-            }, TimeSpan.FromSeconds(2));
+
+                Driver.WaitFor(() => resultTextBlock.Text == "Clicks: 1");
+
+                WindowsElement showDialogButton = Driver.FindElementByAccessibilityId("ShowDialogButton", TimeSpan.FromSeconds(0.5));
+                showDialogButton.Click();
+                //The presence of the close button indicates the dialog is shown
+                WindowsElement closeDialogButton = Driver.FindElementByAccessibilityId("CloseDialogButton", TimeSpan.FromSeconds(0.5));
+                testOverlayButton.Click();
+                Driver.WaitFor(() => resultTextBlock.Text == "Clicks: 1");
+
+                closeDialogButton.Click();
+
+                Driver.WaitUntil(() =>
+                {
+                    Output.WriteLine("Input");
+                    testOverlayButton.Click();
+                    Assert.Equal("Clicks: 2", resultTextBlock.Text);
+                    Output.WriteLine("Output");
+                }, TimeSpan.FromSeconds(2));
+            }
+            finally
+            {
+                var screenshot = Driver.GetScreenshot();
+                string fullPath = Path.GetFullPath($"{nameof(WhenDialogHostIsShown_OverlayPreventsClicking)}.jpg");
+                Output.WriteLine("Saving image " + fullPath);
+                screenshot.SaveAsFile(fullPath);
+            }
         }
     }
 }
