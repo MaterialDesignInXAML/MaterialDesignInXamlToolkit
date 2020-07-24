d780df51bfdcbda586dd8cfea12fc2d6baf58477
Kevin B <Keboo@users.noreply.github.com>
2020-07-23 23:56:23 -0700

    Switching to XAMLTest for UI testing (#1991)
    * Protocol buffer communication working
    
    
    Basic query of elements working
    
    
    Added property and resource retrieval.
    
    
    Adding property setters and starting work on query API
    
    
    More work to try and get mouse/keyboard working
    
    
    Fixing move over element method to work 
    
    
    Getting list box test working
    
    
    Lots of test cleanup to make it easier.
    
    
    Starting work on doing color based tests
    
    
    Nullability because 
    
    
    Getting contrast ratio working.
    
    
    Fixing nullability warnings.
    
    
    Starting work on using built-in app
    
    
    More work on trying to get Xaml deserialization working.
    
    
    Got showing a window working.
    
    
    Separating window and app initialization
    
    
    Adding screenshot functionality
    
    
    Starting work on implementing UI interaction.
    
    
    All tests converted to new VTTest framework.
    
    Removed old UI testing framework
    Cleaning up dependencies
    
    * Clean up of VTTests library
    
    * Removing VTTest project and using new NuGet package
    
    * Switching to XAMLTest to run the UI tests

---
+++ MaterialDesignThemes.UITests/WPF/DialogHost/DialogHostTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using MaterialDesignThemes.UITests.Samples.DialogHost;
+using XamlTest;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MaterialDesignThemes.UITests.WPF.DialogHost
+{
+    public class DialogHostTests : TestBase
+    {
+        public DialogHostTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task OnOpenDialog_OverlayCoversContent()
+        {
+            await using var recorder = new TestRecorder(App);
+
+            IVisualElement dialogHost = await LoadUserControl<WithCounter>();
+
+            IVisualElement resultTextBlock = await dialogHost.GetElement("ResultTextBlock");
+            await Wait.For(async () => await resultTextBlock.GetText() == "Clicks: 0");
+
+            IVisualElement testOverlayButton = await dialogHost.GetElement("TestOverlayButton");
+            await testOverlayButton.Click();
+            await Wait.For(async () => await resultTextBlock.GetText() == "Clicks: 1");
+
+            IVisualElement showDialogButton = await dialogHost.GetElement("ShowDialogButton");
+            await showDialogButton.Click();
+
+            IVisualElement closeDialogButton = await dialogHost.GetElement("CloseDialogButton");
+            await Wait.For(async () => await closeDialogButton.GetIsVisible() == true);
+
+            await testOverlayButton.Click();
+            await Wait.For(async () => await resultTextBlock.GetText() == "Clicks: 1");
+            await closeDialogButton.Click();
+
+
+            await Wait.For(async () =>
+            {
+                Output.WriteLine("Input");
+                await testOverlayButton.Click();
+                Assert.Equal("Clicks: 2", await resultTextBlock.GetText());
+                Output.WriteLine("Output");
+            });
+        }
+    }
+}
---
+++ MaterialDesignThemes.UITests/WPF/ListBox/ListBoxTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Media;
+using XamlTest;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MaterialDesignThemes.UITests.WPF.ListBox
+{
+    public class ListBoxTests : TestBase
+    {
+        public ListBoxTests(ITestOutputHelper output)
+            : base(output)
+        { }
+
+        [Fact]
+        public async Task OnMouseOver_BackgroundIsSet()
+        {
+            await using var recorder = new TestRecorder(App);
+
+            IVisualElement listBox = await LoadXaml(@"
+<ListBox MinWidth=""200"">
+    <ListBoxItem Content=""Item1"" />
+    <ListBoxItem Content=""Item2"" />
+    <ListBoxItem Content=""Item3"" />
+    <ListBoxItem Content=""Item4"" />
+</ListBox>
+");
+
+            IVisualElement listBoxItem = await listBox.GetElement("/ListBoxItem[2]");
+            Assert.Equal("Item3", await listBoxItem.GetProperty<string>("Content"));
+            IVisualElement mouseOverBorder = await listBoxItem.GetElement("MouseOverBorder");
+
+            await listBox.MoveCursorToElement(Position.TopLeft);
+            await Wait.For(async () => Assert.Equal(0.0, await mouseOverBorder.GetOpacity()));
+
+            await mouseOverBorder.MoveCursorToElement();
+            await Wait.For(async () =>
+            {
+                double opacity = await mouseOverBorder.GetOpacity();
+                Output.WriteLine($"Got opacity {opacity}");
+                Assert.Equal(0.1, opacity);
+            });
+
+            Color effectiveBackground = await mouseOverBorder.GetEffectiveBackground();
+            Color foreground = await listBoxItem.GetForegroundColor();
+            foreground = foreground.FlattenOnto(effectiveBackground);
+
+            float contrastRatio = foreground.ContrastRatio(effectiveBackground);
+            Assert.True(contrastRatio >= MaterialDesignSpec.MinimumContrastSmallText);
+
+            recorder.Success();
+        }
+    }
+}
