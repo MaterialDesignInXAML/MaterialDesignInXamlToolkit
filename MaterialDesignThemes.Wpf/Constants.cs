272e83f97fcd6326595879e97e28a266b7ab062a
Oliver Ehrenmüller <oliver.ehrenmueller@gmail.com>
2020-09-12 19:30:29 +0200

    Unify TextBox-Padding in Contants.TextBoxDefaultPadding

---
+++ MaterialDesignThemes.Wpf/Constants.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+namespace MaterialDesignThemes.Wpf
+{
+    internal static class Constants
+    {
+        internal static readonly Thickness TextBoxDefaultPadding = new Thickness(0, 4, 0, 4);
+    }
+}
