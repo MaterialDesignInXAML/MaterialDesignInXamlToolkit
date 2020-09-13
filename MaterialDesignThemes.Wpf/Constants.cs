21647231048da72a43790582fe68f6855759341a
Oliver Ehrenmüller <oliver.ehrenmueller@gmail.com>
2020-09-13 13:46:19 +0200

    Unify padding for floating hints

--- MaterialDesignThemes.Wpf/Constants.cs
+++ MaterialDesignThemes.Wpf/Constants.cs
@@ -5,5 +5,6 @@ namespace MaterialDesignThemes.Wpf
     public static class Constants
     {
         public static readonly Thickness TextBoxDefaultPadding = new Thickness(0, 4, 0, 4);
+        public static readonly Thickness TextBoxFloatingHintPadding = new Thickness(0, 12, 0, 0);
     }
 }
