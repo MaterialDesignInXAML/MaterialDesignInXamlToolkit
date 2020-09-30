93a273baadeff2016c3e498dd777c9f219111ac7
Oliver Ehrenmüller <oliver.ehrenmueller@gmail.com>
2020-09-30 18:18:09 +0200

    Adjust TextBoxViewMargin

--- MaterialDesignThemes.Wpf/Constants.cs
+++ MaterialDesignThemes.Wpf/Constants.cs
@@ -6,6 +6,8 @@ namespace MaterialDesignThemes.Wpf
     {
         public static readonly Thickness TextBoxDefaultPadding = new Thickness(0, 4, 0, 4);
         public static readonly Thickness TextBoxFloatingHintPadding = new Thickness(0, 12, 0, 0);
+        public static readonly Thickness DefaultTextBoxViewMargin = new Thickness(1, 0, 1, 0);
+        public static readonly Thickness DefaultTextBoxViewMarginEmbedded = new Thickness(0);
         public const double TextBoxNotEnabledOpacity = 0.56;
         public const double InnerButtonMarginLeft = 2;
         public static readonly Thickness InnerButtonMargin = new Thickness(InnerButtonMarginLeft, 0, 0, 0);
