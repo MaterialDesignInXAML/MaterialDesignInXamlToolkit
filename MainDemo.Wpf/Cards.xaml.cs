0ac3b0ce9e1f8f137fec50c6b202ff7b8c40df85
AmRo <virtualemailbox045@gmail.com>
2020-10-03 20:10:48 +0330

    Cleanup & reformat Cards section

--- MainDemo.Wpf/Cards.xaml.cs
+++ MainDemo.Wpf/Cards.xaml.cs
@@ -3,19 +3,11 @@ using System.Windows.Controls;
 
 namespace MaterialDesignColors.WpfExample
 {
-    /// <summary>
-    /// Interaction logic for Cards.xaml
-    /// </summary>
     public partial class Cards : UserControl
     {
-        public Cards()
-        {
-            InitializeComponent();
-        }
+        public Cards() => InitializeComponent();
 
         private void Flipper_OnIsFlippedChanged(object sender, RoutedPropertyChangedEventArgs<bool> e)
-        {
-            System.Diagnostics.Debug.WriteLine("Card is flipped = " + e.NewValue);
-        }
+            => System.Diagnostics.Debug.WriteLine("Card is flipped = " + e.NewValue);
     }
 }
