7fc12fed708b27c64bebc22c2ca5842263faa811
AmRo <virtualemailbox045@gmail.com>
2020-10-04 01:53:47 +0330

    Demo App Code Cleanup (#2104)
    * Cleanup & reformat Cards section
    
    * Cleanup & reformat Buttons section
    
    * Cleanup & reformat Chips section

--- MainDemo.Wpf/Chips.xaml.cs
+++ MainDemo.Wpf/Chips.xaml.cs
@@ -3,26 +3,15 @@ using System.Windows.Controls;
 
 namespace MaterialDesignColors.WpfExample
 {
-    /// <summary>
-    /// Interaction logic for Chips.xaml
-    /// </summary>
     public partial class Chips : UserControl
     {
-        public Chips()
-        {
-            InitializeComponent();
-
-        }
+        public Chips() => InitializeComponent();
 
         private void ButtonsDemoChip_OnClick(object sender, RoutedEventArgs e)
-        {
-            MainWindow.Snackbar.MessageQueue.Enqueue("Chip clicked!");
-        }
+            => MainWindow.Snackbar.MessageQueue.Enqueue("Chip clicked!");
 
         private void ButtonsDemoChip_OnDeleteClick(object sender, RoutedEventArgs e)
-        {
-            MainWindow.Snackbar.MessageQueue.Enqueue("Chip delete clicked!");
-        }
+            => MainWindow.Snackbar.MessageQueue.Enqueue("Chip delete clicked!");
 
     }
 }
