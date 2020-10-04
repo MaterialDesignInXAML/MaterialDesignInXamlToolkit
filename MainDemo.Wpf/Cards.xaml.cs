9ec752c1365aa5ce5d13b917090c3126128d4544
AmRo <virtualemailbox045@gmail.com>
2020-10-04 10:05:08 +0330

    Code refactoring

--- MainDemo.Wpf/Cards.xaml.cs
+++ MainDemo.Wpf/Cards.xaml.cs
@@ -7,6 +7,6 @@ namespace MaterialDesignColors.WpfExample
         public Cards() => InitializeComponent();
 
         private void Flipper_OnIsFlippedChanged(object sender, RoutedPropertyChangedEventArgs<bool> e)
-            => System.Diagnostics.Debug.WriteLine("Card is flipped = " + e.NewValue);
+            => System.Diagnostics.Debug.WriteLine($"Card is flipped = {e.NewValue}");
     }
 }
