462dcb53759ab347b972c0d210e5ff94de173d5c
AmRo <virtualemailbox045@gmail.com>
2020-10-04 10:04:06 +0330

    Cleanup & reformat Dialogs section

--- MainDemo.Wpf/Dialogs.xaml.cs
+++ MainDemo.Wpf/Dialogs.xaml.cs
@@ -1,45 +1,38 @@
 ﻿using MaterialDesignThemes.Wpf;
 using System;
-using System.Windows.Controls;
 
 namespace MaterialDesignColors.WpfExample
 {
-    /// <summary>
-    /// Interaction logic for Dialogs.xaml
-    /// </summary>
-    public partial class Dialogs : UserControl
+    public partial class Dialogs
     {
-        public Dialogs()
-        {
-            InitializeComponent();
-        }
+        public Dialogs() => InitializeComponent();
 
         private void Sample1_DialogHost_OnDialogClosing(object sender, DialogClosingEventArgs eventArgs)
         {
-            Console.WriteLine("SAMPLE 1: Closing dialog with parameter: " + (eventArgs.Parameter ?? ""));
+            Console.WriteLine($"SAMPLE 1: Closing dialog with parameter: {eventArgs.Parameter ?? string.Empty}");
 
             //you can cancel the dialog close:
             //eventArgs.Cancel();
 
-            if (!Equals(eventArgs.Parameter, true)) return;
+            if (!Equals(eventArgs.Parameter, true))
+                return;
 
             if (!string.IsNullOrWhiteSpace(FruitTextBox.Text))
                 FruitListBox.Items.Add(FruitTextBox.Text.Trim());
         }
 
         private void Sample2_DialogHost_OnDialogClosing(object sender, DialogClosingEventArgs eventArgs)
-        {
-            Console.WriteLine("SAMPLE 2: Closing dialog with parameter: " + (eventArgs.Parameter ?? ""));
-        }
+            => Console.WriteLine($"SAMPLE 2: Closing dialog with parameter: {eventArgs.Parameter ?? string.Empty}");
 
         private void Sample5_DialogHost_OnDialogClosing(object sender, DialogClosingEventArgs eventArgs)
         {
-            Console.WriteLine("SAMPLE 5: Closing dialog with parameter: " + (eventArgs.Parameter ?? ""));
+            Console.WriteLine($"SAMPLE 5: Closing dialog with parameter: {eventArgs.Parameter ?? string.Empty}");
 
             //you can cancel the dialog close:
             //eventArgs.Cancel();
 
-            if (!Equals(eventArgs.Parameter, true)) return;
+            if (!Equals(eventArgs.Parameter, true))
+                return;
 
             if (!string.IsNullOrWhiteSpace(AnimalTextBox.Text))
                 AnimalListBox.Items.Add(AnimalTextBox.Text.Trim());
