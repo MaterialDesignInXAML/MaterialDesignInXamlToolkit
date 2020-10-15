43e6f2d1598d6d65d382abccd9556301d3521b99
AmRo <virtualemailbox045@gmail.com>
2020-10-15 13:02:00 +0330

    Fix MaterialDesignFlatButton theming issue

---
+++ MaterialDesignThemes.Wpf/Converters/BrushOpacityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    public class BrushOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var opacity = System.Convert.ToDouble(parameter, culture);
+            var brush = value as SolidColorBrush;
+
+            return new SolidColorBrush(brush.Color)
+            {
+                Opacity = opacity
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => Binding.DoNothing;
+    }
+}
