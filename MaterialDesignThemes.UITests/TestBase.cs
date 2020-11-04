b3afd6245bb2ad8ca857d48ba19c3523d448ae18
Kevin B <Keboo@users.noreply.github.com>
2020-11-03 21:22:12 -0800

    Updating to latest XAML Test version (#2157)
    * Updating to latest XAML Test version
    
    * Fixing GetValueAs to use new method
    
    * Updating to version 0.0.8

--- MaterialDesignThemes.UITests/TestBase.cs
+++ MaterialDesignThemes.UITests/TestBase.cs
@@ -28,7 +28,7 @@ namespace MaterialDesignThemes.UITests
         protected async Task<Color> GetThemeColor(string name)
         {
             IResource resource = await App.GetResource(name);
-            return resource.GetValueAs<Color?>() ?? throw new Exception($"Failed to convert resource '{name}' to color");
+            return resource.GetAs<Color?>() ?? throw new Exception($"Failed to convert resource '{name}' to color");
         }
 
         protected async Task<IVisualElement> LoadXaml(string xaml)
