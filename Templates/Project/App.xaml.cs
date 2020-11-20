53293371a736938d76633356dbcb0fc64aaa2486
Kevin Bost <kitokeboo@gmail.com>
2020-11-19 22:52:42 -0800

    First round of adding a new project template

---
+++ Templates/Project/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace MaterailDesign.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
