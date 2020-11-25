7fdececf02288c2c5b571f27f883b114cbee3a31
rnamedynski <rafaln6@hotmail.com>
2020-11-25 15:03:36 +0100

    Fix incorrect ComboxPopup position when used ScaleTranform #2179

---
+++ MaterialDesignThemes.Wpf/ScaleHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf
+{
+    internal static class ScaleHelper
+    {
+        internal static double GetTotalTransformScaleX(Visual visual)
+        {
+            double totalTransform = 1.0d;
+            DependencyObject currentVisualTreeElement = visual;
+            do
+            {
+                visual = (Visual)currentVisualTreeElement as Visual;
+                if (visual != null)
+                {
+                    Transform transform = VisualTreeHelper.GetTransform(visual);
+                    if ((transform != null) &&
+                        (transform.Value.M12 == 0) &&
+                        (transform.Value.OffsetX == 0))
+                    {
+                        totalTransform *= transform.Value.M11;
+                    }
+                }
+                currentVisualTreeElement = VisualTreeHelper.GetParent(currentVisualTreeElement);
+            }
+            while (currentVisualTreeElement != null);
+
+            return totalTransform;
+        }
+
+        internal static double GetTotalTransformScaleY(Visual visual)
+        {
+            double totalTransform = 1.0d;
+            DependencyObject currentVisualTreeElement = visual;
+            do
+            {
+                visual = currentVisualTreeElement as Visual;
+                if (visual != null)
+                {
+                    Transform transform = VisualTreeHelper.GetTransform(visual);
+                    if ((transform != null) &&
+                        (transform.Value.M21 == 0) &&
+                        (transform.Value.OffsetY == 0))
+                    {
+                        totalTransform *= transform.Value.M22;
+                    }
+                }
+                currentVisualTreeElement = VisualTreeHelper.GetParent(currentVisualTreeElement);
+            }
+            while (currentVisualTreeElement != null);
+
+            return totalTransform;
+        }
+    }
+}
