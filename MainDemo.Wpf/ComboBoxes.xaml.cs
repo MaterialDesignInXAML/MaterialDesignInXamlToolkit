24986d4b4997bfc9cc539905667c98491b0898f0
Jan Ivar Z. Carlsen <jizc@outlook.com>
2021-01-04 02:47:24 +0100

    Outlined ComboBox style (#2204)
    * Just starting work for an outline combo box
    
    * Combined ComboBox templates
    
    Aiming for the same type of solution TextBox uses, making more use of attach properties instead of separate templates.
    
    * Restored default filled ComboBox behaviour
    
    * Flip toggle arrow when drop down is open
    
    * Implemented outlined combo box template
    
    * Validation and clear button style improvements
    
    * Fixed corner radius issues
    
    Co-authored-by: Kevin Bost <kitokeboo@gmail.com>

--- MainDemo.Wpf/ComboBoxes.xaml.cs
+++ MainDemo.Wpf/ComboBoxes.xaml.cs
@@ -7,10 +7,13 @@ namespace MaterialDesignDemo
         public ComboBoxes()
         {
             InitializeComponent();
-            DataContext = new FieldsViewModel();
+            DataContext = new ComboBoxesViewModel();
         }
 
         private void ClearFilledComboBox_Click(object sender, System.Windows.RoutedEventArgs e)
             => FilledComboBox.SelectedItem = null;
+
+        private void ClearOutlinedComboBox_Click(object sender, System.Windows.RoutedEventArgs e)
+            => OutlinedComboBox.SelectedItem = null;
     }
 }
