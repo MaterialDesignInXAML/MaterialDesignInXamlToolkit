ff6671c5fbaca7598db784717fc8f8a3fb8f012a
Jan Ivar Z. Carlsen <jizc@outlook.com>
2021-02-19 06:12:25 +0100

    Added OnlyShowFocusVisualWhileDragging attached property (#2245)

---
+++ MaterialDesignThemes.Wpf/Converters/InvertBooleanConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace MaterialDesignThemes.Wpf.Converters
+{
+    internal class InvertBooleanConverter : BooleanConverter<bool>
+    {
+        public InvertBooleanConverter()
+            : base(false, true)
+        {
+        }
+    }
+}
