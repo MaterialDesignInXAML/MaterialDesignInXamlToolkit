5916f5120e955211455182161c68af8e2c7361a9
Kevin Bost <kitokeboo@gmail.com>
2021-02-18 21:30:08 -0800

    Fix format exception
    Fixes #2248

--- MaterialDesignThemes.Wpf/DrawerHostOpenMode.cs
+++ MaterialDesignThemes.Wpf/DrawerHostOpenMode.cs
@@ -1,8 +1,12 @@
-﻿namespace MaterialDesignThemes.Wpf
+﻿using System;
+
+namespace MaterialDesignThemes.Wpf
 {
     public enum DrawerHostOpenMode
     {
         Default = 0,
+        Modal = 0,
+        [Obsolete("Use DrawerHostOpenMode.Modal instead; will be removed in future release")]
         Model = 0,
         Standard = 1
     }
