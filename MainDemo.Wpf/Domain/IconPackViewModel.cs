c6774f186f4381182b50b38c673429b4bbb7d1a2
Kevin B <Keboo@users.noreply.github.com>
2021-03-18 23:25:27 -0700

    Adding icon generator to the demo app (#2284)
    * Adding icon generator to the demo app
    
    * Fixing build warnings

--- MainDemo.Wpf/Domain/IconPackViewModel.cs
+++ MainDemo.Wpf/Domain/IconPackViewModel.cs
@@ -4,7 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using MaterialDesignThemes.Wpf;
+using MaterialDesignDemo.Domain;
+using Microsoft.Win32;
+using BluwolfIcons;
+using System.Windows.Media.Imaging;
+using System.IO;
 
 namespace MaterialDesignDemo.Domain
 {
@@ -23,11 +29,21 @@ namespace MaterialDesignDemo.Domain
 
             _packIconKinds = new Lazy<IEnumerable<PackIconKindGroup>>(() =>
                 Enum.GetNames(typeof(PackIconKind))
-                    .GroupBy(k => (PackIconKind) Enum.Parse(typeof(PackIconKind), k))
+                    .GroupBy(k => (PackIconKind)Enum.Parse(typeof(PackIconKind), k))
                     .Select(g => new PackIconKindGroup(g))
                     .OrderBy(x => x.Kind)
                     .ToList());
+
+            var helper = new PaletteHelper();
+            if (helper.GetThemeManager() is { } themeManager)
+            {
+                themeManager.ThemeChanged += ThemeManager_ThemeChanged;
+            }
+            SetDefaultIconColors();
         }
+
+        private void ThemeManager_ThemeChanged(object? sender, ThemeChangedEventArgs e)
+            => SetDefaultIconColors();
 
         public ICommand OpenDotComCommand { get; }
         public ICommand SearchCommand { get; }
@@ -62,7 +78,9 @@ namespace MaterialDesignDemo.Domain
             set
             {
                 if (SetProperty(ref _kind, value))
-                    PackIconKind = value != null ? (PackIconKind) Enum.Parse(typeof(PackIconKind), value) : default;
+                {
+                    PackIconKind = value != null ? (PackIconKind)Enum.Parse(typeof(PackIconKind), value) : default;
+                }
             }
         }
 
@@ -73,15 +91,15 @@ namespace MaterialDesignDemo.Domain
         }
 
         private void OpenDotCom(object obj)
-        {
-            Link.OpenInBrowser("https://materialdesignicons.com/");
-        }
+            => Link.OpenInBrowser("https://materialdesignicons.com/");
 
         private async void Search(object obj)
         {
             var text = obj as string;
             if (string.IsNullOrWhiteSpace(text))
+            {
                 Kinds = _packIconKinds.Value;
+            }
             else
             {
                 Kinds = await Task.Run(() => _packIconKinds.Value
@@ -95,6 +113,76 @@ namespace MaterialDesignDemo.Domain
             var toBeCopied = $"<materialDesign:PackIcon Kind=\"{obj}\" />";
             Clipboard.SetDataObject(toBeCopied);
             _snackbarMessageQueue.Enqueue(toBeCopied + " copied to clipboard");
+        }
+
+        private void SetDefaultIconColors()
+        {
+            var helper = new PaletteHelper();
+            ITheme theme = helper.GetTheme();
+            GeneratedIconBackground = theme.Paper;
+            GeneratedIconForeground = theme.PrimaryMid.Color;
+        }
+
+        private Color _generatedIconBackground;
+        public Color GeneratedIconBackground
+        {
+            get => _generatedIconBackground;
+            set => SetProperty(ref _generatedIconBackground, value);
+        }
+
+        private Color _generatedIconForeground;
+        public Color GeneratedIconForeground
+        {
+            get => _generatedIconForeground;
+            set => SetProperty(ref _generatedIconForeground, value);
+        }
+
+        private ICommand? _saveIconCommand;
+        public ICommand SaveIconCommand => _saveIconCommand ??= new AnotherCommandImplementation(OnSaveIcon);
+
+        private void OnSaveIcon(object _)
+        {
+            var saveDialog = new SaveFileDialog
+            {
+                DefaultExt = ".ico",
+                Title = "Save Icon (.ico)",
+                Filter = "Icon Files|*.ico|All Files|*",
+                CheckPathExists = true,
+                OverwritePrompt = true,
+                RestoreDirectory = true
+            };
+            if (saveDialog.ShowDialog() != true) return;
+
+            var icon = new Icon();
+
+            //TODO: Make this size list configurable
+            foreach (var size in new[] { 256, 128, 64, 48, 32, 24, 16 })
+            {
+                RenderTargetBitmap bmp = RenderImage(size);
+                icon.Images.Add(new BmpIconImage(bmp));
+            }
+
+            icon.Save(saveDialog.FileName);
+
+            RenderTargetBitmap RenderImage(int size)
+            {
+                var packIcon = new PackIcon
+                {
+                    Kind = PackIconKind,
+                    Background = new SolidColorBrush(GeneratedIconBackground),
+                    Foreground = new SolidColorBrush(GeneratedIconForeground),
+                    Width = size,
+                    Height = size,
+                    Style = (Style)Application.Current.FindResource(typeof(PackIcon))
+                };
+                packIcon.Measure(new Size(size, size));
+                packIcon.Arrange(new Rect(0, 0, size, size));
+                packIcon.UpdateLayout();
+
+                RenderTargetBitmap bmp = new(size, size, 96, 96, PixelFormats.Pbgra32);
+                bmp.Render(packIcon);
+                return bmp;
+            }
         }
     }
 }
