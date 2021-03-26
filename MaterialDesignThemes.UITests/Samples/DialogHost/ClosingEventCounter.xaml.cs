cd884493eb67d93b458bece326671bb2bf319ca0
Kevin Bost <kitokeboo@gmail.com>
2021-03-25 22:55:10 -0700

    Closing Dialog with IsOpen property now raises Closing event
    Fixes #2282

---
+++ MaterialDesignThemes.UITests/Samples/DialogHost/ClosingEventCounter.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace MaterialDesignThemes.UITests.Samples.DialogHost
+{
+    /// <summary>
+    /// Interaction logic for ClosingEventCounter.xaml
+    /// </summary>
+    public partial class ClosingEventCounter : UserControl
+    {
+        public ClosingEventCounter()
+            => InitializeComponent();
+
+        private int _ClosingCount;
+        private void DialogHost_DialogClosing(object sender, Wpf.DialogClosingEventArgs eventArgs)
+            => ResultTextBlock.Text = (++_ClosingCount).ToString();
+    }
+}
