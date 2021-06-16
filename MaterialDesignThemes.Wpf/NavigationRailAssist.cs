8103d25bddf693459da2056119ccbc3f8855d758
Stefano Rivolta <stefano.rivolta@previero.it>
2021-06-16 11:43:25 +0200

    Add CornerRadius property for the selection
    also changed the selections to border, instead of rectangle

--- MaterialDesignThemes.Wpf/NavigationRailAssist.cs
+++ MaterialDesignThemes.Wpf/NavigationRailAssist.cs
@@ -18,13 +18,23 @@ namespace MaterialDesignThemes.Wpf
 
         #endregion
 
-        #region Property FloatingContent
+        #region Property ShowSelectionBackground
 
         public static readonly DependencyProperty ShowSelectionBackgroundProperty = DependencyProperty.RegisterAttached(
             "ShowSelectionBackground", typeof(bool), typeof(NavigationRailAssist), new PropertyMetadata(false));
 
         public static object GetShowSelectionBackground(DependencyObject element) => (bool)element.GetValue(ShowSelectionBackgroundProperty);
         public static void SetShowSelectionBackground(DependencyObject element, bool value) => element.SetValue(ShowSelectionBackgroundProperty, value);
+
+        #endregion
+
+        #region Property SelectionCornerRadius
+
+        public static readonly DependencyProperty SelectionUniformCornerRadiusProperty = DependencyProperty.RegisterAttached(
+            "SelectionUniformCornerRadius", typeof(CornerRadius), typeof(NavigationRailAssist), new PropertyMetadata(default(CornerRadius)));
+
+        public static object GetSelectionUniformCornerRadius(DependencyObject element) => (CornerRadius)element.GetValue(SelectionUniformCornerRadiusProperty);
+        public static void SetSelectionUniformCornerRadius(DependencyObject element, CornerRadius value) => element.SetValue(SelectionUniformCornerRadiusProperty, value);
 
         #endregion
     }
