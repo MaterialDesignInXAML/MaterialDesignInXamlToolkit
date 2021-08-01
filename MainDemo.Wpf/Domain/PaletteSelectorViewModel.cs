f8f18628f6787554627be2928925f651433a2f84
FantasyTeddy <matthiasbaer@bluewin.ch>
2021-08-01 11:48:53 +0200

    Allow modification of color adjustment values in demo application

--- MainDemo.Wpf/Domain/PaletteSelectorViewModel.cs
+++ MainDemo.Wpf/Domain/PaletteSelectorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
 using MaterialDesignColors;
 using MaterialDesignThemes.Wpf;
@@ -18,7 +19,14 @@ namespace MaterialDesignDemo.Domain
             IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark;
 
             if (theme is Theme internalTheme)
-                IsColorAdjusted = internalTheme.ColorAdjustment is not null;
+            {
+                _isColorAdjusted = internalTheme.ColorAdjustment is not null;
+
+                var colorAdjustment = internalTheme.ColorAdjustment ?? new ColorAdjustment();
+                _desiredContrastRatio = colorAdjustment.DesiredContrastRatio;
+                _contrastValue = colorAdjustment.Contrast;
+                _colorSelectionValue = colorAdjustment.Colors;
+            }
 
             if (paletteHelper.GetThemeManager() is { } themeManager)
             {
@@ -53,7 +61,71 @@ namespace MaterialDesignDemo.Domain
                     ModifyTheme(theme =>
                     {
                         if (theme is Theme internalTheme)
-                            internalTheme.ColorAdjustment = value ? new ColorAdjustment() : null;
+                        {
+                            internalTheme.ColorAdjustment = value
+                                ? new ColorAdjustment
+                                {
+                                    DesiredContrastRatio = DesiredContrastRatio,
+                                    Contrast = ContrastValue,
+                                    Colors = ColorSelectionValue
+                                }
+                                : null;
+                        }
+                    });
+                }
+            }
+        }
+
+        private float _desiredContrastRatio = 4.5f;
+        public float DesiredContrastRatio
+        {
+            get => _desiredContrastRatio;
+            set
+            {
+                if (SetProperty(ref _desiredContrastRatio, value))
+                {
+                    ModifyTheme(theme =>
+                    {
+                        if (theme is Theme internalTheme && internalTheme.ColorAdjustment != null)
+                            internalTheme.ColorAdjustment.DesiredContrastRatio = value;
+                    });
+                }
+            }
+        }
+
+        public IEnumerable<Contrast> ContrastValues => Enum.GetValues(typeof(Contrast)).Cast<Contrast>();
+
+        private Contrast _contrastValue;
+        public Contrast ContrastValue
+        {
+            get => _contrastValue;
+            set
+            {
+                if (SetProperty(ref _contrastValue, value))
+                {
+                    ModifyTheme(theme =>
+                    {
+                        if (theme is Theme internalTheme && internalTheme.ColorAdjustment != null)
+                            internalTheme.ColorAdjustment.Contrast = value;
+                    });
+                }
+            }
+        }
+
+        public IEnumerable<ColorSelection> ColorSelectionValues => Enum.GetValues(typeof(ColorSelection)).Cast<ColorSelection>();
+
+        private ColorSelection _colorSelectionValue;
+        public ColorSelection ColorSelectionValue
+        {
+            get => _colorSelectionValue;
+            set
+            {
+                if (SetProperty(ref _colorSelectionValue, value))
+                {
+                    ModifyTheme(theme =>
+                    {
+                        if (theme is Theme internalTheme && internalTheme.ColorAdjustment != null)
+                            internalTheme.ColorAdjustment.Colors = value;
                     });
                 }
             }
