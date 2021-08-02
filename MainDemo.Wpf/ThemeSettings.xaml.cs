ee4e9d1f7d32cd82b6125a8475f1167b7ba5714b
FantasyTeddy <matthiasbaer@bluewin.ch>
2021-08-02 19:43:30 +0200

    Refactor theme settings into separate control

---
+++ MainDemo.Wpf/ThemeSettings.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MaterialDesignDemo.Domain;
+
+namespace MaterialDesignDemo
+{
+    public partial class ThemeSettings
+    {
+        public ThemeSettings()
+        {
+            DataContext = new ThemeSettingsViewModel();
+            InitializeComponent();
+        }
+    }
+}
