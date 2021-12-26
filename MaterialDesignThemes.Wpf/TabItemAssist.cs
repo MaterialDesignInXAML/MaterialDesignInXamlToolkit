3f96c0f458c2845f9f90bd9bce6174d24c3afc00
Michel Michels <michel.michels.jr@gmail.com>
2021-12-26 11:57:15 +0100

    Tabs page in demo app rework, wip TopIcon for TabItem

---
+++ MaterialDesignThemes.Wpf/TabItemAssist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public static class TabItemAssist
+    {
+        #region AttachedProperty : TopIconProperty
+        public static readonly DependencyProperty TopIconProperty
+            = DependencyProperty.RegisterAttached("TopIcon", typeof(PackIconKind?), typeof(TabItemAssist),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.Inherits));
+
+        public static object GetTopIcon(DependencyObject element)
+            => element.GetValue(TopIconProperty);
+        public static void SetTopIcon(DependencyObject element, object value)
+            => element.SetValue(TopIconProperty, value);
+        #endregion
+
+    }
+}
