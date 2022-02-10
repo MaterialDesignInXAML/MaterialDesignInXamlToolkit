c6fd7e13a9392dbf69c9035cb7ef98f7a302629c
Stefano Rivolta <stefano.rivolta@previero.it>
2022-02-10 14:26:14 +0100

    Test Material Design 3 Listbox

--- MaterialDesignThemes.Wpf/ListBoxItemAssist.cs
+++ MaterialDesignThemes.Wpf/ListBoxItemAssist.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Drawing;
+using System.Windows;
 
 namespace MaterialDesignThemes.Wpf
 {
@@ -14,7 +15,8 @@ namespace MaterialDesignThemes.Wpf
         public static readonly DependencyProperty CornerRadiusProperty
             = DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(ListBoxItemAssist), new PropertyMetadata(DefaultCornerRadius));
 
-        public static CornerRadius GetCornerRadius(DependencyObject element) => (CornerRadius)element.GetValue(CornerRadiusProperty);
+        public static CornerRadius GetCornerRadius(DependencyObject element)
+            => (CornerRadius)element.GetValue(CornerRadiusProperty);
         public static void SetCornerRadius(DependencyObject element, CornerRadius value) => element.SetValue(CornerRadiusProperty, value);
         #endregion
 
@@ -26,6 +28,37 @@ namespace MaterialDesignThemes.Wpf
 
         public static readonly DependencyProperty ShowSelectionProperty =
             DependencyProperty.RegisterAttached("ShowSelection", typeof(bool), typeof(ListBoxItemAssist), new PropertyMetadata(true));
+        #endregion
+
+
+        #region SelectionHeight
+        public static int GetSelectionHeight(DependencyObject element)
+            => (int)element.GetValue(SelectionHeightProperty);
+        public static void SetSelectionHeight(DependencyObject element, int value)
+            => element.SetValue(SelectionHeightProperty, value);
+
+        public static readonly DependencyProperty SelectionHeightProperty =
+            DependencyProperty.RegisterAttached("SelectionHeight", typeof(int), typeof(ListBoxItemAssist), new PropertyMetadata(0));
+        #endregion
+
+        #region SelectionWidth
+        public static int GetSelectionWidth(DependencyObject element)
+            => (int)element.GetValue(SelectionWidthProperty);
+        public static void SetSelectionWidth(DependencyObject element, int value)
+            => element.SetValue(SelectionWidthProperty, value);
+
+        public static readonly DependencyProperty SelectionWidthProperty =
+            DependencyProperty.RegisterAttached("SelectionWidth", typeof(int), typeof(ListBoxItemAssist), new PropertyMetadata(0));
+        #endregion
+
+        #region SelectionCornerRadius
+        public static CornerRadius GetSelectionCornerRadius(DependencyObject element)
+            => (CornerRadius)element.GetValue(SelectionCornerRadiusProperty);
+        public static void SetSelectionCornerRadius(DependencyObject element, CornerRadius value)
+            => element.SetValue(SelectionCornerRadiusProperty, value);
+
+        public static readonly DependencyProperty SelectionCornerRadiusProperty =
+            DependencyProperty.RegisterAttached("SelectionCornerRadius", typeof(CornerRadius), typeof(ListBoxItemAssist), new PropertyMetadata(new CornerRadius(2.0)));
         #endregion
     }
 }
