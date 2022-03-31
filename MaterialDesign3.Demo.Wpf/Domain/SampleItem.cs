98f234b569930cfab46100ae3d284fd979b7f746
StefanoRivolta-Previero <58171461+StefanoRivolta-Previero@users.noreply.github.com>
2022-03-31 15:04:35 +0200

    Update MaterialDesign3.Demo.Wpf/Domain/SampleItem.cs
    Co-authored-by: Kevin B <Keboo@users.noreply.github.com>

--- MaterialDesign3.Demo.Wpf/Domain/SampleItem.cs
+++ MaterialDesign3.Demo.Wpf/Domain/SampleItem.cs
@@ -4,7 +4,7 @@ namespace MaterialDesign3Demo.Domain;
 
 public class SampleItem
 {
-    public string Title { get; set; }
+    public string? Title { get; set; }
     public PackIconKind SelectedIcon { get; set; }
     public PackIconKind UnselectedIcon { get; set; }
 }
