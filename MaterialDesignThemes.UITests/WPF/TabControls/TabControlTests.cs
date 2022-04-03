ec1af5705d9a92e728b574238c49dd03e1a3080c
Kevin B <Keboo@users.noreply.github.com>
2022-04-02 22:01:34 -0700

    Fix TabControl foreground color (#2639)
    * WIP
    
    Work on fixing tab control styles
    
    * Fixes 2602
    
    Addresses issues with tab control foreground not being set.

---
+++ MaterialDesignThemes.UITests/WPF/TabControls/TabControlTests.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using XamlTest;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MaterialDesignThemes.UITests.WPF.TabControls;
+
+public class TabControlTests : TestBase
+{
+    public TabControlTests(ITestOutputHelper output)
+        : base(output)
+    { }
+
+    [Fact]
+    [Description("Issue 2602")]
+    public async Task OnLoad_ThemeBrushesSet()
+    {
+        await using var recorder = new TestRecorder(App);
+
+        //Arrange
+        IVisualElement<TabControl> tabControl = await LoadXaml<TabControl>(@"
+<TabControl 
+        materialDesign:ColorZoneAssist.Mode=""PrimaryMid""
+        Style=""{StaticResource MaterialDesignFilledTabControl}"">
+    <TabItem Header=""TAB 1"">
+        <TextBlock Margin=""8"" Text=""PrimaryMid Tab 1"" />
+    </TabItem>
+    <TabItem Header=""TAB 2"">
+        <TextBlock Margin=""8"" Text=""PrimaryMid Tab 2"" />
+    </TabItem>
+</TabControl> ");
+
+        IVisualElement<TextBlock> textBlock = await tabControl.GetElement<TextBlock>(@"/TabItem[0]/TextBlock[0]");
+
+        //Act
+        Color? foreground = await textBlock.GetForegroundColor();
+        Color? background = await textBlock.GetEffectiveBackground();
+
+        //Assert
+        Assert.NotNull(foreground);
+        Assert.NotNull(background);
+
+        MaterialDesignSpec.AssertContrastRatio(foreground.Value, background.Value, MaterialDesignSpec.MinimumContrastSmallText);
+
+        recorder.Success();
+    }
+}
