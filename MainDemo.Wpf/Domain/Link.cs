53cde6721d2e6227857155b96b9226be05d66fea
Kevin Bost <kitokeboo@gmail.com>
2022-04-02 22:46:01 -0700

    Fixing nullability warnings

--- MainDemo.Wpf/Domain/Link.cs
+++ MainDemo.Wpf/Domain/Link.cs
@@ -5,8 +5,9 @@ namespace MaterialDesignDemo.Domain
 {
     public static class Link
     {
-        public static void OpenInBrowser(string url)
+        public static void OpenInBrowser(string? url)
         {
+            if (url is null) return;
             //https://github.com/dotnet/corefx/issues/10361
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
