a6339b4a25ca27fb93d59d88f4ee8e5138df03e5
Kevin Bost <kitokeboo@gmail.com>
2022-04-14 21:40:26 -0700

    Fixing issue with tab control not showing its underline

--- MaterialDesignThemes.Wpf/ColorZoneAssist.cs
+++ MaterialDesignThemes.Wpf/ColorZoneAssist.cs
@@ -1,47 +1,34 @@
 ﻿using System.Windows;
 using System.Windows.Media;
 
-namespace MaterialDesignThemes.Wpf
+namespace MaterialDesignThemes.Wpf;
+
+public static class ColorZoneAssist
 {
-    public static class ColorZoneAssist
-    {
-        public static readonly DependencyProperty ModeProperty = DependencyProperty.RegisterAttached(
-            "Mode", typeof(ColorZoneMode), typeof(ColorZoneAssist), new FrameworkPropertyMetadata(default(ColorZoneMode), FrameworkPropertyMetadataOptions.Inherits));
+    public static readonly DependencyProperty ModeProperty = DependencyProperty.RegisterAttached(
+        "Mode", typeof(ColorZoneMode), typeof(ColorZoneAssist), new FrameworkPropertyMetadata(default(ColorZoneMode), FrameworkPropertyMetadataOptions.Inherits));
 
-        public static void SetMode(DependencyObject element, ColorZoneMode value)
-        {
-            element.SetValue(ModeProperty, value);
-        }
+    public static void SetMode(DependencyObject element, ColorZoneMode value)
+        => element.SetValue(ModeProperty, value);
 
-        public static ColorZoneMode GetMode(DependencyObject element)
-        {
-            return (ColorZoneMode)element.GetValue(ModeProperty);
-        }
+    public static ColorZoneMode GetMode(DependencyObject element)
+        => (ColorZoneMode)element.GetValue(ModeProperty);
 
-        public static readonly DependencyProperty BackgroundProperty = DependencyProperty.RegisterAttached(
-            "Background", typeof(Brush), typeof(ColorZoneAssist), new FrameworkPropertyMetadata(default(Brush)));
+    public static readonly DependencyProperty BackgroundProperty = DependencyProperty.RegisterAttached(
+        "Background", typeof(Brush), typeof(ColorZoneAssist), new FrameworkPropertyMetadata(default(Brush)));
 
-        public static void SetBackground(DependencyObject element, Brush value)
-        {
-            element.SetValue(BackgroundProperty, value);
-        }
+    public static void SetBackground(DependencyObject element, Brush value)
+        => element.SetValue(BackgroundProperty, value);
 
-        public static Brush GetBackground(DependencyObject element)
-        {
-            return (Brush)element.GetValue(BackgroundProperty);
-        }
+    public static Brush GetBackground(DependencyObject element)
+        => (Brush)element.GetValue(BackgroundProperty);
 
-        public static readonly DependencyProperty ForegroundProperty = DependencyProperty.RegisterAttached(
-            "Foreground", typeof(Brush), typeof(ColorZoneAssist), new FrameworkPropertyMetadata(default(Brush)));
+    public static readonly DependencyProperty ForegroundProperty = DependencyProperty.RegisterAttached(
+        "Foreground", typeof(Brush), typeof(ColorZoneAssist), new FrameworkPropertyMetadata(default(Brush)));
 
-        public static void SetForeground(DependencyObject element, Brush value)
-        {
-            element.SetValue(ForegroundProperty, value);
-        }
+    public static void SetForeground(DependencyObject element, Brush value)
+        => element.SetValue(ForegroundProperty, value);
 
-        public static Brush GetForeground(DependencyObject element)
-        {
-            return (Brush)element.GetValue(ForegroundProperty);
-        }
-    }
+    public static Brush GetForeground(DependencyObject element)
+        => (Brush)element.GetValue(ForegroundProperty);
 }
