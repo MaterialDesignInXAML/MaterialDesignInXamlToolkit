f6fd82f6cdff3a2b1a79d681472bd619c3f0b279
Xaalek <54487782+Xaalek@users.noreply.github.com>
2022-04-22 15:41:37 +0200

    add menu assist class

---
+++ MaterialDesignThemes.Wpf/MenuAssist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public static class MenuAssist
+    {
+        private static readonly int DefaulTopLevelMenuItemHeight = 48;
+
+        #region AttachedProperty : TopLevelMenuItemHeight
+        public static readonly DependencyProperty TopLevelMenuItemHeightProperty
+            = DependencyProperty.RegisterAttached(
+                "TopLevelMenuItemHeight",
+                typeof(int),
+                typeof(MenuAssist),
+                new PropertyMetadata(DefaulTopLevelMenuItemHeight)
+                );
+
+        public static int GetTopLevelMenuItemHeight(DependencyObject element) => (int)element.GetValue(TopLevelMenuItemHeightProperty);
+        public static void SetTopLevelMenuItemHeight(DependencyObject element, int value) => element.SetValue(TopLevelMenuItemHeightProperty, value);
+        #endregion
+    }
+}
