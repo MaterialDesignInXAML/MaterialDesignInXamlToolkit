2d06d3acc4459201be49697c9289d9fdf06ae163
Xaalek <54487782+Xaalek@users.noreply.github.com>
2022-04-26 06:12:57 +0200

    Better control of menu height (#2657)
    * add menu assist class
    
    * change int to double + test
    
    * working property
    
    * restructuring demo

---
+++ MaterialDesignThemes.Wpf/MenuAssist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MaterialDesignThemes.Wpf
+{
+    public static class MenuAssist
+    {
+        #region AttachedProperty : TopLevelMenuItemHeight
+        public static readonly DependencyProperty TopLevelMenuItemHeightProperty
+            = DependencyProperty.RegisterAttached(
+                "TopLevelMenuItemHeight",
+                typeof(double),
+                typeof(MenuAssist));
+
+        public static double GetTopLevelMenuItemHeight(DependencyObject element) => (double)element.GetValue(TopLevelMenuItemHeightProperty);
+        public static void SetTopLevelMenuItemHeight(DependencyObject element, double value) => element.SetValue(TopLevelMenuItemHeightProperty, value);
+        #endregion
+    }
+}
