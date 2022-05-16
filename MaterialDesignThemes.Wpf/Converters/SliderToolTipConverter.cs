dd088c6d1e7c9a87d8555fafb526f2d20a79b2f8
Kevin Bost <kitokeboo@gmail.com>
2022-05-16 15:53:57 -0700

    Making the converter internal

--- MaterialDesignThemes.Wpf/Converters/SliderToolTipConverter.cs
+++ MaterialDesignThemes.Wpf/Converters/SliderToolTipConverter.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 using System.Globalization;
-using System.Text.RegularExpressions;
 using System.Windows.Data;
 
-namespace MaterialDesignThemes.Wpf.Converters
-{
-    public class SliderToolTipConverter : IMultiValueConverter
-    {
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (values.Length >= 2 && values[1] is string format && !string.IsNullOrEmpty(format))
-                try
-                {
-                    return string.Format(culture, format, values[0]);
-                }
-                catch (FormatException) { }
-            return System.Convert.ChangeType(values[0], targetType, culture);
-        }
+namespace MaterialDesignThemes.Wpf.Converters;
 
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-            => throw new NotImplementedException();
+internal class SliderToolTipConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.Length >= 2 && values[1] is string format && !string.IsNullOrEmpty(format))
+        {
+            try
+            {
+                return string.Format(culture, format, values[0]);
+            }
+            catch (FormatException) { }
+        }
+        return System.Convert.ChangeType(values[0], targetType, culture);
     }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
 }
