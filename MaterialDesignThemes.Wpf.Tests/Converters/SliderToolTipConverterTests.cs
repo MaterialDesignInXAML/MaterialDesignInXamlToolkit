920293a08f8dc0619d380ed0da50b9bf96a94680
Kevin Bost <kitokeboo@gmail.com>
2022-05-16 17:20:12 -0700

    Handling NRT warnings

--- MaterialDesignThemes.Wpf.Tests/Converters/SliderToolTipConverterTests.cs
+++ MaterialDesignThemes.Wpf.Tests/Converters/SliderToolTipConverterTests.cs
@@ -15,22 +15,22 @@ namespace MaterialDesignThemes.Wpf.Tests.Converters
             Wpf.Converters.SliderToolTipConverter converter = new Wpf.Converters.SliderToolTipConverter();
 
             //test a valid case
-            var result = converter.Convert(new object[] { value, "Test String Format {0}" }, typeof(string), null, CultureInfo.CurrentCulture);
+            var result = converter.Convert(new object?[] { value, "Test String Format {0}" }, typeof(string), null, CultureInfo.CurrentCulture);
             Assert.Equal($"Test String Format {value}", result);
 
             //test too many placeholders in format string
-            result = converter.Convert(new object[] { value, "{0} {1}" }, typeof(string), null, CultureInfo.CurrentCulture);
+            result = converter.Convert(new object?[] { value, "{0} {1}" }, typeof(string), null, CultureInfo.CurrentCulture);
             Assert.Equal(value.ToString(), result);
 
-            result = converter.Convert(new object[] { value, "{0} {1} {2}" }, typeof(string), null, CultureInfo.CurrentCulture);
+            result = converter.Convert(new object?[] { value, "{0} {1} {2}" }, typeof(string), null, CultureInfo.CurrentCulture);
             Assert.Equal(value.ToString(), result);
 
             //test empty format string
-            result = converter.Convert(new object[] { value, "" }, typeof(string), null, CultureInfo.CurrentCulture);
+            result = converter.Convert(new object?[] { value, "" }, typeof(string), null, CultureInfo.CurrentCulture);
             Assert.Equal(value.ToString(), result);
 
             //test null format string
-            result = converter.Convert(new object[] { value, null }, typeof(string), null, CultureInfo.CurrentCulture);
+            result = converter.Convert(new object?[] { value, null }, typeof(string), null, CultureInfo.CurrentCulture);
             Assert.Equal(value.ToString(), result);
         }
     }
--- MaterialDesignThemes.Wpf/Converters/SliderToolTipConverter.cs
+++ MaterialDesignThemes.Wpf/Converters/SliderToolTipConverter.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace MaterialDesignThemes.Wpf.Converters;
 
 internal class SliderToolTipConverter : IMultiValueConverter
 {
-    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    public object? Convert(object?[]? values, Type? targetType, object? parameter, CultureInfo? culture)
     {
-        if (values.Length >= 2 && values[1] is string format && !string.IsNullOrEmpty(format))
+        if (values?.Length >= 2 && values[1] is string format && !string.IsNullOrEmpty(format))
         {
             try
             {
@@ -16,7 +17,11 @@ internal class SliderToolTipConverter : IMultiValueConverter
             }
             catch (FormatException) { }
         }
-        return System.Convert.ChangeType(values[0], targetType, culture);
+        if (values?.Length >= 1 && targetType is not null)
+        {
+            return System.Convert.ChangeType(values[0], targetType, culture);
+        }
+        return DependencyProperty.UnsetValue;
     }
 
     public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
