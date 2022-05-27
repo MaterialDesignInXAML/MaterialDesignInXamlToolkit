729d1412a248ba2cb02b8b1c9ea5a5289ef664f2
Kevin B <Keboo@users.noreply.github.com>
2022-05-26 22:51:51 -0700

    Adding new TimeChanged event to Clock (#2716)
    * Adding new TimeChanged event to Clock
    
    Fixes #2698
    
    * Fixing nullability of parameter

---
+++ MaterialDesignThemes.Wpf/TimeChangedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace MaterialDesignThemes.Wpf;
+
+public sealed class TimeChangedEventArgs : RoutedEventArgs
+{
+    public DateTime OldTime { get; }
+    public DateTime NewTime { get; }
+
+    public TimeChangedEventArgs(RoutedEvent routedEvent, DateTime oldTime, DateTime newTime)
+        : base(routedEvent)
+    {
+        OldTime = oldTime;
+        NewTime = newTime;
+    }
+}
