6cc900d24cb5ff5190a32699ee6f92c5a1fd2982
Nicolai Henriksen <nicolai.h80@gmail.com>
2022-07-02 01:26:04 +0200

    First unit test for OutlinedCard added

---
+++ MaterialDesignThemes.UITests/WPF/Cards/OutlinedCardTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Media;
+
+namespace MaterialDesignThemes.UITests.WPF.Cards;
+
+public class OutlinedCardTests : TestBase
+{
+    public OutlinedCardTests(ITestOutputHelper output)
+        : base(output)
+    { }
+
+    [Fact]
+    public async Task OutlinedCard_UsesThemeColorForBorder()
+    {
+        await using var recorder = new TestRecorder(App);
+
+        //Arrange
+        IVisualElement<Card> card = await LoadXaml<Card>(
+            @"<materialDesign:Card Content=""Hello World"" Style=""{StaticResource MaterialDesignOutlinedCard}""/>");
+        Color dividerColor = await GetThemeColor("MaterialDesignDivider");
+        IVisualElement<Border> internalBorder = await card.GetElement<Border>();
+
+        //Act
+        Color? internalBorderColor = await internalBorder.GetBorderBrushColor();
+
+        //Assert
+        Assert.Equal(dividerColor, internalBorderColor);
+
+        recorder.Success();
+    }
+}
