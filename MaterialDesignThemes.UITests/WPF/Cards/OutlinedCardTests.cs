3f50f009ecdeedc6eef05c98f88322be9fac3218
Nicolai Henriksen <nicolai.h80@gmail.com>
2022-07-05 09:07:44 +0200

    Fix2658 (#2757)
    * Added card styles and sample
    
    Default card style is now MaterialDesignElevatedCard and a new alternative style, MaterialDesignOutlineCard added.
    
    * First unit test for OutlinedCard added
    
    * More unit tests
    
    Also ensured the DoubleToCornerRadiusConverter returns a CornerRadius of 0 for negative values.

---
+++ MaterialDesignThemes.UITests/WPF/Cards/OutlinedCardTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Media;
+
+namespace MaterialDesignThemes.UITests.WPF.Cards;
+
+public class OutlinedCardTests : TestBase
+{
+    public OutlinedCardTests(ITestOutputHelper output)
+        : base(output)
+    { }
+
+    [Fact]
+    public async Task OutlinedCard_UsesThemeColorForBorder()
+    {
+        await using var recorder = new TestRecorder(App);
+
+        //Arrange
+        IVisualElement<Card> card = await LoadXaml<Card>(
+            @"<materialDesign:Card Content=""Hello World"" Style=""{StaticResource MaterialDesignOutlinedCard}""/>");
+        Color dividerColor = await GetThemeColor("MaterialDesignDivider");
+        IVisualElement<Border> internalBorder = await card.GetElement<Border>();
+
+        //Act
+        Color? internalBorderColor = await internalBorder.GetBorderBrushColor();
+
+        //Assert
+        Assert.Equal(dividerColor, internalBorderColor);
+
+        recorder.Success();
+    }
+
+    [Fact]
+    public async Task OutlinedCard_UniformCornerRadiusApplied_AppliesCornerRadiusOnBorder()
+    {
+        await using var recorder = new TestRecorder(App);
+
+        //Arrange
+        IVisualElement<Card> card = await LoadXaml<Card>(
+            @"<materialDesign:Card Content=""Hello World"" Style=""{StaticResource MaterialDesignOutlinedCard}"" UniformCornerRadius=""5"" />");
+        IVisualElement<Border> internalBorder = await card.GetElement<Border>();
+
+        //Act
+        CornerRadius? internalBorderCornerRadius = await internalBorder.GetCornerRadius();
+
+        //Assert
+        Assert.Equal(5, internalBorderCornerRadius.Value.TopLeft);
+        Assert.Equal(5, internalBorderCornerRadius.Value.TopRight);
+        Assert.Equal(5, internalBorderCornerRadius.Value.BottomRight);
+        Assert.Equal(5, internalBorderCornerRadius.Value.BottomLeft);
+
+        recorder.Success();
+    }
+}
---
+++ MaterialDesignThemes.Wpf/Converters/DoubleToCornerRadiusConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters;
+
+internal class DoubleToCornerRadiusConverter : IValueConverter
+{
+    public static readonly DoubleToCornerRadiusConverter Instance = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => new CornerRadius(Math.Max(0, (double)value));
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
