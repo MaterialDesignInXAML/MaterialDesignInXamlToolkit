798b6bb33174b70e0d4f6db09c439b42fa701754
Nicolai Henriksen <nicolai.h80@gmail.com>
2022-07-06 16:05:48 +0200

    Added unit test for coercion

---
+++ MaterialDesignThemes.Wpf.Tests/RatingBarTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace MaterialDesignThemes.Wpf.Tests;
+
+public class RatingBarTests
+{
+    [Theory]
+    [InlineData(-5, 0.0)]
+    [InlineData(5, 5.0)]
+    [InlineData(15, 10.0)]
+    [InlineData(0.2, 0.0)]
+    [InlineData(0.3, 0.5)]
+    [InlineData(0.7, 0.5)]
+    [InlineData(0.8, 1.0)]
+    public void SetValue_CoercesToCorrectMultipleAndStaysWithinBounds(double valueToSet, double expectedValue)
+    {
+        // Arrange
+        RatingBar ratingBar = new() { Min = 0, Max = 10};
+
+        // Act
+        ratingBar.Value = valueToSet;
+
+        // Assert
+        Assert.Equal(expectedValue, ratingBar.Value);
+    }
+}
