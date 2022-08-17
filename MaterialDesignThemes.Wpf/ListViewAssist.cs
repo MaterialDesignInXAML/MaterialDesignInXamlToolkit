4cd847f53c1d0b292583f9bfbfe3573082f65a03
Mike Christiansen <37249876+binarycow@users.noreply.github.com>
2022-08-16 20:40:26 -0400

    Add ListViewAssist.HeaderRowBackgroundProperty (#2807)

--- MaterialDesignThemes.Wpf/ListViewAssist.cs
+++ MaterialDesignThemes.Wpf/ListViewAssist.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 
 namespace MaterialDesignThemes.Wpf
 {
@@ -18,6 +19,22 @@ namespace MaterialDesignThemes.Wpf
         public static Thickness GetListViewItemPadding(DependencyObject element)
         {
             return (Thickness)element.GetValue(ListViewItemPaddingProperty);
+        }
+
+        public static readonly DependencyProperty HeaderRowBackgroundProperty = DependencyProperty.RegisterAttached(
+            "HeaderRowBackground",
+            typeof(Brush),
+            typeof(ListViewAssist),
+            new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.Inherits));
+
+        public static void SetHeaderRowBackground(DependencyObject element, Brush value)
+        {
+            element.SetValue(HeaderRowBackgroundProperty, value);
+        }
+
+        public static Brush GetHeaderRowBackground(DependencyObject element)
+        {
+            return (Brush)element.GetValue(HeaderRowBackgroundProperty);
         }
     }
 }
