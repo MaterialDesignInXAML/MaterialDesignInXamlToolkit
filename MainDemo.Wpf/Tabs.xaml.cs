ee22d6f1f5533e7dea94bb6df4bd27fc3bf25b35
Nicolai Henriksen <nicolai.h80@gmail.com>
2022-08-18 19:48:03 +0200

    Fixed template selector bug in TabItem style

--- MainDemo.Wpf/Tabs.xaml.cs
+++ MainDemo.Wpf/Tabs.xaml.cs
@@ -1,28 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using MaterialDesignDemo.Domain;
 
-namespace MaterialDesignDemo
+namespace MaterialDesignDemo;
+
+/// <summary>
+/// Interaction logic for Tabs.xaml
+/// </summary>
+public partial class Tabs
 {
-    /// <summary>
-    /// Interaction logic for Tabs.xaml
-    /// </summary>
-    public partial class Tabs : UserControl
+    public Tabs()
     {
-        public Tabs()
-        {
-            InitializeComponent();
-        }
+        DataContext = new TabsViewModel();
+        InitializeComponent();
     }
 }
