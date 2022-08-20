33f6bbe4a7c7988a00b090f3ce809c49b7af9096
Nicolai Henriksen <nicolai.h80@gmail.com>
2022-08-20 20:56:07 +0200

    Fix for issue 2816 - Fixed template selector bug in TabItem styles (#2818)
    * Fixed template selector bug in TabItem style
    
    * Fixing same issue for RailTabItem

---
+++ MainDemo.Wpf/Domain/TabsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MaterialDesignDemo.Domain;
+
+internal class TabsViewModel : ViewModelBase
+{
+    public ObservableCollection<CustomTab> CustomTabs { get; }
+
+    public CustomTab? SelectedTab { get; set; }
+
+    public TabsViewModel() =>
+        CustomTabs = new()
+        {
+            new CustomTab {CustomHeader = "Custom tab 1", CustomContent = "Custom tab 1 content"},
+            new CustomTab {CustomHeader = "Custom tab 2", CustomContent = "Custom tab 2 content"}
+        };
+}
+
+internal class CustomTab
+{
+    public string? CustomHeader { get; set; }
+
+    public string? CustomContent { get; set; }
+}
