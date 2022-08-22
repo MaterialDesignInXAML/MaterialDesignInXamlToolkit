7a10441312d3a5df7ab34a9f2eb7c01e2a61fc47
Nicolai Henriksen <nicolai.h80@gmail.com>
2022-08-22 14:15:20 +0200

    Adjusted error text left margin for filled text fields
    ComboBoxes, DatePicker and TimePicker all rely on the fields internally, and thus inherit these changes.

--- MainDemo.Wpf/Domain/FieldsViewModel.cs
+++ MainDemo.Wpf/Domain/FieldsViewModel.cs
@@ -4,6 +4,10 @@
     {
         private string? _name;
         private string? _name2;
+        private string? _text1;
+        private string? _text2;
+        private string? _password1 = "password";
+        private string? _password2 = "password";
 
         public string? Name
         {
@@ -15,6 +19,40 @@
         {
             get => _name2;
             set => SetProperty(ref _name2, value);
+        }
+
+        public string? Text1
+        {
+            get => _text1;
+            set => SetProperty(ref _text1, value);
+        }
+
+        public string? Text2
+        {
+            get => _text2;
+            set => SetProperty(ref _text2, value);
+        }
+
+        public string? Password1
+        {
+            get => _password1;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Password cannot be empty");
+                SetProperty(ref _password1, value);
+            }
+        }
+
+        public string? Password2
+        {
+            get => _password2;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Password cannot be empty");
+                SetProperty(ref _password2, value);
+            }
         }
 
         public FieldsTestObject TestObject => new() { Name = "Mr. Test" };
---
+++ MainDemo.Wpf/Domain/PasswordHelper.cs
@@ -0,0 +1,69 @@
+﻿namespace MaterialDesignDemo.Domain;
+
+/// <summary>
+/// Simple helper class to enable data-binding for <see cref="PasswordBox.Password"/> which is not a <see cref="DependencyProperty"/>
+/// </summary>
+internal static class PasswordHelper
+{
+    public static readonly DependencyProperty PasswordProperty =
+        DependencyProperty.RegisterAttached("Password",
+        typeof(string), typeof(PasswordHelper),
+        new FrameworkPropertyMetadata(string.Empty, OnPasswordPropertyChanged));
+
+    public static readonly DependencyProperty AttachProperty =
+        DependencyProperty.RegisterAttached("Attach",
+        typeof(bool), typeof(PasswordHelper), new PropertyMetadata(false, Attach));
+
+    private static readonly DependencyProperty IsUpdatingProperty =
+       DependencyProperty.RegisterAttached("IsUpdating", typeof(bool),
+       typeof(PasswordHelper));
+    
+    public static void SetAttach(DependencyObject dp, bool value) => dp.SetValue(AttachProperty, value);
+
+    public static bool GetAttach(DependencyObject dp) => (bool)dp.GetValue(AttachProperty);
+
+    public static string GetPassword(DependencyObject dp) => (string)dp.GetValue(PasswordProperty);
+
+    public static void SetPassword(DependencyObject dp, string value) => dp.SetValue(PasswordProperty, value);
+
+    private static bool GetIsUpdating(DependencyObject dp) => (bool)dp.GetValue(IsUpdatingProperty);
+
+    private static void SetIsUpdating(DependencyObject dp, bool value) => dp.SetValue(IsUpdatingProperty, value);
+
+    private static void OnPasswordPropertyChanged(DependencyObject sender,
+        DependencyPropertyChangedEventArgs e)
+    {
+        PasswordBox passwordBox = (PasswordBox) sender;
+        passwordBox.PasswordChanged -= PasswordChanged;
+
+        if (!GetIsUpdating(passwordBox))
+        {
+            passwordBox.Password = (string)e.NewValue;
+        }
+        passwordBox.PasswordChanged += PasswordChanged;
+    }
+
+    private static void Attach(DependencyObject sender,
+        DependencyPropertyChangedEventArgs e)
+    {
+        PasswordBox passwordBox = (PasswordBox) sender;
+
+        if ((bool)e.OldValue)
+        {
+            passwordBox.PasswordChanged -= PasswordChanged;
+        }
+
+        if ((bool)e.NewValue)
+        {
+            passwordBox.PasswordChanged += PasswordChanged;
+        }
+    }
+
+    private static void PasswordChanged(object sender, RoutedEventArgs e)
+    {
+        PasswordBox passwordBox = (PasswordBox) sender;
+        SetIsUpdating(passwordBox, true);
+        SetPassword(passwordBox, passwordBox.Password);
+        SetIsUpdating(passwordBox, false);
+    }
+}
