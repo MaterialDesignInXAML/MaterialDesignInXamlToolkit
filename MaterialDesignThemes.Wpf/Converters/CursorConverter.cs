023584d5eecb69e68c2968f2d0c45444cf87c7dd
Nicolai Henriksen <nicolai.h80@gmail.com>
2022-08-26 16:17:31 +0200

    Cursor fix
    Respects a cursor set from outside, otherwise falls back to the desired defaults

---
+++ MaterialDesignThemes.Wpf/Converters/CursorConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using Cursor = System.Windows.Input.Cursor;
+
+namespace MaterialDesignThemes.Wpf.Converters;
+
+/// <summary>
+/// Value converter that uses the Cursor from the bound property if set, otherwise it returns the <see cref="FallbackCursor"/>.
+/// </summary>
+public sealed class CursorConverter : IValueConverter
+{
+    public Cursor FallbackCursor { get; set; } = Cursors.Arrow;
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value as Cursor ?? FallbackCursor;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
