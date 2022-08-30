0fc56350f595ed540fed8a8df2ddd08feac094f3
Kevin Bost <kitokeboo@gmail.com>
2022-08-29 21:44:51 -0700

    Added template column example
    Fixed issue where it would not switch to edit on click

--- MainDemo.Wpf/Domain/SelectableViewModel.cs
+++ MainDemo.Wpf/Domain/SelectableViewModel.cs
@@ -1,55 +1,54 @@
-﻿namespace MaterialDesignDemo.Domain
+﻿namespace MaterialDesignDemo.Domain;
+
+public class SelectableViewModel : ViewModelBase
 {
-    public class SelectableViewModel : ViewModelBase
+    private bool _isSelected;
+    private string? _name;
+    private string? _description;
+    private char _code;
+    private double _numeric;
+    private string? _food;
+    private string? _files;
+
+    public bool IsSelected
     {
-        private bool _isSelected;
-        private string? _name;
-        private string? _description;
-        private char _code;
-        private double _numeric;
-        private string? _food;
-        private string? _files;
+        get => _isSelected;
+        set => SetProperty(ref _isSelected, value);
+    }
 
-        public bool IsSelected
-        {
-            get => _isSelected;
-            set => SetProperty(ref _isSelected, value);
-        }
+    public char Code
+    {
+        get => _code;
+        set => SetProperty(ref _code, value);
+    }
 
-        public char Code
-        {
-            get => _code;
-            set => SetProperty(ref _code, value);
-        }
+    public string? Name
+    {
+        get => _name;
+        set => SetProperty(ref _name, value);
+    }
 
-        public string? Name
-        {
-            get => _name;
-            set => SetProperty(ref _name, value);
-        }
+    public string? Description
+    {
+        get => _description;
+        set => SetProperty(ref _description, value);
+    }
 
-        public string? Description
-        {
-            get => _description;
-            set => SetProperty(ref _description, value);
-        }
+    public double Numeric
+    {
+        get => _numeric;
+        set => SetProperty(ref _numeric, value);
+    }
 
-        public double Numeric
-        {
-            get => _numeric;
-            set => SetProperty(ref _numeric, value);
-        }
+    public string? Food
+    {
+        get => _food;
+        set => SetProperty(ref _food, value);
+    }
 
-        public string? Food
-        {
-            get => _food;
-            set => SetProperty(ref _food, value);
-        }
-
-        public string? Files
-        {
-            get => _files;
-            set => SetProperty(ref _files, value);
-        }
+    public string? Files
+    {
+        get => _files;
+        set => SetProperty(ref _files, value);
     }
 }
