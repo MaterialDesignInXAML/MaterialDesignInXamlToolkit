bfbd3d54a69d6eae03c7d67fd37e7d5b2916885f
Nicolai Henriksen <nicolai.h80@gmail.com>
2022-11-07 21:47:31 +0100

    Yet another attempt at getting test window shown

--- MaterialDesignThemes.UITests/Samples/PasswordBox/BoundPasswordBoxWindow.xaml.cs
+++ MaterialDesignThemes.UITests/Samples/PasswordBox/BoundPasswordBoxWindow.xaml.cs
@@ -4,5 +4,11 @@ public partial class BoundPasswordBoxWindow
 {
     public BoundPasswordBoxWindow() => InitializeComponent();
 
-    private void BoundPasswordBoxWindow_OnLoaded(object sender, RoutedEventArgs e) => Activate();
+    private void BoundPasswordBoxWindow_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        Activate();
+        Topmost = true;
+        Topmost = false;
+        Focus();
+    }
 }
