7f623e11ee889b53b2cb00a0343fb1695552437d
MichelMichels <michel@miche.ls>
2022-11-09 20:46:20 +0100

    Fixes the fast switching bug

--- MaterialDesignThemes.Wpf/Transitions/CircleWipe.cs
+++ MaterialDesignThemes.Wpf/Transitions/CircleWipe.cs
@@ -6,15 +6,33 @@ namespace MaterialDesignThemes.Wpf.Transitions
 {
     public class CircleWipe : ITransitionWipe
     {
+        private KeyTime startKeyTime, midKeyTime, endKeyTime;
+
+        public CircleWipe()
+        {
+            startKeyTime = KeyTime.FromTimeSpan(TimeSpan.Zero);
+            midKeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(200));
+            endKeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(400));
+        }
+
         public void Wipe(TransitionerSlide fromSlide, TransitionerSlide toSlide, Point origin, IZIndexController zIndexController)
         {
-            if (fromSlide == null) throw new ArgumentNullException(nameof(fromSlide));
-            if (toSlide == null) throw new ArgumentNullException(nameof(toSlide));
-            if (zIndexController == null) throw new ArgumentNullException(nameof(zIndexController));
+            if (fromSlide == null)
+            {
+                throw new ArgumentNullException(nameof(fromSlide));
+            }
+            if (toSlide == null)
+            {
+                throw new ArgumentNullException(nameof(toSlide));
+            }
+            if (zIndexController == null)
+            {
+                throw new ArgumentNullException(nameof(zIndexController));
+            }
 
-            var horizontalProportion = Math.Max(1.0 - origin.X, 1.0 * origin.X);
-            var verticalProportion = Math.Max(1.0 - origin.Y, 1.0 * origin.Y);
-            var radius = Math.Sqrt(Math.Pow(toSlide.ActualWidth * horizontalProportion, 2) + Math.Pow(toSlide.ActualHeight * verticalProportion, 2));
+            double horizontalProportion = Math.Max(1.0 - origin.X, 1.0 * origin.X);
+            double verticalProportion = Math.Max(1.0 - origin.Y, 1.0 * origin.Y);
+            double radius = Math.Sqrt(Math.Pow(toSlide.ActualWidth * horizontalProportion, 2) + Math.Pow(toSlide.ActualHeight * verticalProportion, 2));
 
             var scaleTransform = new ScaleTransform(0, 0);
             var translateTransform = new TranslateTransform(toSlide.ActualWidth * origin.X, toSlide.ActualHeight * origin.Y);
@@ -31,35 +49,42 @@ namespace MaterialDesignThemes.Wpf.Transitions
             toSlide.SetCurrentValue(UIElement.ClipProperty, ellipseGeometry);
             zIndexController.Stack(toSlide, fromSlide);
 
-            var zeroKeyTime = KeyTime.FromTimeSpan(TimeSpan.Zero);
-            var midKeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(200));
-            var endKeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(400));
-
-            var opacityAnimation = new DoubleAnimationUsingKeyFrames();
-            opacityAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(1, zeroKeyTime));
-            opacityAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(1, midKeyTime));
-            opacityAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(0, endKeyTime));
-            opacityAnimation.Completed += (sender, args) =>
-            {
-                fromSlide.BeginAnimation(UIElement.OpacityProperty, null);
-                fromSlide.Opacity = 0;
-            };
-
+            var opacityAnimation = SetupOpacityAnimation(fromSlide);
             fromSlide.BeginAnimation(UIElement.OpacityProperty, opacityAnimation);
 
+            var scaleAnimation = SetupScaleAnimation(fromSlide, toSlide);
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
+        }
 
+        private DoubleAnimationUsingKeyFrames SetupScaleAnimation(TransitionerSlide fromSlide, TransitionerSlide toSlide)
+        {
             var scaleAnimation = new DoubleAnimationUsingKeyFrames();
+            scaleAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(0, startKeyTime));
+            scaleAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(1, endKeyTime));
             scaleAnimation.Completed += (sender, args) =>
             {
                 toSlide.SetCurrentValue(UIElement.ClipProperty, null);
                 fromSlide.BeginAnimation(UIElement.OpacityProperty, null);
-                fromSlide.Opacity = 0;
-
             };
-            scaleAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(0, zeroKeyTime));
-            scaleAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(1, endKeyTime));
-            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
-            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
+            return scaleAnimation;
+        }
+        private DoubleAnimationUsingKeyFrames SetupOpacityAnimation(TransitionerSlide fromSlide)
+        {
+            var opacityAnimation = GenerateThreeStepAnimation();
+            opacityAnimation.Completed += (sender, args) =>
+            {
+                fromSlide.BeginAnimation(UIElement.OpacityProperty, null);
+            };
+            return opacityAnimation;
+        }
+        private DoubleAnimationUsingKeyFrames GenerateThreeStepAnimation()
+        {
+            var animation = new DoubleAnimationUsingKeyFrames();
+            animation.KeyFrames.Add(new EasingDoubleKeyFrame(1, startKeyTime));
+            animation.KeyFrames.Add(new EasingDoubleKeyFrame(1, midKeyTime));
+            animation.KeyFrames.Add(new EasingDoubleKeyFrame(0, endKeyTime));
+            return animation;
         }
     }
 }
--- MaterialDesignThemes.Wpf/Transitions/SlideOutWipe.cs
+++ MaterialDesignThemes.Wpf/Transitions/SlideOutWipe.cs
@@ -6,45 +6,63 @@ namespace MaterialDesignThemes.Wpf.Transitions
     public class SlideOutWipe : ITransitionWipe
     {
         private readonly SineEase _sineEase = new SineEase();
+        private KeyTime startKeyTime, midKeyTime, endKeyTime;
+
+        public SlideOutWipe()
+        {
+            startKeyTime = KeyTime.FromTimeSpan(TimeSpan.Zero);
+            midKeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(200));
+            endKeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(400));
+        }
 
         public void Wipe(TransitionerSlide fromSlide, TransitionerSlide toSlide, Point origin, IZIndexController zIndexController)
         {
-            if (fromSlide == null) throw new ArgumentNullException(nameof(fromSlide));
-            if (toSlide == null) throw new ArgumentNullException(nameof(toSlide));
-            if (zIndexController == null) throw new ArgumentNullException(nameof(zIndexController));
+            if (fromSlide == null)
+            {
+                throw new ArgumentNullException(nameof(fromSlide));
+            }
+            if (toSlide == null)
+            {
+                throw new ArgumentNullException(nameof(toSlide));
+            }
+            if (zIndexController == null)
+            {
+                throw new ArgumentNullException(nameof(zIndexController));
+            }
 
-            var zeroKeyTime = KeyTime.FromTimeSpan(TimeSpan.Zero);
-            var midishKeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(200));
-            var endKeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(400));
-
-            //back out old slide setup
+            // Back out old slide setup
             var scaleTransform = new ScaleTransform(1, 1);
             fromSlide.RenderTransform = scaleTransform;
+
+            // Setup scale animation
             var scaleAnimation = new DoubleAnimationUsingKeyFrames();
-            scaleAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(1, zeroKeyTime));
+            scaleAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(1, startKeyTime));
             scaleAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(.8, endKeyTime));
             scaleAnimation.Completed += (sender, args) =>
             {
                 fromSlide.RenderTransform = null;
             };
+
+            // Setup opacity animation
             var opacityAnimation = new DoubleAnimationUsingKeyFrames();
-            opacityAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(1, zeroKeyTime));
+            opacityAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(1, startKeyTime));
             opacityAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(0, endKeyTime));
             opacityAnimation.Completed += (sender, args) =>
             {
                 fromSlide.BeginAnimation(UIElement.OpacityProperty, null);
-                fromSlide.Opacity = 0;
             };
 
-            //slide in new slide setup
+            // Slide in new slide setup
             var translateTransform = new TranslateTransform(0, toSlide.ActualHeight);
             toSlide.RenderTransform = translateTransform;
+
+            // Setup slide animation
             var slideAnimation = new DoubleAnimationUsingKeyFrames();
-            slideAnimation.KeyFrames.Add(new LinearDoubleKeyFrame(toSlide.ActualHeight, zeroKeyTime));
-            slideAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(toSlide.ActualHeight, midishKeyTime) { EasingFunction = _sineEase });
+            slideAnimation.KeyFrames.Add(new LinearDoubleKeyFrame(toSlide.ActualHeight, startKeyTime));
+            slideAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(toSlide.ActualHeight, midKeyTime) { EasingFunction = _sineEase });
             slideAnimation.KeyFrames.Add(new EasingDoubleKeyFrame(0, endKeyTime) { EasingFunction = _sineEase });
 
-            //kick off!
+            // Start animations
             translateTransform.BeginAnimation(TranslateTransform.YProperty, slideAnimation);
             scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
             scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
