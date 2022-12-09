6be496b24b70c4a2b247a80d45e9f65ba6eb2be5
Nicolai Henriksen <nicolai.h80@gmail.com>
2022-12-09 23:49:43 +0100

    TabControl disconnects TabItem.DataContext for HorizontalContentAlignment="Stretch" (#2984)
    * Added UI test reproducing the issue
    
    The issue is present when HorizontalContentAlignment is either omitted, or explicitly set to (the default) "Stretch"
    
    * Fixing issue with tab control
    
    See live stream recording, but the issue appears to be multiple Panels with IsItemsHost=True
    
    * Extended UI test with HasUniformTabWidth value as test input
    
    Ensuring all the triggers are "massaged" in the test(s)
    
    Co-authored-by: Kevin Bost <kitokeboo@gmail.com>

--- MaterialDesignThemes.UITests/WPF/TabControls/TabControlTests.cs
+++ MaterialDesignThemes.UITests/WPF/TabControls/TabControlTests.cs
@@ -46,4 +46,79 @@ public class TabControlTests : TestBase
 
         recorder.Success();
     }
+
+    [Description("Issue 2983")]
+    [Theory]
+    [InlineData("Center", true)]
+    [InlineData("Center", false)]
+    [InlineData("Left", true)]
+    [InlineData("Left", false)]
+    [InlineData("Right", true)]
+    [InlineData("Right", false)]
+    [InlineData("Stretch", true)]
+    [InlineData("Stretch", false)]
+    [InlineData("", true)]
+    [InlineData("", false)]
+    public async Task TabItem_ShouldKeepDataContext_WhenContextMenuOpens(string horizontalContentAlignment, bool hasUniformTabWidth)
+    {
+        await using var recorder = new TestRecorder(App);
+
+        string alignment = string.Empty;
+        if (!string.IsNullOrEmpty(horizontalContentAlignment))
+        {
+            alignment = $"HorizontalContentAlignment=\"{horizontalContentAlignment}\"";
+        }
+
+        //Arrange
+        IVisualElement<StackPanel> stackPanel = await LoadXaml<StackPanel>(@$"
+<StackPanel Orientation=""Vertical"">
+  <TabControl
+          {alignment}
+          materialDesign:TabAssist.HasUniformTabWidth=""{hasUniformTabWidth}""
+          materialDesign:ColorZoneAssist.Mode=""PrimaryMid""
+          Style=""{{StaticResource MaterialDesignFilledTabControl}}"">
+    <system:String>aaaa</system:String>
+    <system:String>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</system:String>
+    <TabControl.ItemTemplate>
+      <DataTemplate DataType=""system:String"">
+        <TextBlock Text=""{{Binding}}"" />
+      </DataTemplate>
+    </TabControl.ItemTemplate>
+    <TabControl.ContentTemplate>
+      <DataTemplate DataType=""system:String"">
+        <TextBlock Text=""{{Binding}}"" />
+      </DataTemplate>
+    </TabControl.ContentTemplate>
+  </TabControl>
+  <Button Margin=""50"" Width=""200"" Content=""Button with context menu"">
+    <Button.ContextMenu>
+      <ContextMenu>
+        <MenuItem Header=""Menu item"" />
+      </ContextMenu>
+    </Button.ContextMenu>
+  </Button>
+</StackPanel>", ("system", typeof(string)));
+
+        IVisualElement<TabControl> tabControl = await stackPanel.GetElement<TabControl>();
+        IVisualElement<Button> button = await stackPanel.GetElement<Button>();
+
+        // Assert initial data context
+        IVisualElement<TabItem> tabItem = await tabControl.GetElement<TabItem>();
+        object? dataContext = await tabItem.GetDataContext();
+        Assert.Equal("aaaa", dataContext);
+
+        // Act
+        await button.MoveCursorTo();
+        await button.RightClick();
+        await tabControl.MoveCursorTo();
+        await tabControl.LeftClick(Position.TopLeft);
+        await Task.Delay(50); // allow a little time for the disconnect to occur
+        
+        // Assert data context still present
+        tabItem = await tabControl.GetElement<TabItem>();
+        dataContext = await tabItem.GetDataContext();
+        Assert.Equal("aaaa", dataContext);
+
+        recorder.Success();
+    }
 }
