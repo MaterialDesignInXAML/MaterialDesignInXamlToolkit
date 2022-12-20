4be7a6ee92e67e3202b44f5c27aa46641ca0eb13
Nicolai Henriksen <nicolai.h80@gmail.com>
2022-12-20 20:15:30 +0100

    Dock the overflow grid to bottom when Orientation=Vertical
    The overflow grid containing the overflow "button" was not correctly "moved" in the trigger handling the vertical scenario.

---
+++ MaterialDesignThemes.UITests/WPF/ToolBars/ToolBarTests.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace MaterialDesignThemes.UITests.WPF.ToolBars;
+
+public class ToolBarTests : TestBase
+{
+    public ToolBarTests(ITestOutputHelper output)
+        : base(output)
+    {
+    }
+
+    [Description("Issue 2991")]
+    [Theory]
+    [InlineData(Orientation.Horizontal, Dock.Right)]
+    [InlineData(Orientation.Vertical, Dock.Bottom)]
+    public async Task ToolBar_OverflowGrid_RespectsOrientation(Orientation orientation, Dock expectedOverflowGridDock)
+    {
+        await using var recorder = new TestRecorder(App);
+
+        //Arrange
+        var toolBarTray = await LoadXaml<ToolBarTray>($@"
+<ToolBarTray Orientation=""{orientation}"" DockPanel.Dock=""Top"">
+  <ToolBar Style=""{{StaticResource MaterialDesignToolBar}}"">
+    <Button Content=""{{materialDesign:PackIcon Kind=File}}""/>
+  </ToolBar>
+</ToolBarTray>");
+        var overflowGrid = await toolBarTray.GetElement<Grid>("OverflowGrid");
+
+        //Act
+        Dock dock = await overflowGrid.GetProperty<Dock>(DockPanel.DockProperty);
+
+        //Assert
+        Assert.Equal(expectedOverflowGridDock, dock);
+
+        recorder.Success();
+    }
+}
