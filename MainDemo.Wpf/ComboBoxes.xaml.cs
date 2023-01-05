11b6757ab0d8a385b150544030cccee74a56599d
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-01-05 05:55:38 +0100

    Add option for custom popup placement for ComboBox (#3005)
    * Add special ComboBox popup handling for 90 degrees rotation
    
    Popup placement was wrong in case a LayoutTransform was applied to rotate the ComboBox. The default WPF ComboBox does respect this, and thus so should MDIX.
    
    To RotateTransfrom.Angle is compared against values of 90 and -90 (10 digit precision) to determine whether this is a rotation where the special handling should be applied.
    
    * Refactor custom popup placement into publicly exposed callback
    
    Rather than handling arbitrary rotations in the library itself, we expose a callback which can then be used to rotate the popup shown by the combobox such that it matches the users needs.
    
    * Update MaterialDesignThemes.Wpf/ComboBoxPopup.cs
    
    Co-authored-by: Kevin B <Keboo@users.noreply.github.com>
    
    * Fix compile error
    
    Co-authored-by: Kevin B <Keboo@users.noreply.github.com>

--- MainDemo.Wpf/ComboBoxes.xaml.cs
+++ MainDemo.Wpf/ComboBoxes.xaml.cs
@@ -1,19 +1,60 @@
 ﻿using MaterialDesignDemo.Domain;
+using Screen = System.Windows.Forms.Screen;
+using DrawingPoint = System.Drawing.Point;
 
-namespace MaterialDesignDemo
+namespace MaterialDesignDemo;
+
+public partial class ComboBoxes
 {
-    public partial class ComboBoxes
+    private const double DropShadowHeight = 6;    // This does not account for DPI scaling!
+
+    public static CustomPopupPlacementCallback Rotate90DegreesClockWiseCallback { get; } = (popupSize, targetSize, offset) =>
     {
-        public ComboBoxes()
+        // ComboBox is rotated 90 degrees clockwise (ie. Left=Up, Right=Down)
+        var comboBox = VisualTreeUtil.GetElementUnderMouse<ComboBox>();
+        var comboBoxLocation = comboBox.PointToScreen(new Point(0, 0));
+        Screen screen = Screen.FromPoint(new DrawingPoint((int)comboBoxLocation.X, (int)comboBoxLocation.Y));
+        int comboBoxOffsetX = (int)(comboBoxLocation.X - screen.Bounds.X) % screen.Bounds.Width;
+        double y = offset.Y - DropShadowHeight;
+        double x = offset.X;
+        double rotatedComboBoxHeight = targetSize.Width;
+        if (comboBoxOffsetX + x > popupSize.Width + rotatedComboBoxHeight)
         {
-            InitializeComponent();
-            DataContext = new ComboBoxesViewModel();
+            x -= popupSize.Width + rotatedComboBoxHeight;
         }
+        return new[] { new CustomPopupPlacement(new Point(x, y), PopupPrimaryAxis.Horizontal) };
+    };
 
-        private void ClearFilledComboBox_Click(object sender, System.Windows.RoutedEventArgs e)
-            => FilledComboBox.SelectedItem = null;
+    public static CustomPopupPlacementCallback Rotate90DegreesCounterClockWiseCallback { get; } = (popupSize, targetSize, offset) =>
+    {
+        // ComboBox is rotated 90 degrees counter-clockwise (ie. Left=Down, Right=Up)
+        var comboBox = VisualTreeUtil.GetElementUnderMouse<ComboBox>();
+        var comboBoxLocation = comboBox.PointToScreen(new Point(0, 0));
+        Screen screen = Screen.FromPoint(new DrawingPoint((int)comboBoxLocation.X, (int)comboBoxLocation.Y));
+        int comboBoxOffsetX = (int)(comboBoxLocation.X - screen.Bounds.X) % screen.Bounds.Width;
+        double y = offset.Y - popupSize.Height + DropShadowHeight;
+        double x = offset.X;
+        double rotatedComboBoxHeight = targetSize.Width;
+        if (comboBoxOffsetX + x + rotatedComboBoxHeight + popupSize.Width > screen.Bounds.Width)
+        {
+            x -= popupSize.Width;
+        }
+        else
+        {
+            x += rotatedComboBoxHeight;
+        }
+        return new[] { new CustomPopupPlacement(new Point(x, y), PopupPrimaryAxis.Horizontal) };
+    };
 
-        private void ClearOutlinedComboBox_Click(object sender, System.Windows.RoutedEventArgs e)
-            => OutlinedComboBox.SelectedItem = null;
+    public ComboBoxes()
+    {
+        InitializeComponent();
+        DataContext = new ComboBoxesViewModel();
     }
+
+    private void ClearFilledComboBox_Click(object sender, System.Windows.RoutedEventArgs e)
+        => FilledComboBox.SelectedItem = null;
+
+    private void ClearOutlinedComboBox_Click(object sender, System.Windows.RoutedEventArgs e)
+        => OutlinedComboBox.SelectedItem = null;
 }
