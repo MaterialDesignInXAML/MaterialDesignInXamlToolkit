0af3ebb2d4fa27b9b61530eb156c064d217004bb
Kevin Bost <kitokeboo@gmail.com>
2023-01-18 21:15:37 -0800

    File scope namespace for MaterialDesignColors.Wpf

--- MaterialDesignColors.Wpf/ColorManipulation/Xyz.cs
+++ MaterialDesignColors.Wpf/ColorManipulation/Xyz.cs
@@ -1,16 +1,15 @@
-﻿namespace MaterialDesignColors.ColorManipulation
-{
-    internal struct Xyz
-    {
-        public double X { get; }
-        public double Y { get; }
-        public double Z { get; }
+﻿namespace MaterialDesignColors.ColorManipulation;
 
-        public Xyz(double x, double y, double z)
-        {
-            X = x;
-            Y = y;
-            Z = z;
-        }
+internal struct Xyz
+{
+    public double X { get; }
+    public double Y { get; }
+    public double Z { get; }
+
+    public Xyz(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
     }
 }
