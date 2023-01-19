1d9e624d312303bc68e8266f072e5d275d92a1af
Kevin Bost <kitokeboo@gmail.com>
2023-01-18 22:08:35 -0800

    Removed ShadowDepth

--- MaterialDesignThemes.Wpf/Converters/ShadowConverter.cs
+++ MaterialDesignThemes.Wpf/Converters/ShadowConverter.cs
@@ -2,39 +2,37 @@
 using System.Windows.Data;
 using System.Windows.Media.Effects;
 
-namespace MaterialDesignThemes.Wpf.Converters
+namespace MaterialDesignThemes.Wpf.Converters;
+
+public class ShadowConverter : IValueConverter
 {
-    public class ShadowConverter : IValueConverter
-    {
-        public static readonly ShadowConverter Instance = new();
+    public static readonly ShadowConverter Instance = new();
 
-        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => value switch
-            {
-                Elevation elevation => Clone(Convert(elevation)),
-#pragma warning disable CS0618
-                ShadowDepth depth => Clone(Convert(ShadowAssist.GetElevation(depth))),
-#pragma warning restore CS0618
-                _ => null
-            };
-
-        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => throw new NotImplementedException();
-
-        public static DropShadowEffect? Convert(Elevation elevation) => ElevationAssist.GetDropShadow(elevation);
-
-        private static DropShadowEffect? Clone(DropShadowEffect? dropShadowEffect)
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value switch
         {
-            if (dropShadowEffect is null) return null;
-            return new DropShadowEffect
-            {
-                BlurRadius = dropShadowEffect.BlurRadius,
-                Color = dropShadowEffect.Color,
-                Direction = dropShadowEffect.Direction,
-                Opacity = dropShadowEffect.Opacity,
-                RenderingBias = dropShadowEffect.RenderingBias,
-                ShadowDepth = dropShadowEffect.ShadowDepth
-            };
-        }
+            Elevation elevation => Clone(Convert(elevation)),
+            _ => null
+        };
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+
+    public static DropShadowEffect? Convert(Elevation elevation) => ElevationAssist.GetDropShadow(elevation);
+
+    private static DropShadowEffect? Clone(DropShadowEffect? dropShadowEffect)
+    {
+        if (dropShadowEffect is null) return null;
+        DropShadowEffect rv = new()
+        {
+            BlurRadius = dropShadowEffect.BlurRadius,
+            Color = dropShadowEffect.Color,
+            Direction = dropShadowEffect.Direction,
+            Opacity = dropShadowEffect.Opacity,
+            RenderingBias = dropShadowEffect.RenderingBias,
+            ShadowDepth = dropShadowEffect.ShadowDepth
+        };
+        rv.Freeze();
+        return rv;
     }
 }
--- MaterialDesignThemes.Wpf/Converters/ShadowEdgeConverter.cs
+++ MaterialDesignThemes.Wpf/Converters/ShadowEdgeConverter.cs
@@ -3,83 +3,81 @@ using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Effects;
 
-namespace MaterialDesignThemes.Wpf.Converters
+namespace MaterialDesignThemes.Wpf.Converters;
+
+public class ShadowEdgeConverter : IMultiValueConverter
 {
-    public class ShadowEdgeConverter : IMultiValueConverter
+    public object? Convert(object[]? values, Type targetType, object? parameter, CultureInfo culture)
     {
-        public object? Convert(object[]? values, Type targetType, object? parameter, CultureInfo culture)
+        static double? GetValidSize(object? value)
+            => value is double d && !double.IsNaN(d) && !double.IsInfinity(d) ? d : null;
+
+        static DropShadowEffect? GetDropShadow(object? value) => value switch
         {
-            static double? GetValidSize(object? value)
-                => value is double d && !double.IsNaN(d) && !double.IsInfinity(d) ? d : null;
+            Elevation elevation => ElevationAssist.GetDropShadow(elevation),
+            _ => null
+        };
 
-            static DropShadowEffect? GetDropShadow(object? value) => value switch
-            {
-                Elevation elevation => ElevationAssist.GetDropShadow(elevation),
-#pragma warning disable CS0618
-                ShadowDepth depth => ElevationAssist.GetDropShadow(ShadowAssist.GetElevation(depth)),
-#pragma warning restore CS0618
-                _ => null
-            };
-
-            if (values is null
-                || values.Length < 3
-                || GetValidSize(values[0]) is not { } width
-                || GetValidSize(values[1]) is not { } height
-                || GetDropShadow(values[2]) is not { } dropShadow)
-            {
-                return null;
-            }
-
-            double blurRadius = dropShadow.BlurRadius;
-
-            Rect rect;
-
-            if (values.Length > 3 && values[3] is ShadowEdges edges and not ShadowEdges.All)
-            {
-                rect = new Rect(0, 0, width, height);
-
-                if (edges.HasFlag(ShadowEdges.Left))
-                {
-                    rect = rect with { X = -blurRadius, Width = width + blurRadius };
-                }
-
-                if (edges.HasFlag(ShadowEdges.Top))
-                {
-                    rect = rect with { Y = -blurRadius, Height = height + blurRadius };
-                }
-
-                if (edges.HasFlag(ShadowEdges.Right))
-                {
-                    rect = rect with { Width = rect.Width + blurRadius };
-                }
-
-                if (edges.HasFlag(ShadowEdges.Bottom))
-                {
-                    rect = rect with { Height = rect.Height + blurRadius };
-                }
-            }
-            else
-            {
-                rect = new Rect(
-                    -blurRadius,
-                    -blurRadius,
-                    width + blurRadius + blurRadius,
-                    height + blurRadius + blurRadius);
-            }
-
-            var drawing = new GeometryDrawing(Brushes.White, null, new RectangleGeometry(rect));
-            return new DrawingBrush(drawing)
-            {
-                Stretch = Stretch.None,
-                TileMode = TileMode.None,
-                Viewport = rect,
-                ViewportUnits = BrushMappingMode.Absolute,
-                Viewbox = rect,
-                ViewboxUnits = BrushMappingMode.Absolute
-            };
+        if (values is null
+            || values.Length < 3
+            || GetValidSize(values[0]) is not { } width
+            || GetValidSize(values[1]) is not { } height
+            || GetDropShadow(values[2]) is not { } dropShadow)
+        {
+            return null;
         }
 
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-            => throw new NotImplementedException();
+        double blurRadius = dropShadow.BlurRadius;
+
+        Rect rect;
+
+        if (values.Length > 3 && values[3] is ShadowEdges edges and not ShadowEdges.All)
+        {
+            rect = new Rect(0, 0, width, height);
+
+            if (edges.HasFlag(ShadowEdges.Left))
+            {
+                rect = rect with { X = -blurRadius, Width = width + blurRadius };
+            }
+
+            if (edges.HasFlag(ShadowEdges.Top))
+            {
+                rect = rect with { Y = -blurRadius, Height = height + blurRadius };
+            }
+
+            if (edges.HasFlag(ShadowEdges.Right))
+            {
+                rect = rect with { Width = rect.Width + blurRadius };
+            }
+
+            if (edges.HasFlag(ShadowEdges.Bottom))
+            {
+                rect = rect with { Height = rect.Height + blurRadius };
+            }
+        }
+        else
+        {
+            rect = new Rect(
+                -blurRadius,
+                -blurRadius,
+                width + blurRadius + blurRadius,
+                height + blurRadius + blurRadius);
+        }
+
+        var drawing = new GeometryDrawing(Brushes.White, null, new RectangleGeometry(rect));
+        DrawingBrush rv = new(drawing)
+        {
+            Stretch = Stretch.None,
+            TileMode = TileMode.None,
+            Viewport = rect,
+            ViewportUnits = BrushMappingMode.Absolute,
+            Viewbox = rect,
+            ViewboxUnits = BrushMappingMode.Absolute
+        };
+        rv.Freeze();
+        return rv;
     }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
 }
