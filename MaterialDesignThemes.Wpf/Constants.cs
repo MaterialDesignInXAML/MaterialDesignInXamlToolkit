db631e597a5ea10efa5872c9df250d74d53df7da
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-02-06 15:22:32 +0100

    Extract TextBox.Padding setters into style overrides instead of triggers

--- MaterialDesignThemes.Wpf/Constants.cs
+++ MaterialDesignThemes.Wpf/Constants.cs
@@ -3,6 +3,8 @@ namespace MaterialDesignThemes.Wpf;
 public static class Constants
 {
     public static readonly Thickness TextBoxDefaultPadding = new Thickness(0, 4, 0, 4);
+    public static readonly Thickness FilledTextBoxDefaultPadding = new Thickness(16, 8, 12, 8);
+    public static readonly Thickness OutlinedTextBoxDefaultPadding = new Thickness(16, 16, 12, 16);
     public static readonly Thickness DefaultTextBoxViewMargin = new Thickness(1, 0, 1, 0);
     public static readonly Thickness DefaultTextBoxViewMarginEmbedded = new Thickness(0);
     public const double TextBoxNotEnabledOpacity = 0.56;
