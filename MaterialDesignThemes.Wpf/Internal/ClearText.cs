50a7885ecd3d86b9f64196949e3a8235e126513e
Kevin B <Keboo@users.noreply.github.com>
2023-02-08 21:34:15 -0800

    Adding UI Test for the condition (#3079)

--- MaterialDesignThemes.Wpf/Internal/ClearText.cs
+++ MaterialDesignThemes.Wpf/Internal/ClearText.cs
@@ -1,59 +1,58 @@
-﻿namespace MaterialDesignThemes.Wpf.Internal
+﻿namespace MaterialDesignThemes.Wpf.Internal;
+
+public static class ClearText
 {
-    public static class ClearText
+    public static readonly RoutedCommand ClearCommand = new();
+
+    public static bool GetHandlesClearCommand(DependencyObject obj)
+        => (bool)obj.GetValue(HandlesClearCommandProperty);
+
+    public static void SetHandlesClearCommand(DependencyObject obj, bool value)
+        => obj.SetValue(HandlesClearCommandProperty, value);
+
+    public static readonly DependencyProperty HandlesClearCommandProperty =
+        DependencyProperty.RegisterAttached("HandlesClearCommand", typeof(bool), typeof(ClearText), new PropertyMetadata(false, OnHandlesClearCommandChanged));
+
+    private static void OnHandlesClearCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        public static readonly RoutedCommand ClearCommand = new();
-
-        public static bool GetHandlesClearCommand(DependencyObject obj)
-            => (bool)obj.GetValue(HandlesClearCommandProperty);
-
-        public static void SetHandlesClearCommand(DependencyObject obj, bool value)
-            => obj.SetValue(HandlesClearCommandProperty, value);
-
-        public static readonly DependencyProperty HandlesClearCommandProperty =
-            DependencyProperty.RegisterAttached("HandlesClearCommand", typeof(bool), typeof(ClearText), new PropertyMetadata(false, OnHandlesClearCommandChanged));
-
-        private static void OnHandlesClearCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        if (d is UIElement element)
         {
-            if (d is UIElement element)
+            if ((bool)e.NewValue)
             {
-                if ((bool)e.NewValue)
+                element.CommandBindings.Add(new CommandBinding(ClearCommand, OnClearCommand));
+            }
+            else
+            {
+                for (int i = element.CommandBindings.Count - 1; i >= 0; i--)
                 {
-                    element.CommandBindings.Add(new CommandBinding(ClearCommand, OnClearCommand));
-                }
-                else
-                {
-                    for (int i = element.CommandBindings.Count - 1; i >= 0; i--)
+                    if (element.CommandBindings[i].Command == ClearCommand)
                     {
-                        if (element.CommandBindings[i].Command == ClearCommand)
-                        {
-                            element.CommandBindings.RemoveAt(i);
-                        }
+                        element.CommandBindings.RemoveAt(i);
                     }
                 }
             }
-
-            static void OnClearCommand(object sender, ExecutedRoutedEventArgs e)
-            {
-                switch (sender)
-                {
-                    case DatePicker datePicker:
-                        datePicker.SetCurrentValue(DatePicker.SelectedDateProperty, null);
-                        break;
-                    case TextBox textBox:
-                        textBox.SetCurrentValue(TextBox.TextProperty, null);
-                        break;
-                    case ComboBox comboBox:
-                        comboBox.SetCurrentValue(ComboBox.TextProperty, null);
-                        comboBox.SetCurrentValue(Selector.SelectedItemProperty, null);
-                        break;
-                    case PasswordBox passwordBox:
-                        passwordBox.Password = null;
-                        break;
-                }
-                e.Handled = true;
-            }
         }
 
+        static void OnClearCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            switch (sender)
+            {
+                case DatePicker datePicker:
+                    datePicker.SetCurrentValue(DatePicker.SelectedDateProperty, null);
+                    break;
+                case TextBox textBox:
+                    textBox.SetCurrentValue(TextBox.TextProperty, null);
+                    break;
+                case ComboBox comboBox:
+                    comboBox.SetCurrentValue(ComboBox.TextProperty, null);
+                    comboBox.SetCurrentValue(Selector.SelectedItemProperty, null);
+                    break;
+                case PasswordBox passwordBox:
+                    passwordBox.Password = null;
+                    break;
+            }
+            e.Handled = true;
+        }
     }
+
 }
