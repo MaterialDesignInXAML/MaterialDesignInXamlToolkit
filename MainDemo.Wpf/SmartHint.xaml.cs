7cc2b97d8281d58e3f294660dcc89708bbb2c649
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-02-13 14:12:05 +0100

    Extend SmartHint demo page with custom padding and helper text options

--- MainDemo.Wpf/SmartHint.xaml.cs
+++ MainDemo.Wpf/SmartHint.xaml.cs
@@ -1,4 +1,6 @@
-﻿using MaterialDesignDemo.Domain;
+﻿using System.Globalization;
+using System.Windows.Data;
+using MaterialDesignDemo.Domain;
 
 namespace MaterialDesignDemo;
 
@@ -12,4 +14,18 @@ public partial class SmartHint : UserControl
         DataContext = new SmartHintViewModel();
         InitializeComponent();
     }
+}
+
+internal class CustomPaddingConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        Thickness? defaultPadding = values[0] as Thickness?;
+        bool applyCustomPadding = (bool) values[1];
+        Thickness customPadding = (Thickness) values[2];
+        return applyCustomPadding ? customPadding : defaultPadding ?? DependencyProperty.UnsetValue;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
 }
