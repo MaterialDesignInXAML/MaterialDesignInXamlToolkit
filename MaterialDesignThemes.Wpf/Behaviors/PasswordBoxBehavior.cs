66b5229d1671cb031bd0d390b350252ce2e5a492
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-02-21 14:56:10 +0100

    Handle forward/backward keyboard focus on reveal style PasswordBox

--- MaterialDesignThemes.Wpf/Behaviors/PasswordBoxBehavior.cs
+++ MaterialDesignThemes.Wpf/Behaviors/PasswordBoxBehavior.cs
@@ -6,10 +6,33 @@ internal class PasswordBoxBehavior : Behavior<PasswordBox>
 {
     private void PasswordBoxLoaded(object sender, RoutedEventArgs e) => PasswordBoxAssist.SetPassword(AssociatedObject, AssociatedObject.Password);
 
+    private void PasswordBoxPreviewGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+    {
+        if (PasswordBoxAssist.GetIsPasswordRevealed(AssociatedObject) &&
+            AssociatedObject.FindChild<TextBox>("RevealPasswordTextBox") is { } revealPasswordTextBox)
+        {
+            if (ReferenceEquals(e.OldFocus, revealPasswordTextBox))
+            {
+                // When password box receives keyboard focus, but it came from the nested reveal TextBox. We request focus transfer to the previous element from the password box's POV
+                TraversalRequest request = new TraversalRequest(FocusNavigationDirection.Previous);
+                AssociatedObject.MoveFocus(request);
+                e.Handled = true;
+            }
+            else if (!ReferenceEquals(e.OriginalSource, revealPasswordTextBox))
+            {
+                // When password box receives keyboard focus while the password is revealed, we transfer the focus to the nested reveal TextBox.
+                revealPasswordTextBox.Focus();
+                e.Handled = true;
+            }
+        }
+        
+    }
+
     protected override void OnAttached()
     {
         base.OnAttached();
         AssociatedObject.Loaded += PasswordBoxLoaded;
+        AssociatedObject.PreviewGotKeyboardFocus += PasswordBoxPreviewGotKeyboardFocus;
     }
 
     protected override void OnDetaching()
@@ -17,6 +40,7 @@ internal class PasswordBoxBehavior : Behavior<PasswordBox>
         if (AssociatedObject != null)
         {
             AssociatedObject.Loaded -= PasswordBoxLoaded;
+            AssociatedObject.PreviewGotKeyboardFocus -= PasswordBoxPreviewGotKeyboardFocus;
         }
         base.OnDetaching();
     }
