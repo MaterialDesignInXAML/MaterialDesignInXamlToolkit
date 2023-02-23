8c705167db77b7833fa19acb42dc3961850cb82b
Kevin B <Keboo@users.noreply.github.com>
2023-02-23 00:33:28 -0800

    More fixing of flaky UI tests (#3100)

--- MaterialDesignThemes.UITests/WPF/PasswordBoxes/PasswordBoxTests.cs
+++ MaterialDesignThemes.UITests/WPF/PasswordBoxes/PasswordBoxTests.cs
@@ -105,6 +105,7 @@ public class PasswordBoxTests : TestBase
         string? clearTextPassword1 = await clearTextPasswordTextBox.GetProperty<string>(TextBox.TextProperty);
 
         // Act 2 (Update in RevealPasswordTextBox updates PasswordBox and VM)
+        await Task.Delay(50);
         await revealPasswordButton.LeftClick();
         await Task.Delay(50);   // Wait for the "clear text TextBox" to become visible
         await clearTextPasswordTextBox.SendKeyboardInput($"2");
