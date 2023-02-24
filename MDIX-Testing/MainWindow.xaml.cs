fc4a5b68602156dab3616bb3822d633ad1b20787
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-02-24 21:09:32 +0100

    Add demo project that reproduces the issue to MDIX solution

---
+++ MDIX-Testing/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace MDIX_Testing;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void NavigationTabControl_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Debug.WriteLine($">>>>> SelectionChanged :: SelectedIndex = {NavigationTabControl.SelectedIndex}");
+    }
+
+    private void NavigationTabControl_OnGotFocus(object sender, RoutedEventArgs e)
+    {
+        //Debug.WriteLine($">>>>> GotFocus :: SelectedIndex = {NavigationTabControl.SelectedIndex}");
+    }
+
+    private void NavigationTabControl_OnPreviewGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+    {
+        //Debug.WriteLine($">>>>> PreviewGotKeyboardFocus :: SelectedIndex = {NavigationTabControl.SelectedIndex}");
+    }
+}
---
+++ MDIX-Testing/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MaterialDesignThemes.Wpf;
+using System.Threading.Tasks;
+
+namespace MDIX_Testing;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private int _selectedIndex;
+
+    [RelayCommand]
+    private async Task OpenDialogAsync()
+    {
+        var view = new Dialog()
+        {
+            DataContext = this
+        };
+
+        await DialogHost.Show(view);
+    }
+
+    [RelayCommand]
+    private async Task NavigateHomeAsync()
+    {
+        DialogHost.CloseDialogCommand.Execute(null, null);
+
+        /// If the dialog has been opened through the menu (System.Windows.Controls),
+        /// adding a delay  of at least 200-300 makes the tabitem switch via code possible
+        //await Task.Delay(500);
+
+        Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+        {
+            SelectedIndex = 0;
+        }));
+
+    }
+}
