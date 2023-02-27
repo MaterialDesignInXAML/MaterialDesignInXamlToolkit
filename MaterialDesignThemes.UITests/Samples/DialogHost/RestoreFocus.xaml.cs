6cb4c8b964b761ce1cad7b5cd44bd09cef02c19b
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-02-27 20:48:20 +0100

    Add failing UI test

---
+++ MaterialDesignThemes.UITests/Samples/DialogHost/RestoreFocus.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MaterialDesignThemes.UITests.Samples.DialogHost;
+
+public partial class RestoreFocus : UserControl
+{
+    public RestoreFocus()
+    {
+        InitializeComponent();
+    }
+
+    private void NavigateHomeButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        Wpf.DialogHost.CloseDialogCommand.Execute(null, null);
+        TabControl.SelectedItem = TabItem1;
+    }
+}
