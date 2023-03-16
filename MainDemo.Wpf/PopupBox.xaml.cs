cc4bf657ed17d785497424aa3aeb68408b813b39
Kevin Bost <kitokeboo@gmail.com>
2023-03-15 22:31:58 -0700

    Small cleanup done during review on stream

--- MainDemo.Wpf/PopupBox.xaml.cs
+++ MainDemo.Wpf/PopupBox.xaml.cs
@@ -17,27 +17,27 @@ public partial class PopupBox : UserControl
 
 internal class ComboBoxItemToDataTemplateConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        => value is ComboBoxItem {Tag: DataTemplate template} ? template : null!;
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is ComboBoxItem { Tag: DataTemplate template} ? template : null;
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         => throw new NotImplementedException();
 }
 
 internal class ComboBoxItemToStyleConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        => value is ComboBoxItem { Tag: Style style } ? style : null!;
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is ComboBoxItem { Tag: Style style } ? style : null;
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         => throw new NotImplementedException();
 }
 
 internal class ComboBoxItemToHelperTextConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         => value is ComboBoxItem item  ? HintAssist.GetHelperText(item) : null!;
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         => throw new NotImplementedException();
 }
