deebc4d0f2d2769aa8f87f77c10ac96e76295a8d
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-03-17 20:27:47 +0100

    Add demo page showcasing the issues

---
+++ MainDemo.Wpf/HintIssues.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MaterialDesignDemo;
+
+/// <summary>
+/// Interaction logic for HintIssues.xaml
+/// </summary>
+public partial class HintIssues : UserControl
+{
+    public HintIssues()
+    {
+        InitializeComponent();
+    }
+}
+
+internal class FixedWidthConverter : IValueConverter
+{
+    public double FixedWidth { get; set; }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is true ? FixedWidth : double.NaN;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
+
+internal class BoolToParameterConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is true ? parameter : null;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
