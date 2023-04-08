ceb3ebdbb51cb3451119b8e05c6441b13fd457f0
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-04-08 08:24:33 +0200

    Refactor PopupBox placement to respect configurable elevation (#3136)
    * Add failing UI test
    
    * Fix Card clipping issue
    
    Fixes issue where Card.ContentClip was not correctly updated based on changes to the UniformCornerRadius.
    
    * Refactor PopupBox custom popup placement
    
    Custom placement (and Popup offsets) now adjusts according to the required space for the drop shadow
    
    * Add dedicated demo app page for PopupBox
    
    Useful for testing various scenarios/alignments and ensuring things work as expected.
    
    * Small cleanup done during review on stream
    
    * Change implementation to dedicated DP
    
    Also makes the demo app page a little more intuitive regarding content of the popup.
    
    * Fix PopupTest to use new dedicated DP
    
    * Moving converters inside of the tempalate
    
    ---------
    
    Co-authored-by: Kevin Bost <kitokeboo@gmail.com>

---
+++ MainDemo.Wpf/PopupBox.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Windows.Data;
+using MaterialDesignThemes.Wpf;
+
+namespace MaterialDesignDemo;
+
+/// <summary>
+/// Interaction logic for PopupBox.xaml
+/// </summary>
+public partial class PopupBox : UserControl
+{
+    public const string DefaultStyleContentKey = nameof(DefaultStyleContentKey);
+    public const string MultiFloatingActionStyleContentKey = nameof(MultiFloatingActionStyleContentKey);
+
+    private readonly IEnumerable _defaultStyleContent;
+    private readonly IEnumerable _multiFloatingActionStyleContentKey;
+
+    public PopupBox()
+    {
+        InitializeComponent();
+
+        _defaultStyleContent = (IEnumerable)FindResource(DefaultStyleContentKey);
+        _multiFloatingActionStyleContentKey = (IEnumerable)FindResource(MultiFloatingActionStyleContentKey);
+
+        Loaded += (sender, args) => StyleComboBox.SelectedIndex = 0;
+    }
+
+    private void StyleComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        ComboBoxItem selectedItem = (ComboBoxItem) StyleComboBox.SelectedItem;
+        if (Equals(selectedItem.Content, "MaterialDesignPopupBox"))
+        {
+            ContentComboBox.ItemsSource = _defaultStyleContent;
+        }
+        else
+        {
+            ContentComboBox.ItemsSource = _multiFloatingActionStyleContentKey;
+        }
+        ContentComboBox.SelectedIndex = 0;
+    }
+}
+
+internal class ComboBoxItemToDataTemplateConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is ComboBoxItem { Tag: DataTemplate template} ? template : null;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
+
+internal class ComboBoxItemToStyleConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is ComboBoxItem { Tag: Style style } ? style : null;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
+
+internal class ComboBoxItemToHelperTextConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is ComboBoxItem item  ? HintAssist.GetHelperText(item) : null!;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
