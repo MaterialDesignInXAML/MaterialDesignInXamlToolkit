a92c186e992e4c026a6cd95cc625f02f219344ff
Kevin Bost <kitokeboo@gmail.com>
2023-04-18 22:41:07 -0700

    Adding UI test for TextBox is the Expander's header

---
+++ MaterialDesignThemes.UITests/WPF/Expanders/ExpanderTests.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace MaterialDesignThemes.UITests.WPF.Expanders;
+
+public class ExpanderTests : TestBase
+{
+    public ExpanderTests(ITestOutputHelper output)
+        : base(output)
+    { }
+
+    [Fact]
+    [Description("Issue 2791")]
+    public async Task TextBoxInHeader_CanBeFocused()
+    {
+        await using var recorder = new TestRecorder(App);
+
+        IVisualElement<StackPanel> stackPanel = await LoadXaml<StackPanel>(
+            """
+            <StackPanel>
+              <TextBox />
+              <Expander>
+                <Expander.Header>
+                  <TextBox />
+                </Expander.Header>
+                <TextBlock Text="Some content" />
+              </Expander>
+            </StackPanel>
+            """);
+        IVisualElement<Expander> expander = await stackPanel.GetElement<Expander>();
+        IVisualElement<TextBox> textBox = await expander.GetElement<TextBox>();
+        await textBox.MoveKeyboardFocus();
+
+        Assert.True(await textBox.GetIsKeyboardFocusWithin());
+
+        recorder.Success();
+    }
+}
