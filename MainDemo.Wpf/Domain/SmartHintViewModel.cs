59230512df0292343b6ee5828d11fe36541fbf94
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-04-25 21:01:55 +0200

    Initial work of respecting TextBox.Height - only done for TextBox so far

--- MainDemo.Wpf/Domain/SmartHintViewModel.cs
+++ MainDemo.Wpf/Domain/SmartHintViewModel.cs
@@ -9,17 +9,27 @@ internal class SmartHintViewModel : ViewModelBase
     private double _selectedFloatingScale = 0.75;
     private bool _showClearButton = true;
     private bool _showLeadingIcon = true;
+    private bool _showTrailingIcon = true;
     private string _hintText = "Hint text";
     private string _helperText = "Helper text";
     private Point _selectedFloatingOffset = new(0, -16);
     private bool _applyCustomPadding;
     private Thickness _selectedCustomPadding = new(5);
+    private double _selectedCustomHeight = double.NaN;
+    private VerticalAlignment _selectedVerticalAlignment = VerticalAlignment.Center;
+    private double _selectedLeadingIconSize = 20;
+    private double _selectedTrailingIconSize = 20;
+    private string? _prefixText;
+    private string? _suffixText;
 
     public IEnumerable<FloatingHintHorizontalAlignment> HorizontalAlignmentOptions { get; } = Enum.GetValues(typeof(FloatingHintHorizontalAlignment)).OfType<FloatingHintHorizontalAlignment>();
     public IEnumerable<double> FloatingScaleOptions { get; } = new[] {0.25, 0.5, 0.75, 1.0};
-    public IEnumerable<Point> FloatingOffsetOptions { get; } = new[] { new Point(0, -16), new Point(0, 16), new Point(16, 16), new Point(-16, -16) };
+    public IEnumerable<Point> FloatingOffsetOptions { get; } = new[] { new Point(0, -16), new Point(0, 16), new Point(16, 16), new Point(-16, -16), new Point(0, -40) };
     public IEnumerable<string> ComboBoxOptions { get; } = new[] {"Option 1", "Option 2", "Option 3"};
     public IEnumerable<Thickness> CustomPaddingOptions { get; } = new [] { new Thickness(0), new Thickness(5), new Thickness(10), new Thickness(15) };
+    public IEnumerable<double> CustomHeightOptions { get; } = new[] { double.NaN, 50, 75, 100 };
+    public IEnumerable<VerticalAlignment> VerticalAlignmentOptions { get; } = (VerticalAlignment[])Enum.GetValues(typeof(VerticalAlignment));
+    public IEnumerable<double> IconSizeOptions { get; } = new[] { 10.0, 15, 20, 30, 50, 75 };
 
     public bool FloatHint
     {
@@ -81,6 +91,16 @@ internal class SmartHintViewModel : ViewModelBase
         }
     }
 
+    public bool ShowTrailingIcon
+    {
+        get => _showTrailingIcon;
+        set
+        {
+            _showTrailingIcon = value;
+            OnPropertyChanged();
+        }
+    }
+
     public string HintText
     {
         get => _hintText;
@@ -117,6 +137,66 @@ internal class SmartHintViewModel : ViewModelBase
         set
         {
             _selectedCustomPadding = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public double SelectedCustomHeight
+    {
+        get => _selectedCustomHeight;
+        set
+        {
+            _selectedCustomHeight = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public VerticalAlignment SelectedVerticalAlignment
+    {
+        get => _selectedVerticalAlignment;
+        set
+        {
+            _selectedVerticalAlignment = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public double SelectedLeadingIconSize
+    {
+        get => _selectedLeadingIconSize;
+        set
+        {
+            _selectedLeadingIconSize = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public double SelectedTrailingIconSize
+    {
+        get => _selectedTrailingIconSize;
+        set
+        {
+            _selectedTrailingIconSize = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string? PrefixText
+    {
+        get => _prefixText;
+        set
+        {
+            _prefixText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string? SuffixText
+    {
+        get => _suffixText;
+        set
+        {
+            _suffixText = value;
             OnPropertyChanged();
         }
     }
--- MaterialDesignThemes.Wpf/Converters/FloatingHintTransformConverter.cs
+++ MaterialDesignThemes.Wpf/Converters/FloatingHintTransformConverter.cs
@@ -11,12 +11,13 @@ internal class FloatingHintTransformConverter : IMultiValueConverter
 
     public object? Convert(object?[]? values, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (values?.Length != 4
+        if (values?.Length != 5
             || values.Any(v => v == null)
             || !double.TryParse(values[0]!.ToString(), out double scale)
             || !double.TryParse(values[1]!.ToString(), out double lower)
             || !double.TryParse(values[2]!.ToString(), out double upper)
-            || values[3] is not Point floatingOffset)
+            || values[3] is not Point floatingOffset
+            || !double.TryParse(values[4]!.ToString(), out double initialVerticalOffset))
         {
             return Transform.Identity;
         }
@@ -34,10 +35,13 @@ internal class FloatingHintTransformConverter : IMultiValueConverter
         }
         if (ApplyTranslateTransform)
         {
+            /* As a consequence of Math.Min() which is used below to ensure the initial offset is respected (in filled style)
+               the SmartHint will not be able to "float downwards". I believe this is acceptable though.
+             */
             transformGroup.Children.Add(new TranslateTransform
             {
                 X = scale * floatingOffset.X,
-                Y = scale * floatingOffset.Y
+                Y = Math.Min(initialVerticalOffset, scale * floatingOffset.Y)
             });
         }
         return transformGroup;
