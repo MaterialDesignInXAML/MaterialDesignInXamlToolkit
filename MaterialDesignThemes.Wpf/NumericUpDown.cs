a09d92820211bac15352b9a7db4dcce09b115725
Arjun Sivaprasadam <asivaprasadam@chargerind.com>
2023-04-25 10:19:29 -0600

    Adding bare bones for the NumericUpDown control! - Auto generated control template added. - Auto generated style from Generic.xaml moved to the specific style file (MaterialDesignTheme.NumericUpDown.xaml).

---
+++ MaterialDesignThemes.Wpf/NumericUpDown.cs
@@ -0,0 +1,9 @@
+﻿namespace MaterialDesignThemes.Wpf;
+
+public class NumericUpDown : Control
+{
+    static NumericUpDown()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(NumericUpDown), new FrameworkPropertyMetadata(typeof(NumericUpDown)));
+    }
+}
