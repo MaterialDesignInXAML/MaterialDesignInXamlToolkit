c4f83cce06e7c553914ceda291dc693b804497cc
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-05-26 18:33:07 +0200

    Return true for null value in IsTransparentBrushConverter

--- MaterialDesignThemes.Wpf/Converters/IsTransparentBrushConverter.cs
+++ MaterialDesignThemes.Wpf/Converters/IsTransparentBrushConverter.cs
@@ -7,7 +7,7 @@ namespace MaterialDesignThemes.Wpf.Converters;
 public sealed class IsTransparentBrushConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        => Equals(value, Brushes.Transparent);
+        => value is null || Equals(value, Brushes.Transparent);
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         => throw new NotImplementedException();
