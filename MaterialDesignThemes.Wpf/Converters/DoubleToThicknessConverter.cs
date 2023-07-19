8373dc01352b42a4a533df537064c5a7a5577288
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-07-19 22:57:34 +0200

    Ensure clear button does not overlap editable text in ComboBox

--- MaterialDesignThemes.Wpf/Converters/DoubleToThicknessConverter.cs
+++ MaterialDesignThemes.Wpf/Converters/DoubleToThicknessConverter.cs
@@ -5,13 +5,15 @@ namespace MaterialDesignThemes.Wpf.Converters
 {
     internal class DoubleToThicknessConverter : IValueConverter
     {
+        public double InitialOffset { get; set; }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
             => (Dock)parameter switch
             {
-                Dock.Left => new Thickness((double)value, 0, 0, 0),
-                Dock.Top => new Thickness(0, (double)value, 0, 0),
-                Dock.Right => new Thickness(0, 0, (double)value, 0),
-                Dock.Bottom => new Thickness(0, 0, 0, (double)value),
+                Dock.Left => new Thickness(InitialOffset + (double)value, 0, 0, 0),
+                Dock.Top => new Thickness(0, InitialOffset + (double)value, 0, 0),
+                Dock.Right => new Thickness(0, 0, InitialOffset + (double)value, 0),
+                Dock.Bottom => new Thickness(0, 0, 0, InitialOffset + (double)value),
                 _ => Binding.DoNothing
             };
 
