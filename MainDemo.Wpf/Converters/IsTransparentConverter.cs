6378716b5f7e88d1d8a79b3bd0fc0a901cf01d71
Kevin B <Keboo@users.noreply.github.com>
2023-07-19 01:25:46 -0700

    Allow for transparent colors for generated icons (#3273)

---
+++ MainDemo.Wpf/Converters/IsTransparentConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MaterialDesignDemo.Converters;
+
+public class IsTransparentConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        Color? color = value switch
+        {
+            Color c => c,
+            SolidColorBrush brush => brush.Color,
+            _ => null
+        };
+        return color == Colors.Transparent;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue &&
+            boolValue)
+        {
+            return Colors.Transparent;
+        }
+        return Binding.DoNothing;
+    }
+}
