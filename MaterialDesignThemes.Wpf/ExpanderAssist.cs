5f9220040664053d71a00d0a14585c83d10a1ed3
Kevin Bost <kitokeboo@gmail.com>
2023-08-16 23:28:56 -0700

    Added support for button position on vertical template
    Added new default button position to support different default for vertical and horizontal.

--- MaterialDesignThemes.Wpf/ExpanderAssist.cs
+++ MaterialDesignThemes.Wpf/ExpanderAssist.cs
@@ -4,14 +4,15 @@ namespace MaterialDesignThemes.Wpf;
 
 public enum ExpanderButtonPosition
 {
+    Default,
     Start,
     End
 }
 
 public static class ExpanderAssist
 {
-    private static readonly Thickness DefaultHorizontalHeaderPadding = new Thickness(24, 12, 24, 12);
-    private static readonly Thickness DefaultVerticalHeaderPadding = new Thickness(12, 24, 12, 24);
+    private static readonly Thickness DefaultHorizontalHeaderPadding = new(24, 12, 24, 12);
+    private static readonly Thickness DefaultVerticalHeaderPadding = new(12, 24, 12, 24);
 
     #region AttachedProperty : HorizontalHeaderPaddingProperty
     public static readonly DependencyProperty HorizontalHeaderPaddingProperty
@@ -68,7 +69,7 @@ public static class ExpanderAssist
 
     #region AttachedProperty : ExpanderButtonPositionProperty
     public static readonly DependencyProperty ExpanderButtonPositionProperty
-        = DependencyProperty.RegisterAttached("ExpanderButtonPosition", typeof(ExpanderButtonPosition), typeof(ExpanderAssist), new PropertyMetadata(ExpanderButtonPosition.End));
+        = DependencyProperty.RegisterAttached("ExpanderButtonPosition", typeof(ExpanderButtonPosition), typeof(ExpanderAssist), new PropertyMetadata(ExpanderButtonPosition.Default));
 
     public static ExpanderButtonPosition GetExpanderButtonPosition(Expander element)
         => (ExpanderButtonPosition)element.GetValue(ExpanderButtonPositionProperty);
