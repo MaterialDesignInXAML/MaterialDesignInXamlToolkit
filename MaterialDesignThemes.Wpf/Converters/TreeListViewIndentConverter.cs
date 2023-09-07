c247195f29216ad54b0deeb922423ba18c05a507
Kevin Bost <kitokeboo@gmail.com>
2023-09-07 00:09:03 -0700

    Working tree view with multi-selection and indentation

---
+++ MaterialDesignThemes.Wpf/Converters/TreeListViewIndentConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters;
+
+public class TreeListViewIndentConverter : IMultiValueConverter
+{
+    public object? Convert(object?[]? values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values?.Length == 2 && values[0] is double size && values[1] is int level)
+        {
+            return new Thickness(size * level, 0, 0, 0);
+        }
+        return Binding.DoNothing;
+    }
+
+    public object?[]? ConvertBack(object? value, Type[] targetTypes, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
