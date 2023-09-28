1de4cf6e99c5a138a619d8863098a62d3d57ca9e
Kevin Bost <kitokeboo@gmail.com>
2023-09-27 22:33:40 -0700

    Adding tests for TreeListViewItemsCollection

---
+++ MaterialDesignThemes.Wpf.Tests/Internal/TreeListViewItemsCollectionTests.cs
@@ -0,0 +1,253 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Threading;
+using MaterialDesignThemes.Wpf.Internal;
+
+namespace MaterialDesignThemes.Wpf.Tests.Internal;
+
+public class TreeListViewItemsCollectionTests
+{
+    [Fact]
+    public void Constructor_AcceptsNull()
+    {
+        TreeListViewItemsCollection<string> collection = new(null);
+
+        Assert.Empty(collection);
+    }
+
+    [Fact]
+    public void Constructor_AcceptsObject()
+    {
+        TreeListViewItemsCollection<string> collection = new(new object());
+
+        Assert.Empty(collection);
+    }
+
+    [Fact]
+    public void Constructor_AcceptsIEnumerable()
+    {
+        IEnumerable<string> enumerable = new[] { "a", "b", "c" };
+        TreeListViewItemsCollection<string> collection = new(enumerable);
+
+        Assert.Equal(new[] { "a", "b", "c" }, collection);
+    }
+
+    [Fact]
+    public void WhenWrappedObjectImplementsIncc_ItHandlesAdditions()
+    {
+        //Arrange
+        ObservableCollection<string> collection = new();
+
+        TreeListViewItemsCollection<string> treeListViewItemsCollection = new(collection);
+
+        //Act
+        collection.Add("a");
+
+        //Assert
+        Assert.Equal(new[] { "a" }, treeListViewItemsCollection);
+    }
+
+    [Fact]
+    public void WhenWrappedObjectImplementsIncc_ItHandlesRemovals()
+    {
+        //Arrange
+        ObservableCollection<string> collection = new() { "a" };
+
+        TreeListViewItemsCollection<string> treeListViewItemsCollection = new(collection);
+
+        //Act
+        collection.Remove("a");
+
+        //Assert
+        Assert.Empty(treeListViewItemsCollection);
+    }
+
+    [Fact]
+    public void WhenWrappedObjectImplementsIncc_ItHandlesReplacements()
+    {
+        //Arrange
+        ObservableCollection<string> collection = new() { "a" };
+
+        TreeListViewItemsCollection<string> treeListViewItemsCollection = new(collection);
+
+        //Act
+        collection[0] = "b";
+
+        //Assert
+        Assert.Equal(new[] { "b" }, treeListViewItemsCollection);
+    }
+
+    [Fact]
+    public void WhenWrappedObjectImplementsIncc_ItHandlesReset()
+    {
+        //Arrange
+        TestableCollection<string> collection = new() { "a" };
+
+        TreeListViewItemsCollection<string> treeListViewItemsCollection = new(collection);
+
+        //Act
+        collection.ReplaceAllItems("b", "c");
+
+        //Assert
+        Assert.Equal(new[] { "b", "c" }, treeListViewItemsCollection);
+    }
+
+    [Fact]
+    public void WhenWrappedObjectImplementsIncc_ItHandlesMoves()
+    {
+        //Arrange
+        ObservableCollection<string> collection = new() { "a", "b", "c" };
+
+        TreeListViewItemsCollection<string> treeListViewItemsCollection = new(collection);
+
+        //Act
+        collection.Move(0, 2);
+
+        //Assert
+        Assert.Equal(new[] { "b", "c", "a" }, treeListViewItemsCollection);
+    }
+
+    [Theory]
+    [InlineData(0, 0)] //x is a sibling of a
+    [InlineData(1, 1)] //x nested under a
+    [InlineData(2, 1)] //x is a sibling of a_a
+    [InlineData(2, 2)] //x is a child of a_a
+    [InlineData(3, 1)] //x is a sibling of a_b
+    [InlineData(3, 2)] //x is a child of a_b
+    [InlineData(4, 1)] //x is a child of b
+    [InlineData(4, 0)] //x is a sibling of b
+    public void WhenAddingItemAtNestedLevel_ItSetsTheItemsLevel(int insertionIndex, int requestedLevel)
+    {
+        //Arrange
+        TreeListViewItemsCollection<string> treeListViewItemsCollection = new(new[] { "a", "b" });
+        treeListViewItemsCollection.Insert(1, "a_a", 1);
+        treeListViewItemsCollection.Insert(2, "a_b", 1);
+        /*
+         * 0. a
+         * 1.  a_a
+         * 2.  a_b
+         * 3. b
+         */
+
+        //Act
+        treeListViewItemsCollection.Insert(insertionIndex, "x", requestedLevel);
+
+        //Assert
+        List<string> expectedItems = new(new[] { "a", "a_a", "a_b", "b" });
+        expectedItems.Insert(insertionIndex, "x");
+        Assert.Equal(expectedItems, treeListViewItemsCollection);
+
+        List<int> expectedLevels = new(new[] { 0, 1, 1, 0 });
+        expectedLevels.Insert(insertionIndex, requestedLevel);
+        Assert.Equal(expectedLevels, treeListViewItemsCollection.GetAllLevels());
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    [InlineData(2, 4)]
+    [InlineData(2, 0)]
+    [InlineData(0, -1)]
+    [InlineData(4, 2)]
+    public void WhenAddingItemAtNestedLevel_ItItThrowsIfRequestIsOutOfRange(int insertionIndex, int requestedLevel)
+    {
+        //Arrange
+        TreeListViewItemsCollection<string> treeListViewItemsCollection = new(new[] { "a", "b" });
+        treeListViewItemsCollection.Insert(1, "a_a", 1);
+        treeListViewItemsCollection.Insert(2, "a_b", 1);
+        /*
+         * 0. a
+         * 1.  a_a
+         * 2.  a_b
+         * 3. b
+         */
+
+        //Act/Assert
+        Assert.Throws<ArgumentOutOfRangeException>(() => treeListViewItemsCollection.Insert(insertionIndex, "x", requestedLevel));
+    }
+
+    public static IEnumerable<object[]> RemoveItemTestData()
+    {
+        yield return new object[] { 0, new[] { 0, 1, 2, 3, 4 } }; 
+        yield return new object[] { 1, new[] { 1,  } }; 
+    }
+
+    [Theory]
+    [InlineData(0, 5)]
+    [InlineData(1, 3)]
+    [InlineData(2, 1)]
+    [InlineData(3, 1)]
+    [InlineData(4, 1)]
+    [InlineData(5, 1)]
+    public void WhenRemovingItem_ItRemovesItemsAndAnyChildren(int indexToRemove, int numItemsRemoved)
+    {
+        //Arrange
+        TreeListViewItemsCollection<string> treeListViewItemsCollection = new(new[] { "a", "b" });
+        treeListViewItemsCollection.Insert(1, "a_a", 1);
+        treeListViewItemsCollection.Insert(2, "a_a_a", 2);
+        treeListViewItemsCollection.Insert(3, "a_a_b", 2);
+        treeListViewItemsCollection.Insert(4, "a_b", 1);
+        /*
+         * 0. a
+         * 1.  a_a
+         * 2.   a_a_a
+         * 3.   a_a_b
+         * 4.  a_b
+         * 5. b
+         */
+
+        //Act
+        treeListViewItemsCollection.RemoveAt(indexToRemove);
+
+        //Assert
+        List<string> expectedItems = new(new[] { "a", "a_a", "a_a_a", "a_a_b", "a_b", "b" });
+        List<int> expectedLevels = new(new[] { 0, 1, 2, 2, 1, 0 });
+        for (int i = 0; i < numItemsRemoved; i++)
+        {
+            expectedItems.RemoveAt(indexToRemove);
+            expectedLevels.RemoveAt(indexToRemove);
+        }
+        Assert.Equal(expectedItems, treeListViewItemsCollection);
+        Assert.Equal(expectedLevels, treeListViewItemsCollection.GetAllLevels());
+    }
+
+
+    private class TestableCollection<T> : ObservableCollection<T>
+    {
+        private int _blockCollectionChanges;
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if (Interlocked.CompareExchange(ref _blockCollectionChanges, 0, 0) == 0)
+            {
+                base.OnCollectionChanged(e);
+            }
+        }
+
+        public void ReplaceAllItems(params T[] newItems)
+        {
+            Interlocked.Exchange(ref _blockCollectionChanges, 1);
+
+            Clear();
+            foreach (T newItem in newItems)
+            {
+                Add(newItem);
+            }
+
+            Interlocked.Exchange(ref _blockCollectionChanges, 0);
+
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
+
+public static class TreeListViewItemsCollectionExtensions
+{
+    public static IEnumerable<int> GetAllLevels<T>(this TreeListViewItemsCollection<T> collection)
+    {
+        for (int i = 0; i < collection.Count; i++)
+        {
+            yield return collection.GetLevel(i);
+        }
+    }
+}
---
+++ MaterialDesignThemes.Wpf/Internal/TreeListViewItemsCollection.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace MaterialDesignThemes.Wpf.Internal;
+
+public class TreeListViewItemsCollection<T> : ObservableCollection<T>
+{
+    private List<int> ItemLevels { get; } = new();
+
+    public TreeListViewItemsCollection(object? wrappedSource)
+    {
+        if (wrappedSource is IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                Add(item);
+            }
+        }
+        if (wrappedSource is INotifyCollectionChanged newCollectionChanged)
+        {
+            newCollectionChanged.CollectionChanged += ItemsSource_CollectionChanged;
+        }
+    }
+
+    public int GetLevel(int index)
+        => ItemLevels[index];
+
+    public void Insert(int index, T item, int level)
+    {
+        if (level < 0) throw new ArgumentOutOfRangeException(nameof(level), level, "Item level must not be negative");
+
+        //Always allowed to request previous item level + 1 as this is inserting a "child"
+        int previousItemLevel = index > 0 ? ItemLevels[index - 1] : -1;
+        if (level > previousItemLevel + 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(level), level, $"Item level must not be more than one level greater the previous item ({previousItemLevel})");
+        }
+
+        int nextItemLevel = index < Count ? ItemLevels[index] : 0;
+        if (level < nextItemLevel)
+        {
+            throw new ArgumentOutOfRangeException(nameof(level), level, $"Item level must not be less than the level item after it ({nextItemLevel})");
+        }
+
+        Insert(index, item);
+        ItemLevels[index] = level;
+    }
+
+    protected override void RemoveItem(int index)
+    {
+        int currentLevel = ItemLevels[index];
+        ItemLevels.RemoveAt(index);
+        base.RemoveItem(index);
+        while (index < Count && ItemLevels[index] > currentLevel)
+        {
+            RemoveItem(index);
+        }
+    }
+
+    protected override void InsertItem(int index, T item)
+    {
+        ItemLevels.Insert(index, 0);
+        base.InsertItem(index, item);
+    }
+
+    private void ItemsSource_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        switch (e.Action)
+        {
+            case NotifyCollectionChangedAction.Add:
+                for (int i = 0; i < e.NewItems?.Count; i++)
+                {
+                    Insert(e.NewStartingIndex + i, (T)e.NewItems[i]!);
+                }
+                break;
+            case NotifyCollectionChangedAction.Remove:
+                for (int i = 0; i < e.OldItems?.Count; i++)
+                {
+                    RemoveAt(e.OldStartingIndex + i);
+                }
+                break;
+            case NotifyCollectionChangedAction.Replace:
+                for (int i = 0; i < e.NewItems?.Count; i++)
+                {
+                    this[e.NewStartingIndex + i] = (T)e.NewItems[i]!;
+                }
+                break;
+            case NotifyCollectionChangedAction.Move:
+                for (int i = 0; i < e.NewItems?.Count; i++)
+                {
+                    Move(e.OldStartingIndex + i, e.NewStartingIndex + i);
+                }
+                break;
+            case NotifyCollectionChangedAction.Reset:
+                Clear();
+                ItemLevels.Clear();
+                foreach (var item in sender as IEnumerable<T> ?? Enumerable.Empty<T>())
+                {
+                    Add(item);
+                }
+                break;
+        }
+    }
+}
