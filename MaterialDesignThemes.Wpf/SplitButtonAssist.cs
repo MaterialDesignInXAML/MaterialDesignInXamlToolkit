61778edc96309178c1362eeff1113c684674df5e
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-11-22 11:14:49 +0100

    Initial POC of SplitButton using attached properties

---
+++ MaterialDesignThemes.Wpf/SplitButtonAssist.cs
@@ -0,0 +1,59 @@
+﻿namespace MaterialDesignThemes.Wpf
+{
+    public static class SplitButtonAssist
+    {
+        public static readonly DependencyProperty PopupElevationProperty = DependencyProperty.RegisterAttached(
+            "PopupElevation", typeof(Elevation), typeof(SplitButtonAssist), new PropertyMetadata(default(Elevation)));
+
+        public static void SetPopupElevation(DependencyObject element, Elevation value)
+            => element.SetValue(PopupElevationProperty, value);
+
+        public static Elevation GetPopupElevation(DependencyObject element)
+            => (Elevation) element.GetValue(PopupElevationProperty);
+
+        public static readonly DependencyProperty PopupUniformCornerRadiusProperty = DependencyProperty.RegisterAttached(
+            "PopupUniformCornerRadius", typeof(double), typeof(SplitButtonAssist), new PropertyMetadata(default(double)));
+
+        public static void SetPopupUniformCornerRadius(DependencyObject element, double value)
+            => element.SetValue(PopupUniformCornerRadiusProperty, value);
+
+        public static double GetPopupUniformCornerRadius(DependencyObject element)
+            => (double) element.GetValue(PopupUniformCornerRadiusProperty);
+
+        public static readonly DependencyProperty PopupContentStyleProperty = DependencyProperty.RegisterAttached(
+            "PopupContentStyle", typeof(Style), typeof(SplitButtonAssist), new PropertyMetadata(default(Style)));
+
+        public static void SetPopupContentStyle(DependencyObject element, Style value)
+            => element.SetValue(PopupContentStyleProperty, value);
+
+        public static Style GetPopupContentStyle(DependencyObject element)
+            => (Style) element.GetValue(PopupContentStyleProperty);
+
+        public static readonly DependencyProperty PopupContentProperty = DependencyProperty.RegisterAttached(
+            "PopupContent", typeof(object), typeof(SplitButtonAssist), new PropertyMetadata(default(object)));
+
+        public static void SetPopupContent(DependencyObject element, object value)
+            => element.SetValue(PopupContentProperty, value);
+
+        public static object GetPopupContent(DependencyObject element)
+            => element.GetValue(PopupContentProperty);
+
+        public static readonly DependencyProperty PopupContentTemplateProperty = DependencyProperty.RegisterAttached(
+            "PopupContentTemplate", typeof(DataTemplate), typeof(SplitButtonAssist), new PropertyMetadata(default(DataTemplate)));
+
+        public static void SetPopupContentTemplate(DependencyObject element, DataTemplate value)
+            => element.SetValue(PopupContentTemplateProperty, value);
+
+        public static DataTemplate GetPopupContentTemplate(DependencyObject element)
+            => (DataTemplate) element.GetValue(PopupContentTemplateProperty);
+
+        public static readonly DependencyProperty PopupContentTemplateSelectorProperty = DependencyProperty.RegisterAttached(
+            "PopupContentTemplateSelector", typeof(DataTemplateSelector), typeof(SplitButtonAssist), new PropertyMetadata(default(DataTemplateSelector)));
+
+        public static void SetPopupContentTemplateSelector(DependencyObject element, DataTemplateSelector value)
+            => element.SetValue(PopupContentTemplateSelectorProperty, value);
+
+        public static DataTemplateSelector GetPopupContentTemplateSelector(DependencyObject element)
+            => (DataTemplateSelector) element.GetValue(PopupContentTemplateSelectorProperty);
+    }
+}
---
+++ MaterialDesignThemes.Wpf/SplitButtonContent.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xaml.Behaviors.Core;
+
+namespace MaterialDesignThemes.Wpf;
+
+public class SplitButtonContent : ContentControl
+{
+    static SplitButtonContent() => DefaultStyleKeyProperty.OverrideMetadata(typeof(SplitButtonContent), new FrameworkPropertyMetadata(typeof(SplitButtonContent)));
+
+    public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register(
+        nameof(IsOpen), typeof(bool), typeof(SplitButtonContent), new PropertyMetadata(default(bool)));
+
+    public bool IsOpen
+    {
+        get => (bool)GetValue(IsOpenProperty);
+        set => SetValue(IsOpenProperty, value);
+    }
+
+    public static readonly DependencyProperty PopupPlacementTargetProperty = DependencyProperty.Register(
+        nameof(PopupPlacementTarget), typeof(UIElement), typeof(SplitButtonContent), new PropertyMetadata(default(UIElement)));
+
+    public UIElement PopupPlacementTarget
+    {
+        get => (UIElement) GetValue(PopupPlacementTargetProperty);
+        set => SetValue(PopupPlacementTargetProperty, value);
+    }
+
+    public static readonly DependencyProperty PopupElevationProperty = DependencyProperty.Register(
+        nameof(PopupElevation), typeof(Elevation), typeof(SplitButtonContent), new PropertyMetadata(default(Elevation)));
+
+    public Elevation PopupElevation
+    {
+        get => (Elevation)GetValue(PopupElevationProperty);
+        set => SetValue(PopupElevationProperty, value);
+    }
+
+    public static readonly DependencyProperty PopupUniformCornerRadiusProperty = DependencyProperty.Register(
+        nameof(PopupUniformCornerRadius), typeof(double), typeof(SplitButtonContent), new PropertyMetadata(default(double)));
+
+    public double PopupUniformCornerRadius
+    {
+        get => (double)GetValue(PopupUniformCornerRadiusProperty);
+        set => SetValue(PopupUniformCornerRadiusProperty, value);
+    }
+
+    public static readonly DependencyProperty PopupContentStyleProperty = DependencyProperty.Register(
+        nameof(PopupContentStyle), typeof(Style), typeof(SplitButtonContent), new PropertyMetadata(default(Style)));
+
+    public Style PopupContentStyle
+    {
+        get => (Style)GetValue(PopupContentStyleProperty);
+        set => SetValue(PopupContentStyleProperty, value);
+    }
+
+    public static readonly DependencyProperty PopupContentProperty = DependencyProperty.Register(
+        nameof(PopupContent), typeof(object), typeof(SplitButtonContent), new PropertyMetadata(default(object)));
+
+    public object PopupContent
+    {
+        get => (object)GetValue(PopupContentProperty);
+        set => SetValue(PopupContentProperty, value);
+    }
+
+    public static readonly DependencyProperty PopupContentTemplateProperty = DependencyProperty.Register(
+        nameof(PopupContentTemplate), typeof(DataTemplate), typeof(SplitButtonContent), new PropertyMetadata(default(DataTemplate)));
+
+    public DataTemplate PopupContentTemplate
+    {
+        get => (DataTemplate)GetValue(PopupContentTemplateProperty);
+        set => SetValue(PopupContentTemplateProperty, value);
+    }
+
+    public static readonly DependencyProperty PopupContentTemplateSelectorProperty = DependencyProperty.Register(
+        nameof(PopupContentTemplateSelector), typeof(DataTemplateSelector), typeof(SplitButtonContent), new PropertyMetadata(default(DataTemplateSelector)));
+
+    public DataTemplateSelector PopupContentTemplateSelector
+    {
+        get => (DataTemplateSelector)GetValue(PopupContentTemplateSelectorProperty);
+        set => SetValue(PopupContentTemplateSelectorProperty, value);
+    }
+
+    public ICommand OpenCommand { get; }
+
+    public SplitButtonContent() => OpenCommand = new ActionCommand(() => IsOpen = true);
+}
