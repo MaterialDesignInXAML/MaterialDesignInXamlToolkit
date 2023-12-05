d39f4272409fda902c5c96377b4d477490a3bff4
Kevin B <Keboo@users.noreply.github.com>
2023-12-04 21:03:26 -0800

    Fixing issue with the template selector not getting forwarded. (#3405)

---
+++ MaterialDesignThemes.UITests/Samples/PopupBox/PopupBoxWithTemplateSelector.xaml.cs
@@ -0,0 +1,35 @@
+﻿
+namespace MaterialDesignThemes.UITests.Samples.PopupBox;
+
+/// <summary>
+/// Interaction logic for PopupBoxWithTemplateSelector.xaml
+/// </summary>
+public partial class PopupBoxWithTemplateSelector : UserControl
+{
+    private int _counter;
+    public PopupBoxWithTemplateSelector()
+    {
+        InitializeComponent();
+        MyPopupBox.ToggleContent = _counter;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        MyPopupBox.ToggleContent =  ++_counter;
+    }
+}
+
+public class ColorTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate? OddTemplate { get; set; }
+    public DataTemplate? EvenTemplate { get; set; }
+
+    public override DataTemplate? SelectTemplate(object? item, DependencyObject container)
+    {
+        if (item is int intValue)
+        {
+            return intValue % 2 == 0 ? EvenTemplate : OddTemplate;
+        }
+        return null;
+    }
+}
