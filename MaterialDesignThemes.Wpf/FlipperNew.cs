b6a0dc88a3d34a011a54be7133a31303f64e9764
Kevin Bost <kitokeboo@gmail.com>
2023-12-13 22:01:54 -0800

    Starting work on new flipper design

---
+++ MaterialDesignThemes.Wpf/FlipperNew.cs
@@ -0,0 +1,183 @@
+﻿namespace MaterialDesignThemes.Wpf;
+
+[TemplatePart(Name = Plane3DPartName, Type = typeof(Plane3DNew))]
+[TemplateVisualState(GroupName = TemplateFlipGroupName, Name = TemplateFlippedStateName)]
+[TemplateVisualState(GroupName = TemplateFlipGroupName, Name = TemplateUnflippedStateName)]
+public class FlipperNew : Control
+{
+    public static readonly RoutedCommand FlipCommand = new();
+
+    public const string Plane3DPartName = "PART_Plane3D";
+    public const string TemplateFlipGroupName = "FlipStates";
+    public const string TemplateFlippedStateName = "Flipped";
+    public const string TemplateUnflippedStateName = "Unflipped";
+
+    private Plane3DNew? _plane3D;
+
+    static FlipperNew()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(FlipperNew), new FrameworkPropertyMetadata(typeof(FlipperNew)));
+    }
+
+    public FlipperNew()
+    {
+        CommandBindings.Add(new CommandBinding(FlipCommand, FlipHandler));
+    }
+
+    public static readonly DependencyProperty FrontContentProperty = DependencyProperty.Register(
+        nameof(FrontContent), typeof(object), typeof(FlipperNew), new PropertyMetadata(default(object?)));
+
+    public object? FrontContent
+    {
+        get => GetValue(FrontContentProperty);
+        set => SetValue(FrontContentProperty, value);
+    }
+
+    public static readonly DependencyProperty FrontContentTemplateProperty = DependencyProperty.Register(
+        nameof(FrontContentTemplate), typeof(DataTemplate), typeof(FlipperNew), new PropertyMetadata(default(DataTemplate?)));
+
+    public DataTemplate? FrontContentTemplate
+    {
+        get => (DataTemplate?)GetValue(FrontContentTemplateProperty);
+        set => SetValue(FrontContentTemplateProperty, value);
+    }
+
+    public static readonly DependencyProperty FrontContentTemplateSelectorProperty = DependencyProperty.Register(
+        nameof(FrontContentTemplateSelector), typeof(DataTemplateSelector), typeof(FlipperNew), new PropertyMetadata(default(DataTemplateSelector)));
+
+    public DataTemplateSelector? FrontContentTemplateSelector
+    {
+        get => (DataTemplateSelector)GetValue(FrontContentTemplateSelectorProperty);
+        set => SetValue(FrontContentTemplateSelectorProperty, value);
+    }
+
+    public static readonly DependencyProperty FrontContentStringFormatProperty = DependencyProperty.Register(
+        nameof(FrontContentStringFormat), typeof(string), typeof(FlipperNew), new PropertyMetadata(default(string?)));
+
+    public string? FrontContentStringFormat
+    {
+        get => (string?)GetValue(FrontContentStringFormatProperty);
+        set => SetValue(FrontContentStringFormatProperty, value);
+    }
+
+    public static readonly DependencyProperty BackContentProperty = DependencyProperty.Register(
+        nameof(BackContent), typeof(object), typeof(FlipperNew), new PropertyMetadata(default(object?)));
+
+    public object? BackContent
+    {
+        get => GetValue(BackContentProperty);
+        set => SetValue(BackContentProperty, value);
+    }
+
+    public static readonly DependencyProperty BackContentTemplateProperty = DependencyProperty.Register(
+        nameof(BackContentTemplate), typeof(DataTemplate), typeof(FlipperNew), new PropertyMetadata(default(DataTemplate?)));
+
+    public DataTemplate? BackContentTemplate
+    {
+        get => (DataTemplate?)GetValue(BackContentTemplateProperty);
+        set => SetValue(BackContentTemplateProperty, value);
+    }
+
+    public static readonly DependencyProperty BackContentTemplateSelectorProperty = DependencyProperty.Register(
+        nameof(BackContentTemplateSelector), typeof(DataTemplateSelector), typeof(FlipperNew), new PropertyMetadata(default(DataTemplateSelector?)));
+
+    public DataTemplateSelector? BackContentTemplateSelector
+    {
+        get => (DataTemplateSelector?)GetValue(BackContentTemplateSelectorProperty);
+        set => SetValue(BackContentTemplateSelectorProperty, value);
+    }
+
+    public static readonly DependencyProperty BackContentStringFormatProperty = DependencyProperty.Register(
+        nameof(BackContentStringFormat), typeof(string), typeof(FlipperNew), new PropertyMetadata(default(string?)));
+
+    public string? BackContentStringFormat
+    {
+        get => (string?)GetValue(BackContentStringFormatProperty);
+        set => SetValue(BackContentStringFormatProperty, value);
+    }
+
+    public static readonly DependencyProperty IsFlippedProperty = DependencyProperty.Register(
+        nameof(IsFlipped), typeof(bool), typeof(FlipperNew), new PropertyMetadata(default(bool), IsFlippedPropertyChangedCallback));
+
+    private static void IsFlippedPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+    {
+        var flipper = (FlipperNew)dependencyObject;
+        flipper.UpdateVisualStates(false);
+        flipper.RemeasureDuringFlip();
+        OnIsFlippedChanged(flipper, dependencyPropertyChangedEventArgs);
+    }
+
+    public bool IsFlipped
+    {
+        get => (bool)GetValue(IsFlippedProperty);
+        set => SetValue(IsFlippedProperty, value);
+    }
+
+    public static readonly RoutedEvent IsFlippedChangedEvent =
+        EventManager.RegisterRoutedEvent(
+            nameof(IsFlipped),
+            RoutingStrategy.Bubble,
+            typeof(RoutedPropertyChangedEventHandler<bool>),
+            typeof(FlipperNew));
+
+    public event RoutedPropertyChangedEventHandler<bool> IsFlippedChanged
+    {
+        add => AddHandler(IsFlippedChangedEvent, value);
+        remove => RemoveHandler(IsFlippedChangedEvent, value);
+    }
+
+    private static void OnIsFlippedChanged(
+        DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var instance = (FlipperNew)d;
+        var args = new RoutedPropertyChangedEventArgs<bool>(
+                (bool)e.OldValue,
+                (bool)e.NewValue)
+        {
+            RoutedEvent = IsFlippedChangedEvent
+        };
+        instance.RaiseEvent(args);
+    }
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        UpdateVisualStates(false);
+
+        _plane3D = GetTemplateChild(Plane3DPartName) as Plane3DNew;
+    }
+
+    private void RemeasureDuringFlip()
+    {
+        ////not entirely happy hardcoding this, but I have explored other options I am not happy with, and this will do for now
+        //const int storyboardMs = 400;
+        //const int granularity = 6;
+
+        //var remeasureInterval = new TimeSpan(0, 0, 0, 0, storyboardMs / granularity);
+        //var refreshCount = 0;
+        //var plane3D = _plane3D;
+        //if (plane3D is null) return;
+
+        //DispatcherTimer? dt = null;
+        //dt = new DispatcherTimer(remeasureInterval, DispatcherPriority.Normal,
+        //    (sender, args) =>
+        //    {
+        //        plane3D.InvalidateMeasure();
+        //        if (refreshCount++ == granularity)
+        //            dt?.Stop();
+        //    }, Dispatcher);
+        //dt.Start();
+    }
+
+    private void UpdateVisualStates(bool useTransitions)
+    {
+        VisualStateManager.GoToState(this, IsFlipped ? TemplateFlippedStateName : TemplateUnflippedStateName,
+            useTransitions);
+    }
+
+    private void FlipHandler(object sender, ExecutedRoutedEventArgs executedRoutedEventArgs)
+    {
+        SetCurrentValue(IsFlippedProperty, !IsFlipped);
+    }
+}
