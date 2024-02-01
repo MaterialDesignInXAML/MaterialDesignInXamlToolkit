516acb43a843040b0f5920da97cd75dac113c938
Kevin Bost <kitokeboo@gmail.com>
2024-01-31 22:33:56 -0800

    AddingMenuItemAssist.HighlightedColor
    This allows changing the background color when the menu item is highlighted

---
+++ MaterialDesignThemes.Wpf/MenuItemAssist.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf;
+
+public static class MenuItemAssist
+{
+    public static Brush? GetHighlightedBackground(DependencyObject obj)
+        => (Brush?)obj.GetValue(HighlightedBackgroundProperty);
+
+    public static void SetHighlightedBackground(DependencyObject obj, Brush? value)
+        => obj.SetValue(HighlightedBackgroundProperty, value);
+
+    // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty HighlightedBackgroundProperty =
+        DependencyProperty.RegisterAttached("HighlightedBackground", typeof(Brush), typeof(MenuItemAssist), new PropertyMetadata(null));
+}
