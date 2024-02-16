7205a856b09af0fdc670114e4042bef56c833a6e
Nicolai Henriksen <nicolai.h80@gmail.com>
2024-02-16 20:38:02 +0100

    SmartHintNew (copy) in place. Now it must be reworked to get rid of magic numbers and weirdness

---
+++ MaterialDesignThemes.Wpf/SmartHintNew.cs
@@ -0,0 +1,18 @@
+﻿using MaterialDesignThemes.Wpf.Converters;
+
+namespace MaterialDesignThemes.Wpf;
+
+/// <summary>
+/// A control that implement placeholder behavior. Can work as a simple placeholder either as a floating hint, see <see cref="UseFloating"/> property.
+/// <para/>
+/// To set a target control you should set the HintProxy property. Use the <see cref="HintProxyFabricConverter.Instance"/> converter which converts a control into the IHintProxy interface.
+/// </summary>
+[TemplateVisualState(GroupName = ContentStatesGroupName, Name = HintRestingPositionName)]
+[TemplateVisualState(GroupName = ContentStatesGroupName, Name = HintFloatingPositionName)]
+public class SmartHintNew : SmartHint
+{
+    static SmartHintNew()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(SmartHintNew), new FrameworkPropertyMetadata(typeof(SmartHintNew)));
+    }
+}
