aba491ee84a5e108afe5a8f23b07fb94eafcd05c
Kevin Bost <kitokeboo@gmail.com>
2024-03-06 22:15:56 -0800

    File scoped namespace of converter
    Bindable property cleanup

--- MaterialDesignThemes.Wpf/Converters/VerticalAlignmentConverter.cs
+++ MaterialDesignThemes.Wpf/Converters/VerticalAlignmentConverter.cs
@@ -1,15 +1,14 @@
 ﻿using System.Globalization;
 using System.Windows.Data;
 
-namespace MaterialDesignThemes.Wpf.Converters
-{
-    public class VerticalAlignmentConverter : IValueConverter
-    {
-        public VerticalAlignment StretchReplacement { get; set; } = VerticalAlignment.Top;
-        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => value is VerticalAlignment.Stretch ? StretchReplacement : value;
+namespace MaterialDesignThemes.Wpf.Converters;
 
-        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => throw new NotImplementedException();
-    }
+public class VerticalAlignmentConverter : IValueConverter
+{
+    public VerticalAlignment StretchReplacement { get; set; } = VerticalAlignment.Top;
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is VerticalAlignment.Stretch ? StretchReplacement : value;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
 }
