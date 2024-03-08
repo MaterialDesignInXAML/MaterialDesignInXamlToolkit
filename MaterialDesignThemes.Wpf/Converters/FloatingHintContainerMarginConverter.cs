80c748d7e10d567194155d4d47cdf4afc0a1fce2
Nicolai Henriksen <nicolai.h80@gmail.com>
2024-03-08 22:59:54 +0100

    Move SmartHint converters into their own files

---
+++ MaterialDesignThemes.Wpf/Converters/FloatingHintContainerMarginConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters;
+
+public class FloatingHintContainerMarginConverter : IMultiValueConverter
+{
+    public object? Convert(object?[]? values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values?.Length != 2
+            || values.Any(v => v == null)
+            || !double.TryParse(values[0]!.ToString(), out double scale)
+            || values[1] is not Thickness floatingMargin)
+        {
+            return new Thickness(0);
+        }
+
+        return floatingMargin with
+        {
+            Left = floatingMargin.Left * scale,
+            Top = floatingMargin.Top * scale,
+            Right = floatingMargin.Right * scale,
+            Bottom = floatingMargin.Bottom * scale,
+        };
+    }
+
+    public object[]? ConvertBack(object? value, Type[] targetTypes, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
---
+++ MaterialDesignThemes.Wpf/Converters/FloatingHintScaleTransformConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf.Converters;
+
+public class FloatingHintScaleTransformConverter : IMultiValueConverter
+{
+    public object? Convert(object?[]? values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values?.Length != 3
+            || values.Any(v => v == null)
+            || !double.TryParse(values[0]!.ToString(), out double scale)
+            || !double.TryParse(values[1]!.ToString(), out double lower)
+            || !double.TryParse(values[2]!.ToString(), out double upper))
+        {
+            return Transform.Identity;
+        }
+        double scalePercentage = upper + (lower - upper) * scale;
+        return new ScaleTransform(scalePercentage, scalePercentage);
+    }
+
+    public object[]? ConvertBack(object? value, Type[] targetTypes, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
