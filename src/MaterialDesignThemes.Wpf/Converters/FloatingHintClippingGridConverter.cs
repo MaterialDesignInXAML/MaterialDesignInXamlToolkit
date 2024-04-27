44e519d59b716603e364137c07f9bd8b3262bdba
Nicolai Henriksen <nicolai.h80@gmail.com>
2024-04-27 14:05:14 +0200

    Fix hint clipping issue

---
+++ src/MaterialDesignThemes.Wpf/Converters/FloatingHintClippingGridConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf.Converters;
+
+public class FloatingHintClippingGridConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value switch
+        {
+            double actualWidth => new RectangleGeometry(new Rect(new Point(0, 0), new Size(actualWidth, double.MaxValue))),
+            _ => null
+        };
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
