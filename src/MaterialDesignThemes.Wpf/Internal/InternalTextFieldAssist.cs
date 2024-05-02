855f104d5274c75aff85fb82933269da7fc11e8f
Nicolai Henriksen <nicolai.h80@gmail.com>
2024-05-02 09:17:57 +0200

    Move AP to dedicated type in the internal namespace

---
+++ src/MaterialDesignThemes.Wpf/Internal/InternalTextFieldAssist.cs
@@ -0,0 +1,13 @@
+﻿namespace MaterialDesignThemes.Wpf.Internal;
+
+public static class InternalTextFieldAssist
+{
+    /// <summary>
+    /// Used by text field "wrappers" (i.e. controls hosting a text field and decorating on top of it) to signal to the text field that the mouse is over it,
+    /// when in fact it is over a sibling (i.e. something in the wrapper) which is visually placed on top of the text field.
+    /// </summary>
+    public static readonly DependencyProperty IsMouseOverProperty = DependencyProperty.RegisterAttached(
+        "IsMouseOver", typeof(bool), typeof(InternalTextFieldAssist), new PropertyMetadata(default(bool)));
+    public static void SetIsMouseOver(DependencyObject element, bool value) => element.SetValue(IsMouseOverProperty, value);
+    public static bool GetIsMouseOver(DependencyObject element) => (bool)element.GetValue(IsMouseOverProperty);
+}
--- src/MaterialDesignThemes.Wpf/TextFieldAssist.cs
+++ src/MaterialDesignThemes.Wpf/TextFieldAssist.cs
@@ -9,15 +9,6 @@ namespace MaterialDesignThemes.Wpf;
 public static class TextFieldAssist
 {
     /// <summary>
-    /// Used by text field "wrappers" (i.e. controls hosting a text field and decorating on top of it) to signal to the text field that the mouse is over it,
-    /// when in fact it is over a sibling (i.e. something in the wrapper) which is visually placed on top of the text field.
-    /// </summary>
-    public static readonly DependencyProperty IsMouseOverProperty = DependencyProperty.RegisterAttached(
-        "IsMouseOver", typeof(bool), typeof(TextFieldAssist), new PropertyMetadata(default(bool)));
-    public static void SetIsMouseOver(DependencyObject element, bool value) => element.SetValue(IsMouseOverProperty, value);
-    public static bool GetIsMouseOver(DependencyObject element) => (bool) element.GetValue(IsMouseOverProperty);
-
-    /// <summary>
     /// The text box view margin property
     /// </summary>
     public static readonly DependencyProperty TextBoxViewMarginProperty = DependencyProperty.RegisterAttached(
