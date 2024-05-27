8585b3e094289549deec183263abb19278cf748f
Joris Voermans <joris@clevr.net>
2024-05-27 08:41:52 +0200

    GroupBoxAssist.HeaderPadding (#3569)

---
+++ src/MaterialDesignThemes.Wpf/GroupBoxAssist.cs
@@ -0,0 +1,14 @@
+﻿namespace MaterialDesignThemes.Wpf;
+
+public static class GroupBoxAssist
+{
+    private const double DefaultHeaderPadding = 9.0;
+
+    #region AttachedProperty : HeaderPaddingProperty
+    public static readonly DependencyProperty HeaderPaddingProperty
+            = DependencyProperty.RegisterAttached("HeaderPadding", typeof(double), typeof(GroupBoxAssist), new PropertyMetadata(DefaultHeaderPadding));
+
+    public static double GetHeaderPadding(GroupBox element) => (double)element.GetValue(HeaderPaddingProperty);
+    public static void SetHeaderPadding(GroupBox element, double headerPadding) => element.SetValue(HeaderPaddingProperty, headerPadding);
+    #endregion
+}
