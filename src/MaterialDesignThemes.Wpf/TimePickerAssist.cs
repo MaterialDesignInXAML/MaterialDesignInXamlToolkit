91020d7f1e868d890ce3bb54247f6f1dcc038670
Kevin Bost <kitokeboo@gmail.com>
2024-06-26 23:16:19 -0700

    Fixing obsolete message on assist classes.

--- src/MaterialDesignThemes.Wpf/TimePickerAssist.cs
+++ src/MaterialDesignThemes.Wpf/TimePickerAssist.cs
@@ -1,6 +1,6 @@
 ﻿namespace MaterialDesignThemes.Wpf;
 
-[Obsolete("This class is obsolete and will be removed in a future version. Please use the TextBoxAssist equivalents instead. For OutlinedBorderInactiveThickness, simply use BorderThickness property instead.")]
+[Obsolete("This class is obsolete and will be removed in a future version. Please use the TextFieldAssist equivalents instead. For OutlinedBorderInactiveThickness, simply use TimePicker.BorderThickness property instead.")]
 public static class TimePickerAssist
 {
     public static readonly DependencyProperty OutlinedBorderInactiveThicknessProperty = DependencyProperty.RegisterAttached(
