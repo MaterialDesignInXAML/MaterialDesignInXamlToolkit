4a888e28b119a654fd80bd6564a2fb4048ca01d0
Joris Voermans <joris@clevr.net>
2024-07-02 09:28:28 +0200

    Set ScrollViewerAssist.IgnorePadding by default to True (#3622)

--- src/MaterialDesignThemes.Wpf/ScrollViewerAssist.cs
+++ src/MaterialDesignThemes.Wpf/ScrollViewerAssist.cs
@@ -76,7 +76,7 @@ public static class ScrollViewerAssist
     }
 
     public static readonly DependencyProperty IgnorePaddingProperty = DependencyProperty.RegisterAttached(
-    "IgnorePadding", typeof(bool), typeof(ScrollViewerAssist), new PropertyMetadata(false));
+    "IgnorePadding", typeof(bool), typeof(ScrollViewerAssist), new PropertyMetadata(true));
 
     public static void SetIgnorePadding(DependencyObject element, bool value) => element.SetValue(IgnorePaddingProperty, value);
     public static bool GetIgnorePadding(DependencyObject element) => (bool)element.GetValue(IgnorePaddingProperty);
