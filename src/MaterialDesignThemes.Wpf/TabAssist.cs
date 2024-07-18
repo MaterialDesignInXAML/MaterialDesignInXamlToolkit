99c93651743cf66366657fc310dae15df1f8a7c4
Nicolai Henriksen <nicolai.h80@gmail.com>
2024-07-18 09:13:22 +0200

    Add new AP to support custom margin in TabControl header

--- src/MaterialDesignThemes.Wpf/TabAssist.cs
+++ src/MaterialDesignThemes.Wpf/TabAssist.cs
@@ -5,16 +5,29 @@ public static class TabAssist
     public static readonly DependencyProperty HasFilledTabProperty = DependencyProperty.RegisterAttached(
         "HasFilledTab", typeof(bool), typeof(TabAssist), new PropertyMetadata(false));
 
-    public static void SetHasFilledTab(DependencyObject element, bool value) => element.SetValue(HasFilledTabProperty, value);
+    public static void SetHasFilledTab(DependencyObject element, bool value)
+        => element.SetValue(HasFilledTabProperty, value);
 
-    public static bool GetHasFilledTab(DependencyObject element) => (bool)element.GetValue(HasFilledTabProperty);
+    public static bool GetHasFilledTab(DependencyObject element)
+        => (bool)element.GetValue(HasFilledTabProperty);
 
     public static readonly DependencyProperty HasUniformTabWidthProperty = DependencyProperty.RegisterAttached(
         "HasUniformTabWidth", typeof(bool), typeof(TabAssist), new PropertyMetadata(false));
 
-    public static void SetHasUniformTabWidth(DependencyObject element, bool value) => element.SetValue(HasUniformTabWidthProperty, value);
+    public static void SetHasUniformTabWidth(DependencyObject element, bool value)
+        => element.SetValue(HasUniformTabWidthProperty, value);
 
-    public static bool GetHasUniformTabWidth(DependencyObject element) => (bool)element.GetValue(HasUniformTabWidthProperty);
+    public static bool GetHasUniformTabWidth(DependencyObject element)
+        => (bool)element.GetValue(HasUniformTabWidthProperty);
+
+    public static readonly DependencyProperty HeaderPanelMarginProperty = DependencyProperty.RegisterAttached(
+        "HeaderPanelMargin", typeof(Thickness), typeof(TabAssist), new PropertyMetadata(default(Thickness)));
+
+    public static void SetHeaderPanelMargin(DependencyObject element, Thickness value)
+        => element.SetValue(HeaderPanelMarginProperty, value);
+
+    public static Thickness GetHeaderPanelMargin(DependencyObject element)
+        => (Thickness) element.GetValue(HeaderPanelMarginProperty);
 
     internal static Visibility GetBindableIsItemsHost(DependencyObject obj)
         => (Visibility)obj.GetValue(BindableIsItemsHostProperty);
