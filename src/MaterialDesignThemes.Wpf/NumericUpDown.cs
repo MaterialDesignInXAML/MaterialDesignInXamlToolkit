94f217777ff67b6e6fe7458a335c24ebed933975
kankan <xukan90@foxmail.com>
2024-11-07 14:40:57 +0800

    NumericUpDown control support decimals value, and the value for each increase or decrease can be set. (#3661)
    * Support decimals value, and the size of value for each increase or decrease can be set.
    
    * UpDown control support decimals, and the value for each increase or decrease can be set.
    
    * Updates to UpDownBase to support generic math on NET8
    
    TODO: Need to fix XAMLTest to support the common generic base class so the generator stops choking
    
    * Updating UI tests
    
    Updated new XAMLTest library
    Added DeciamlUpDown tests
    
    ---------
    
    Co-authored-by: Kevin Bost <kevin@intellitect.com>
    Co-authored-by: Kevin Bost <kitokeboo@gmail.com>

--- src/MaterialDesignThemes.Wpf/NumericUpDown.cs
+++ src/MaterialDesignThemes.Wpf/NumericUpDown.cs
@@ -1,259 +1,41 @@
-using System.ComponentModel;
+#if !NET8_0_OR_GREATER
 using System.Globalization;
+#endif
 
 namespace MaterialDesignThemes.Wpf;
 
-[TemplatePart(Name = IncreaseButtonPartName, Type = typeof(RepeatButton))]
-[TemplatePart(Name = DecreaseButtonPartName, Type = typeof(RepeatButton))]
-[TemplatePart(Name = TextBoxPartName, Type = typeof(TextBox))]
-public class NumericUpDown : Control
+public class NumericUpDown
+#if NET8_0_OR_GREATER
+    : UpDownBase<int>
+#else
+    : UpDownBase<int, IntArithmetic>
+#endif
 {
-    public const string IncreaseButtonPartName = "PART_IncreaseButton";
-    public const string DecreaseButtonPartName = "PART_DecreaseButton";
-    public const string TextBoxPartName = "PART_TextBox";
-
-    private TextBox? _textBoxField;
-    private RepeatButton? _decreaseButton;
-    private RepeatButton? _increaseButton;
-
     static NumericUpDown()
     {
         DefaultStyleKeyProperty.OverrideMetadata(typeof(NumericUpDown), new FrameworkPropertyMetadata(typeof(NumericUpDown)));
     }
-
-    #region DependencyProperties
-
-
-
-    public object? IncreaseContent
-    {
-        get => GetValue(IncreaseContentProperty);
-        set => SetValue(IncreaseContentProperty, value);
-    }
-
-    // Using a DependencyProperty as the backing store for IncreaseContent.  This enables animation, styling, binding, etc...
-    public static readonly DependencyProperty IncreaseContentProperty =
-        DependencyProperty.Register(nameof(IncreaseContent), typeof(object), typeof(NumericUpDown), new PropertyMetadata(null));
-
-    public object? DecreaseContent
-    {
-        get => GetValue(DecreaseContentProperty);
-        set => SetValue(DecreaseContentProperty, value);
-    }
-
-    // Using a DependencyProperty as the backing store for DecreaseContent.  This enables animation, styling, binding, etc...
-    public static readonly DependencyProperty DecreaseContentProperty =
-        DependencyProperty.Register(nameof(DecreaseContent), typeof(object), typeof(NumericUpDown), new PropertyMetadata(null));
-
-    #region DependencyProperty : MinimumProperty
-    public int Minimum
-    {
-        get => (int)GetValue(MinimumProperty);
-        set => SetValue(MinimumProperty, value);
-    }
-    public static readonly DependencyProperty MinimumProperty =
-        DependencyProperty.Register(nameof(Minimum), typeof(int), typeof(NumericUpDown), new PropertyMetadata(int.MinValue, OnMinimumChanged));
-
-    private static void OnMinimumChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        NumericUpDown ctrl = (NumericUpDown)d;
-        ctrl.CoerceValue(ValueProperty);
-        ctrl.CoerceValue(MaximumProperty);
-    }
-
-    #endregion DependencyProperty : MinimumProperty
-
-    #region DependencyProperty : MaximumProperty
-
-    public int Maximum
-    {
-        get => (int)GetValue(MaximumProperty);
-        set => SetValue(MaximumProperty, value);
-    }
-
-    public static readonly DependencyProperty MaximumProperty =
-        DependencyProperty.Register(nameof(Maximum), typeof(int), typeof(NumericUpDown), new PropertyMetadata(int.MaxValue, OnMaximumChanged, CoerceMaximum));
-
-    private static void OnMaximumChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        NumericUpDown ctrl = (NumericUpDown)d;
-        ctrl.CoerceValue(ValueProperty);
-    }
-
-    private static object? CoerceMaximum(DependencyObject d, object? value)
-    {
-        if (d is NumericUpDown numericUpDown &&
-            value is int numericValue)
-        {
-            return Math.Max(numericUpDown.Minimum, numericValue);
-        }
-        return value;
-    }
-
-    #endregion DependencyProperty : MaximumProperty
-
-    #region DependencyProperty : ValueProperty
-    public int Value
-    {
-        get => (int)GetValue(ValueProperty);
-        set => SetValue(ValueProperty, value);
-    }
-
-    public static readonly DependencyProperty ValueProperty =
-            DependencyProperty.Register(nameof(Value), typeof(int), typeof(NumericUpDown), new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnNumericValueChanged, CoerceNumericValue));
-
-    private static void OnNumericValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        if (d is NumericUpDown numericUpDown)
-        {
-            var args = new RoutedPropertyChangedEventArgs<int>((int)e.OldValue, (int)e.NewValue)
-            {
-                RoutedEvent = ValueChangedEvent
-            };
-            numericUpDown.RaiseEvent(args);
-            if (numericUpDown._textBoxField is { } textBox)
-            {
-                textBox.Text = ((int)e.NewValue).ToString(CultureInfo.CurrentUICulture);
-            }
-
-            if (numericUpDown._increaseButton is { } increaseButton)
-            {
-                increaseButton.IsEnabled = numericUpDown.Value != numericUpDown.Maximum;
-            }
-
-            if (numericUpDown._decreaseButton is { } decreaseButton)
-            {
-                decreaseButton.IsEnabled = numericUpDown.Value != numericUpDown.Minimum;
-            }
-        }
-    }
-
-    private static object? CoerceNumericValue(DependencyObject d, object? value)
-    {
-        if (d is NumericUpDown numericUpDown &&
-            value is int numericValue)
-        {
-            numericValue = Math.Min(numericUpDown.Maximum, numericValue);
-            numericValue = Math.Max(numericUpDown.Minimum, numericValue);
-            return numericValue;
-        }
-        return value;
-    }
-    #endregion ValueProperty
-
-    #region DependencyProperty : AllowChangeOnScroll
-
-    public bool AllowChangeOnScroll
-    {
-        get => (bool)GetValue(AllowChangeOnScrollProperty);
-        set => SetValue(AllowChangeOnScrollProperty, value);
-    }
-
-    public static readonly DependencyProperty AllowChangeOnScrollProperty =
-        DependencyProperty.Register(nameof(AllowChangeOnScroll), typeof(bool), typeof(NumericUpDown), new PropertyMetadata(false));
-
-    #endregion
-
-    #endregion DependencyProperties
-
-    #region Event : ValueChangedEvent
-    [Category("Behavior")]
-    public static readonly RoutedEvent ValueChangedEvent = EventManager.RegisterRoutedEvent(nameof(ValueChanged), RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<int>), typeof(NumericUpDown));
-
-    public event RoutedPropertyChangedEventHandler<int> ValueChanged
-    {
-        add => AddHandler(ValueChangedEvent, value);
-        remove => RemoveHandler(ValueChangedEvent, value);
-    }
-    #endregion Event : ValueChangedEvent
-
-    public override void OnApplyTemplate()
-    {
-        if (_increaseButton != null)
-            _increaseButton.Click -= IncreaseButtonOnClick;
-
-        if (_decreaseButton != null)
-            _decreaseButton.Click -= DecreaseButtonOnClick;
-        if (_textBoxField != null)
-            _textBoxField.TextChanged -= OnTextBoxFocusLost;
-
-        _increaseButton = GetTemplateChild(IncreaseButtonPartName) as RepeatButton;
-        _decreaseButton = GetTemplateChild(DecreaseButtonPartName) as RepeatButton;
-        _textBoxField = GetTemplateChild(TextBoxPartName) as TextBox;
-
-        if (_increaseButton != null)
-            _increaseButton.Click += IncreaseButtonOnClick;
-
-        if (_decreaseButton != null)
-            _decreaseButton.Click += DecreaseButtonOnClick;
-
-        if (_textBoxField != null)
-        {
-            _textBoxField.LostFocus += OnTextBoxFocusLost;
-            _textBoxField.Text = Value.ToString(CultureInfo.CurrentUICulture);
-        }
-
-        base.OnApplyTemplate();
-    }
-
-    private void OnTextBoxFocusLost(object sender, EventArgs e)
-    {
-        if (_textBoxField is { } textBoxField)
-        {
-            if (int.TryParse(textBoxField.Text, NumberStyles.Integer, CultureInfo.CurrentUICulture, out int numericValue))
-            {
-                SetCurrentValue(ValueProperty, numericValue);
-            }
-            else
-            {
-                textBoxField.Text = Value.ToString(CultureInfo.CurrentUICulture);
-            }
-        }
-    }
-
-    private void IncreaseButtonOnClick(object sender, RoutedEventArgs e) => OnIncrease();
-
-    private void DecreaseButtonOnClick(object sender, RoutedEventArgs e) => OnDecrease();
-
-    private void OnIncrease()
-    {
-        SetCurrentValue(ValueProperty, Value + 1);
-    }
-
-    private void OnDecrease()
-    {
-        SetCurrentValue(ValueProperty, Value - 1);
-    }
-
-    protected override void OnPreviewKeyDown(KeyEventArgs e)
-    {
-        if (e.Key == Key.Up)
-        {
-            OnIncrease();
-            e.Handled = true;
-        }
-        else if (e.Key == Key.Down)
-        {
-            OnDecrease();
-            e.Handled = true;
-        }
-        base.OnPreviewKeyDown(e);
-    }
-
-    protected override void OnPreviewMouseWheel(MouseWheelEventArgs e)
-    {
-        if (IsKeyboardFocusWithin && AllowChangeOnScroll)
-        {
-            if (e.Delta > 0)
-            {
-                OnIncrease();
-            }
-            else if (e.Delta < 0)
-            {
-                OnDecrease();
-            }
-            e.Handled = true;
-        }
-        base.OnPreviewMouseWheel(e);
-    }
 }
+
+#if !NET8_0_OR_GREATER
+public class IntArithmetic : IArithmetic<int>
+{
+    public int Add(int value1, int value2) => value1 + value2;
+
+    public int Subtract(int value1, int value2) => value1 - value2;
+
+    public int Compare(int value1, int value2) => value1.CompareTo(value2);
+
+    public int MinValue() => int.MinValue;
+
+    public int MaxValue() => int.MaxValue;
+    public int One() => 1;
+
+    public int Max(int value1, int value2) => Math.Max(value1, value2);
+
+    public int Min(int value1, int value2) => Math.Min(value1, value2);
+
+    public bool TryParse(string text, IFormatProvider? formatProvider, out int value)
+        => int.TryParse(text, NumberStyles.Integer, formatProvider, out value);
+}
+#endif
---
+++ src/MaterialDesignThemes.Wpf/UpDownBase.cs
@@ -0,0 +1,325 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace MaterialDesignThemes.Wpf;
+
+
+#if NET8_0_OR_GREATER
+using System.Numerics;
+public class UpDownBase<T> : UpDownBase
+    where T : INumber<T>, IMinMaxValue<T>
+{
+    private static readonly Type SelfType = typeof(UpDownBase<T>);
+
+    private static UpDownBase<T> ToUpDownBase(DependencyObject dependencyObject) => (UpDownBase<T>)dependencyObject;
+
+    private static T MinValue => T.MinValue;
+    private static T MaxValue => T.MaxValue;
+    private static T One => T.One;
+    private static T Max(T value1, T value2) => T.Max(value1, value2);
+    private static T Clamp(T value, T min, T max) => T.Clamp(value, min, max);
+    private static T Add(T value1, T value2) => value1 + value2;
+    private static T Subtract(T value1, T value2) => value1 - value2;
+    private static bool TryParse(string text, IFormatProvider? formatProvider, out T? value)
+        => T.TryParse(text, formatProvider, out value);
+    private static int Compare(T value1, T value2) => value1.CompareTo(value2);
+#else
+public class UpDownBase<T, TArithmetic> : UpDownBase
+    where TArithmetic : IArithmetic<T>, new()
+{
+    private static readonly Type SelfType = typeof(UpDownBase<T, TArithmetic>);
+    private static readonly TArithmetic _arithmetic = new();
+
+    private static UpDownBase<T, TArithmetic> ToUpDownBase(DependencyObject dependencyObject) => (UpDownBase<T, TArithmetic>)dependencyObject;
+
+    private static T MinValue => _arithmetic.MinValue();
+    private static T MaxValue => _arithmetic.MaxValue();
+    private static T One => _arithmetic.One();
+    private static T Max(T value1, T value2) => _arithmetic.Max(value1, value2);
+    private static T Clamp(T value, T min, T max) => _arithmetic.Max(_arithmetic.Min(value, max), min);
+    private static T Add(T value1, T value2) => _arithmetic.Add(value1, value2);
+    private static T Subtract(T value1, T value2) => _arithmetic.Subtract(value1, value2);
+    private static bool TryParse(string text, IFormatProvider? formatProvider, out T? value)
+        => _arithmetic.TryParse(text, formatProvider, out value);
+    private static int Compare(T value1, T value2) => _arithmetic.Compare(value1, value2);
+#endif
+
+    #region DependencyProperties
+
+    #region DependencyProperty : MinimumProperty
+
+    public virtual T Minimum
+    {
+        get => (T)GetValue(MinimumProperty);
+        set => SetValue(MinimumProperty, value);
+    }
+    public static readonly DependencyProperty MinimumProperty =
+        DependencyProperty.Register(nameof(Minimum), typeof(T), SelfType, new PropertyMetadata(MinValue, OnMinimumChanged));
+
+    private static void OnMinimumChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctrl = ToUpDownBase(d);
+        ctrl.CoerceValue(MaximumProperty);
+        ctrl.CoerceValue(ValueProperty);
+    }
+
+    #endregion DependencyProperty : MinimumProperty
+
+    #region DependencyProperty : MaximumProperty
+
+    public T Maximum
+    {
+        get => (T)GetValue(MaximumProperty);
+        set => SetValue(MaximumProperty, value);
+    }
+
+    public static readonly DependencyProperty MaximumProperty =
+        DependencyProperty.Register(nameof(Maximum), typeof(T), SelfType, new PropertyMetadata(MaxValue, OnMaximumChanged, CoerceMaximum));
+
+    private static void OnMaximumChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var upDownBase = ToUpDownBase(d);
+        upDownBase.CoerceValue(ValueProperty);
+    }
+
+    private static object? CoerceMaximum(DependencyObject d, object? value)
+    {
+        if (value is T numericValue)
+        {
+            var upDownBase = ToUpDownBase(d);
+            return Max(upDownBase.Minimum, numericValue);
+        }
+        return value;
+    }
+
+    #endregion DependencyProperty : MaximumProperty
+
+    #region DependencyProperty : ValueProperty
+    public T Value
+    {
+        get => (T)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+
+    public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(nameof(Value), typeof(T), SelfType, new FrameworkPropertyMetadata(default(T), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnNumericValueChanged, CoerceNumericValue));
+
+    private static void OnNumericValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var upDownBase = ToUpDownBase(d);
+        var args = new RoutedPropertyChangedEventArgs<T>((T)e.OldValue, (T)e.NewValue)
+        {
+            RoutedEvent = ValueChangedEvent
+        };
+        upDownBase.RaiseEvent(args);
+
+        if (upDownBase._textBoxField is { } textBox)
+        {
+            textBox.Text = e.NewValue.ToString();
+        }
+
+        if (upDownBase._increaseButton is { } increaseButton)
+        {
+            increaseButton.IsEnabled = Compare(upDownBase.Value, upDownBase.Maximum) < 0;
+        }
+
+        if (upDownBase._decreaseButton is { } decreaseButton)
+        {
+            decreaseButton.IsEnabled = Compare(upDownBase.Value, upDownBase.Minimum) > 0;
+        }
+    }
+
+    private static object? CoerceNumericValue(DependencyObject d, object? value)
+    {
+        if (value is T numericValue)
+        {
+            var upDownBase = ToUpDownBase(d);
+            numericValue = Clamp(numericValue, upDownBase.Minimum, upDownBase.Maximum);
+            return numericValue;
+        }
+        return value;
+    }
+    #endregion ValueProperty
+
+    #region DependencyProperty : ValueStep
+    /// <summary>
+    /// The step of value for each increase or decrease
+    /// </summary>
+    public T ValueStep
+    {
+        get => (T)GetValue(ValueStepProperty);
+        set => SetValue(ValueStepProperty, value);
+    }
+
+    public static readonly DependencyProperty ValueStepProperty =
+        DependencyProperty.Register(nameof(ValueStep), typeof(T), SelfType, new PropertyMetadata(One));
+    #endregion
+
+    #region DependencyProperty : AllowChangeOnScroll
+
+    public bool AllowChangeOnScroll
+    {
+        get => (bool)GetValue(AllowChangeOnScrollProperty);
+        set => SetValue(AllowChangeOnScrollProperty, value);
+    }
+
+    public static readonly DependencyProperty AllowChangeOnScrollProperty =
+        DependencyProperty.Register(nameof(AllowChangeOnScroll), typeof(bool), SelfType, new PropertyMetadata(false));
+
+    #endregion
+
+    #endregion DependencyProperties
+
+    #region Event : ValueChangedEvent
+    [Category("Behavior")]
+    public static readonly RoutedEvent ValueChangedEvent = EventManager.RegisterRoutedEvent(nameof(ValueChanged), RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<T>), SelfType);
+
+    public event RoutedPropertyChangedEventHandler<T> ValueChanged
+    {
+        add => AddHandler(ValueChangedEvent, value);
+        remove => RemoveHandler(ValueChangedEvent, value);
+    }
+    #endregion Event : ValueChangedEvent
+
+    public override void OnApplyTemplate()
+    {
+        if (_increaseButton != null)
+            _increaseButton.Click -= IncreaseButtonOnClick;
+
+        if (_decreaseButton != null)
+            _decreaseButton.Click -= DecreaseButtonOnClick;
+        if (_textBoxField != null)
+            _textBoxField.TextChanged -= OnTextBoxFocusLost;
+
+        _increaseButton = GetTemplateChild(IncreaseButtonPartName) as RepeatButton;
+        _decreaseButton = GetTemplateChild(DecreaseButtonPartName) as RepeatButton;
+        _textBoxField = GetTemplateChild(TextBoxPartName) as TextBox;
+
+        if (_increaseButton != null)
+            _increaseButton.Click += IncreaseButtonOnClick;
+
+        if (_decreaseButton != null)
+            _decreaseButton.Click += DecreaseButtonOnClick;
+
+        if (_textBoxField != null)
+        {
+            _textBoxField.LostFocus += OnTextBoxFocusLost;
+            _textBoxField.Text = Value?.ToString();
+        }
+
+        base.OnApplyTemplate();
+    }
+
+    private void OnTextBoxFocusLost(object sender, EventArgs e)
+    {
+        if (_textBoxField is { } textBoxField)
+        {
+            if (TryParse(textBoxField.Text, CultureInfo.CurrentUICulture, out T? value))
+            {
+                SetCurrentValue(ValueProperty, value);
+            }
+            else
+            {
+                textBoxField.Text = Value?.ToString();
+            }
+        }
+    }
+
+    private void IncreaseButtonOnClick(object sender, RoutedEventArgs e) => OnIncrease();
+
+    private void DecreaseButtonOnClick(object sender, RoutedEventArgs e) => OnDecrease();
+
+    private void OnIncrease() => SetCurrentValue(ValueProperty, Clamp(Add(Value, ValueStep), Minimum, Maximum));
+
+    private void OnDecrease() => SetCurrentValue(ValueProperty, Clamp(Subtract(Value, ValueStep), Minimum, Maximum));
+
+    protected override void OnPreviewKeyDown(KeyEventArgs e)
+    {
+        if (e.Key == Key.Up)
+        {
+            OnIncrease();
+            e.Handled = true;
+        }
+        else if (e.Key == Key.Down)
+        {
+            OnDecrease();
+            e.Handled = true;
+        }
+        base.OnPreviewKeyDown(e);
+    }
+
+    protected override void OnPreviewMouseWheel(MouseWheelEventArgs e)
+    {
+        if (IsKeyboardFocusWithin && AllowChangeOnScroll)
+        {
+            if (e.Delta > 0)
+            {
+                OnIncrease();
+            }
+            else if (e.Delta < 0)
+            {
+                OnDecrease();
+            }
+            e.Handled = true;
+        }
+        base.OnPreviewMouseWheel(e);
+    }
+}
+
+[TemplatePart(Name = IncreaseButtonPartName, Type = typeof(RepeatButton))]
+[TemplatePart(Name = DecreaseButtonPartName, Type = typeof(RepeatButton))]
+[TemplatePart(Name = TextBoxPartName, Type = typeof(TextBox))]
+public class UpDownBase : Control
+{
+    public const string IncreaseButtonPartName = "PART_IncreaseButton";
+    public const string DecreaseButtonPartName = "PART_DecreaseButton";
+    public const string TextBoxPartName = "PART_TextBox";
+
+    protected TextBox? _textBoxField;
+    protected RepeatButton? _decreaseButton;
+    protected RepeatButton? _increaseButton;
+
+    public object? IncreaseContent
+    {
+        get => GetValue(IncreaseContentProperty);
+        set => SetValue(IncreaseContentProperty, value);
+    }
+
+    // Using a DependencyProperty as the backing store for IncreaseContent.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty IncreaseContentProperty =
+        DependencyProperty.Register(nameof(IncreaseContent), typeof(object), typeof(UpDownBase), new PropertyMetadata(null));
+
+    public object? DecreaseContent
+    {
+        get => GetValue(DecreaseContentProperty);
+        set => SetValue(DecreaseContentProperty, value);
+    }
+
+    // Using a DependencyProperty as the backing store for DecreaseContent.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty DecreaseContentProperty =
+        DependencyProperty.Register(nameof(DecreaseContent), typeof(object), typeof(UpDownBase), new PropertyMetadata(null));
+
+}
+
+#if !NET8_0_OR_GREATER
+public interface IArithmetic<T>
+{
+    T Add(T value1, T value2);
+
+    T Subtract(T value1, T value2);
+
+    int Compare(T value1, T value2);
+
+    T MinValue();
+
+    T MaxValue();
+
+    T One();
+
+    T Max(T value1, T value2);
+
+    T Min(T value1, T value2);
+
+    bool TryParse(string text, IFormatProvider? formatProvider, out T? value);
+}
+#endif
