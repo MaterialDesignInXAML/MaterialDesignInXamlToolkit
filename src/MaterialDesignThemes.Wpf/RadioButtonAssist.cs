6564c4cf82497bd5afea79a1e664c57ea9a56862
mzy666888 <mzy_666888@126.com>
2024-11-20 16:49:28 +0800

    RadioButton 添加自定义大小功能

---
+++ src/MaterialDesignThemes.Wpf/RadioButtonAssist.cs
@@ -0,0 +1,23 @@
+﻿namespace MaterialDesignThemes.Wpf
+{
+    public class RadioButtonAssist
+    {
+        private const double DefaultRadioButtonSize = 18.0;
+
+        #region AttachedProperty : RadioButtonSizeProperty
+        public static readonly DependencyProperty RadioButtonSizeProperty =
+            DependencyProperty.RegisterAttached(
+                "RadioButtonSize",
+                typeof(double),
+                typeof(RadioButtonAssist),
+                new PropertyMetadata(DefaultRadioButtonSize)
+            );
+
+        public static double GetRadioButtonSize(RadioButton element) =>
+            (double)element.GetValue(RadioButtonSizeProperty);
+
+        public static void SetRadioButtonSize(RadioButton element, double checkBoxSize) =>
+            element.SetValue(RadioButtonSizeProperty, checkBoxSize);
+        #endregion
+    }
+}
