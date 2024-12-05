c9bc5502067c1be0f633474bb24a53b9edb771d3
Joris Voermans <joris@clevr.net>
2024-12-05 08:52:15 +0100

    Removed global converter resources: Handled PR feedback

--- src/MaterialDesignThemes.Wpf/Converters/BooleanToVisibilityConverter.cs
+++ src/MaterialDesignThemes.Wpf/Converters/BooleanToVisibilityConverter.cs
@@ -2,9 +2,12 @@
 
 public sealed class BooleanToVisibilityConverter : BooleanConverter<Visibility>
 {
-    public static readonly BooleanToVisibilityConverter Instance = new();
-    public static readonly BooleanToVisibilityConverter InverseInstance = new() { FalseValue = Visibility.Visible, TrueValue = Visibility.Hidden };
+    public static readonly BooleanToVisibilityConverter CollapsedInstance = new() { FalseValue = Visibility.Collapsed, TrueValue = Visibility.Visible };
+    public static readonly BooleanToVisibilityConverter NotCollapsedInstance = new() { FalseValue = Visibility.Visible, TrueValue = Visibility.Collapsed };
 
+    public static readonly BooleanToVisibilityConverter HiddenInstance = new() { FalseValue = Visibility.Hidden, TrueValue = Visibility.Visible };
+    public static readonly BooleanToVisibilityConverter NotHiddenInstance = new() { FalseValue = Visibility.Visible, TrueValue = Visibility.Hidden };
+    
     public BooleanToVisibilityConverter() :
         base(Visibility.Visible, Visibility.Collapsed)
     { }
--- src/MaterialDesignThemes.Wpf/Converters/TextFieldHintVisibilityConverter.cs
+++ src/MaterialDesignThemes.Wpf/Converters/TextFieldHintVisibilityConverter.cs
@@ -5,8 +5,8 @@ namespace MaterialDesignThemes.Wpf.Converters;
 
 public class TextFieldHintVisibilityConverter : IValueConverter
 {
-    public static readonly TextFieldHintVisibilityConverter Instance = new();
-    public static readonly TextFieldHintVisibilityConverter StringIsEmptyInstance = new() { IsEmptyValue = Visibility.Collapsed, IsNotEmptyValue = Visibility.Visible};
+    public static readonly TextFieldHintVisibilityConverter IsNotEmptyInstance = new();
+    public static readonly TextFieldHintVisibilityConverter IsEmptyInstance = new() { IsEmptyValue = Visibility.Collapsed, IsNotEmptyValue = Visibility.Visible};
 
     public Visibility IsEmptyValue { get; set; } = Visibility.Visible;
     public Visibility IsNotEmptyValue { get; set; } = Visibility.Hidden;
