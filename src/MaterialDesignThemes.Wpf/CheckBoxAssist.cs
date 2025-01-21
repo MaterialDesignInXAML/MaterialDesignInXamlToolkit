a9beaaa3b75d3c4f31c3d5412580f73cbd7b020b
mzy666888 <mzy_666888@126.com>
2025-01-21 11:47:58 +0800

    RadioButton and CheckBox use custom foreground

--- src/MaterialDesignThemes.Wpf/CheckBoxAssist.cs
+++ src/MaterialDesignThemes.Wpf/CheckBoxAssist.cs
@@ -1,3 +1,5 @@
+using System.Windows.Media;
+
 namespace MaterialDesignThemes.Wpf;
 
 public class CheckBoxAssist
@@ -5,10 +7,31 @@ public class CheckBoxAssist
     private const double DefaultCheckBoxSize = 18.0;
 
     #region AttachedProperty : CheckBoxSizeProperty
-    public static readonly DependencyProperty CheckBoxSizeProperty
-        = DependencyProperty.RegisterAttached("CheckBoxSize", typeof(double), typeof(CheckBoxAssist), new PropertyMetadata(DefaultCheckBoxSize));
+    public static readonly DependencyProperty CheckBoxSizeProperty = DependencyProperty.RegisterAttached(
+        "CheckBoxSize",
+        typeof(double),
+        typeof(CheckBoxAssist),
+        new PropertyMetadata(DefaultCheckBoxSize)
+    );
 
     public static double GetCheckBoxSize(CheckBox element) => (double)element.GetValue(CheckBoxSizeProperty);
+
     public static void SetCheckBoxSize(CheckBox element, double checkBoxSize) => element.SetValue(CheckBoxSizeProperty, checkBoxSize);
+    #endregion
+
+
+
+    #region AttachedProperty : CheckBoxForegroundProperty
+    public static readonly DependencyProperty CheckBoxForegroundProperty = DependencyProperty.RegisterAttached(
+        "CheckBoxForeground",
+        typeof(Brush),
+        typeof(CheckBoxAssist),
+        null
+    );
+
+    public static Brush GetCheckBoxForeground(CheckBox element) => (Brush)element.GetValue(CheckBoxForegroundProperty);
+
+    public static void SetCheckBoxForeground(CheckBox element, Brush checkBoxForeground) =>
+        element.SetValue(CheckBoxForegroundProperty, checkBoxForeground);
     #endregion
 }
