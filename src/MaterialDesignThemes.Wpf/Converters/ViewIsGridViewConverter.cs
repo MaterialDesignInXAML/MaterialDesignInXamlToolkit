2cb2f69a1ab47035080b364445fb09624d71879f
MohammadHadi Attarieh <mhadi.atttarieh@gmail.com>
2025-06-23 11:50:11 +0330

    Implement GridView support for having columns

---
+++ src/MaterialDesignThemes.Wpf/Converters/ViewIsGridViewConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters;
+
+internal class ViewIsGridViewConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // Returns true if value is a GridView, otherwise false
+        return value is GridView;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
