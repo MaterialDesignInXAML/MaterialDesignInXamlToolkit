6b40aa2cb78011072eb28ba042b9ac08dea12d65
Nicolai Henriksen <nicolai.h80@gmail.com>
2025-09-29 12:54:03 +0200

    Place custom scrollbar correctly using converters

---
+++ src/MaterialDesignThemes.Wpf/Converters/TextBoxHorizontalScrollBarMarginConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters;
+
+public class TextBoxHorizontalScrollBarMarginConverter : IMultiValueConverter
+{
+    public object? Convert(object?[]? values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values is [
+            double leadingIconWidth,
+            Thickness leadingIconMargin,
+            double prefixTextWidth,
+            Thickness prefixTextMargin])
+        {
+            double offset = leadingIconWidth + leadingIconMargin.Left + leadingIconMargin.Right + prefixTextWidth + prefixTextMargin.Left + prefixTextMargin.Right;
+            return new Thickness(offset, 1, 0, 0);
+        }
+        return new Thickness(0);
+    }
+
+    public object?[]? ConvertBack(object? value, Type[] targetTypes, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
