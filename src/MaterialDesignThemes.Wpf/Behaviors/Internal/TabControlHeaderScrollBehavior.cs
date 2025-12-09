05111176fc49d568be7be1c5e0d7131489e6cc9e
Nicolai Henriksen <nicolai.h80@gmail.com>
2025-12-09 21:52:37 +0100

    Adding smooth/animated scrolling to the behavior

--- src/MaterialDesignThemes.Wpf/Behaviors/Internal/TabControlHeaderScrollBehavior.cs
+++ src/MaterialDesignThemes.Wpf/Behaviors/Internal/TabControlHeaderScrollBehavior.cs
@@ -1,12 +1,25 @@
 ﻿using System.Diagnostics;
+using System.Windows.Media.Animation;
 using Microsoft.Xaml.Behaviors;
 
 namespace MaterialDesignThemes.Wpf.Behaviors.Internal;
 
 public class TabControlHeaderScrollBehavior : Behavior<ScrollViewer>
 {
+    public static readonly DependencyProperty CustomHorizontalOffsetProperty =
+    DependencyProperty.RegisterAttached("CustomHorizontalOffset", typeof(double),
+        typeof(TabControlHeaderScrollBehavior), new PropertyMetadata(0d, CustomHorizontalOffsetChanged));
+    public static double GetCustomHorizontalOffset(DependencyObject obj) => (double)obj.GetValue(CustomHorizontalOffsetProperty);
+    public static void SetCustomHorizontalOffset(DependencyObject obj, double value) => obj.SetValue(CustomHorizontalOffsetProperty, value);
+    private static void CustomHorizontalOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var scrollViewer = (ScrollViewer)d;
+        scrollViewer.ScrollToHorizontalOffset((double)e.NewValue);
+    }
+
     public static readonly DependencyProperty TabScrollDirectionProperty =
-        DependencyProperty.RegisterAttached("TabScrollDirection", typeof(TabScrollDirection), typeof(TabControlHeaderScrollBehavior), new PropertyMetadata(TabScrollDirection.Unknown));
+        DependencyProperty.RegisterAttached("TabScrollDirection", typeof(TabScrollDirection),
+            typeof(TabControlHeaderScrollBehavior), new PropertyMetadata(TabScrollDirection.Unknown));
     public static TabScrollDirection GetTabScrollDirection(DependencyObject obj) => (TabScrollDirection)obj.GetValue(TabScrollDirectionProperty);
     public static void SetTabScrollDirection(DependencyObject obj, TabScrollDirection value) => obj.SetValue(TabScrollDirectionProperty, value);
 
@@ -33,12 +46,16 @@ public class TabControlHeaderScrollBehavior : Behavior<ScrollViewer>
         }
     }
 
+    private double? _desiredScrollStart;
+    private bool _isAnimatingScroll;
+
     private void OnTabChanged(object sender, SelectionChangedEventArgs e)
     {
         TabControl tabControl = (TabControl)sender;
 
         if (e.AddedItems.Count > 0)
         {
+            _desiredScrollStart = AssociatedObject.ContentHorizontalOffset;
             SetTabScrollDirection(tabControl, (IsMovingForward() ? TabScrollDirection.Forward : TabScrollDirection.Backward));
         }
 
@@ -70,7 +87,23 @@ public class TabControlHeaderScrollBehavior : Behavior<ScrollViewer>
 
     private void AssociatedObject_ScrollChanged(object sender, ScrollChangedEventArgs e)
     {
-        Debug.WriteLine($"HorizontalOffset: {e.HorizontalOffset}, ViewportWidth: {e.ViewportWidth}, ExtentWidth: {e.ExtentWidth}");
+        Debug.WriteLine($"ContentHorizontalOffset: {AssociatedObject.ContentHorizontalOffset}, HorizontalOffset: {e.HorizontalOffset}, HorizontalChange: {e.HorizontalChange}, ViewportWidth: {e.ViewportWidth}, ExtentWidth: {e.ExtentWidth}");
+        if (_isAnimatingScroll || _desiredScrollStart is not { } desiredOffsetStart) return;
+
+        double originalValue = desiredOffsetStart;
+        double newValue = e.HorizontalOffset;
+
+        _isAnimatingScroll = true;
+        AssociatedObject.ScrollToHorizontalOffset(originalValue);
+        Debug.WriteLine($"Initiating animated scroll from {originalValue} to {newValue}. Change is: {e.HorizontalChange}");
+        DoubleAnimation scrollAnimation = new(originalValue, newValue, new Duration(TimeSpan.FromMilliseconds(250)));
+        scrollAnimation.Completed += (_, _) =>
+        {
+            Debug.WriteLine("Animation completed");
+            _desiredScrollStart = null;
+            _isAnimatingScroll = false;
+        };
+        AssociatedObject.BeginAnimation(TabControlHeaderScrollBehavior.CustomHorizontalOffsetProperty, scrollAnimation);
     }
 }
 
