8f02def51a1ac8e4fa67f8a5b642b7ba4164ffe8
Nicolai Henriksen <nicolai.h80@gmail.com>
2025-12-09 21:42:55 +0100

    Improves tab header scrolling behavior
    Adds a behavior to handle tab selection changes intended to adjust scrolling for better user experience.
    Introduces a custom panel to hijack the BringIntoView event, allowing for offsetting the Rect being scrolled to based on the tab selection direction.

---
+++ src/MaterialDesignThemes.Wpf/Behaviors/Internal/TabControlHeaderScrollBehavior.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+using Microsoft.Xaml.Behaviors;
+
+namespace MaterialDesignThemes.Wpf.Behaviors.Internal;
+
+public class TabControlHeaderScrollBehavior : Behavior<ScrollViewer>
+{
+    public static readonly DependencyProperty TabScrollDirectionProperty =
+        DependencyProperty.RegisterAttached("TabScrollDirection", typeof(TabScrollDirection), typeof(TabControlHeaderScrollBehavior), new PropertyMetadata(TabScrollDirection.Unknown));
+    public static TabScrollDirection GetTabScrollDirection(DependencyObject obj) => (TabScrollDirection)obj.GetValue(TabScrollDirectionProperty);
+    public static void SetTabScrollDirection(DependencyObject obj, TabScrollDirection value) => obj.SetValue(TabScrollDirectionProperty, value);
+
+    public TabControl TabControl
+    {
+        get => (TabControl)GetValue(TabControlProperty);
+        set => SetValue(TabControlProperty, value);
+    }
+
+    public static readonly DependencyProperty TabControlProperty =
+        DependencyProperty.Register(nameof(TabControl), typeof(TabControl),
+            typeof(TabControlHeaderScrollBehavior), new PropertyMetadata(null, OnTabControlChanged));
+
+    private static void OnTabControlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var behavior = (TabControlHeaderScrollBehavior)d;
+        if (e.OldValue is TabControl oldTabControl)
+        {
+            oldTabControl.SelectionChanged -= behavior.OnTabChanged;
+        }
+        if (e.NewValue is TabControl newTabControl)
+        {
+            newTabControl.SelectionChanged += behavior.OnTabChanged;
+        }
+    }
+
+    private void OnTabChanged(object sender, SelectionChangedEventArgs e)
+    {
+        TabControl tabControl = (TabControl)sender;
+
+        if (e.AddedItems.Count > 0)
+        {
+            SetTabScrollDirection(tabControl, (IsMovingForward() ? TabScrollDirection.Forward : TabScrollDirection.Backward));
+        }
+
+        bool IsMovingForward()
+        {
+            if (e.RemovedItems.Count == 0) return true;
+            int previousIndex = GetItemIndex(e.RemovedItems[0]);
+            int nextIndex = GetItemIndex(e.AddedItems[^1]);
+            return nextIndex > previousIndex;
+        }
+
+        int GetItemIndex(object? item) => tabControl.Items.IndexOf(item);
+    }
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.ScrollChanged += AssociatedObject_ScrollChanged;
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        if (AssociatedObject is { } ao)
+        {
+            ao.ScrollChanged -= AssociatedObject_ScrollChanged;
+        }
+    }
+
+    private void AssociatedObject_ScrollChanged(object sender, ScrollChangedEventArgs e)
+    {
+        Debug.WriteLine($"HorizontalOffset: {e.HorizontalOffset}, ViewportWidth: {e.ViewportWidth}, ExtentWidth: {e.ExtentWidth}");
+    }
+}
+
+public enum TabScrollDirection
+{
+    Unknown,
+    Backward,
+    Forward
+}
---
+++ src/MaterialDesignThemes.Wpf/Internal/BringIntoViewHijackingStackPanel.cs
@@ -0,0 +1,36 @@
+﻿
+using MaterialDesignThemes.Wpf.Behaviors.Internal;
+
+namespace MaterialDesignThemes.Wpf.Internal;
+
+public class BringIntoViewHijackingStackPanel : StackPanel
+{
+    public TabScrollDirection TabScrollDirection
+    {
+        get => (TabScrollDirection)GetValue(TabScrollDirectionProperty);
+        set => SetValue(TabScrollDirectionProperty, value);
+    }
+
+    public static readonly DependencyProperty TabScrollDirectionProperty =
+        DependencyProperty.Register(nameof(TabScrollDirection), typeof(TabScrollDirection),
+            typeof(BringIntoViewHijackingStackPanel), new PropertyMetadata(TabScrollDirection.Unknown));
+
+    public BringIntoViewHijackingStackPanel()
+        => AddHandler(FrameworkElement.RequestBringIntoViewEvent, new RoutedEventHandler(OnRequestBringIntoView), false);
+
+    private void OnRequestBringIntoView(object sender, RoutedEventArgs e)
+    {
+        if (e.OriginalSource is FrameworkElement child && child != this)
+        {
+            e.Handled = true;
+            double offset = TabScrollDirection switch {
+                TabScrollDirection.Backward => -52,
+                TabScrollDirection.Forward => 52,
+                _ => 0
+            };
+            var point = child.TranslatePoint(new Point(), this);
+            var newTargetRect = new Rect(new Point(point.X + offset, point.Y), child.RenderSize);
+            BringIntoView(newTargetRect);
+        }
+    }
+}
