2e0183b78ade1684477316ee57f91ac7c41f0b0b
Nicolai Henriksen <nicolai.h80@gmail.com>
2026-04-24 15:11:17 +0200

    Introduce BindingProxy to mitigate temporary XAML binding failures

---
+++ src/MaterialDesignThemes.Wpf/BindingProxy.cs
@@ -0,0 +1,15 @@
+﻿namespace MaterialDesignThemes.Wpf;
+
+public class BindingProxy : Freezable
+{
+    protected override Freezable CreateInstanceCore() => new BindingProxy();
+
+    public static readonly DependencyProperty DataProperty =
+        DependencyProperty.Register(nameof(Data), typeof(object), typeof(BindingProxy), new PropertyMetadata(null));
+
+    public object? Data
+    {
+        get => GetValue(DataProperty);
+        set => SetValue(DataProperty, value);
+    }
+}
