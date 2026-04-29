b0c8e8f81b1413e55fa402ba2deed8d572f792af
Corvin <43533385+corvinsz@users.noreply.github.com>
2026-04-29 18:18:22 +0200

    docs: add MinuteSelectionStep example to MD2/3 demo apps and consolidate viewmodels

--- src/MaterialDesign3.Demo.Wpf/Pickers.xaml.cs
+++ src/MaterialDesign3.Demo.Wpf/Pickers.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Globalization;
-using MaterialDesign3Demo.Domain;
+using MaterialDesignDemo.Domain;
 using MaterialDesignThemes.Wpf;
 
 namespace MaterialDesign3Demo;
