836e12bb0a6a6b4861e530492f929c06e2fa3da2
Nicolai Henriksen <nicolai.h80@gmail.com>
2026-05-01 07:18:39 +0200

    Introduce BindingProxy to mitigate temporary XAML binding failures (#4045)
    * Introduce BindingProxy to mitigate temporary XAML binding failures
    
    * Internalize BindingProxy and seal the class
    
    Moves `BindingProxy` to the `MaterialDesignThemes.Wpf.Internal` namespace and marks it as `sealed`. This change clarifies that `BindingProxy` is an internal implementation detail of the library, preventing direct external usage or inheritance and helping to maintain a cleaner public API surface.
    
    Relates to #3814
    
    ---------
    
    Co-authored-by: Kevin Bost <kitokeboo@gmail.com>

---
+++ src/MaterialDesignThemes.Wpf/Internal/BindingProxy.cs
@@ -0,0 +1,15 @@
+﻿namespace MaterialDesignThemes.Wpf.Internal;
+
+public sealed class BindingProxy : Freezable
+{
+    protected override Freezable CreateInstanceCore() => new BindingProxy();
+
+    public static readonly DependencyProperty DataProperty =
+        DependencyProperty.Register(nameof(Data), typeof(object), typeof(BindingProxy), new PropertyMetadata(null));
+
+    public object? Data
+    {
+        get => GetValue(DataProperty);
+        set => SetValue(DataProperty, value);
+    }
+}
